167a465ba840c2cff0e7af819816c68ce98dfe0e
Sam Kothari <sam.kothari@gov.ab.ca>
2020-12-10 10:17:42 -0700

    Cypress Automation Update

---
+++ apps/QA/cypress/integration/smoke-Test/TenantManagementApp/TenantManage.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+import { Before, After, Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+
+Given(/^I am a user$/, () => {
+
+});
+
+When(/^I do something$/, () => {
+
+});
+
+Then(/^I get something$/, () => {
+    cy.request('GET', 'https://tenant-management-api-core-services-dev.os99.gov.ab.ca/health').then(function (response) {
+        expect(response.status).to.eq(200)
+        expect(response.body, 'response body').to.have.property('uptime')
+    });
+})
