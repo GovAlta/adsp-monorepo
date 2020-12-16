8f38ef3bf81cb300717dd0f3b43a733b2f46b374
Chris Olsen <chris.olsen@gov.ab.ca>
2020-12-16 10:32:14 -0700

    add ability to fetch environment specific configuration settings from API

--- apps/tenant-management-webapp/src/app/components/notifications/index.tsx
+++ apps/tenant-management-webapp/src/app/components/notifications/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import BaseApp from '../../baseApp';
+import useConfig from '../../utils/useConfig';
 
 function Notifications() {
+
+  const [config,,] = useConfig();
+
   return (
     <BaseApp>
       <h2>Future notifications</h2>
+      {config && `Notification Url: ${config.notificationServiceUrl}`}
     </BaseApp>
   );
 }
---
+++ apps/tenant-management-webapp/src/app/utils/useConfig.ts
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+
+export const configUrl = '/config/config.json';
+export const configKey = 'config-key';
+
+/**
+ * Interface of the properties contained within the config.json file
+ * returned from the API
+ */
+interface Config {
+  notificationServiceUrl: string;
+  keyCloakUrl: string;
+}
+
+/**
+ * Config states
+ */
+type State = 'loading' | 'error' | 'loaded';
+
+/**
+ * React hook that fetches the environment specific configuration
+ */
+export function useConfig(): [Config, State, string] {
+  const [config, setConfig] = useState<Config>();
+  const [error, setError] = useState<string>();
+  const [state, setState] = useState<State>('loading');
+
+  useEffect(() => {
+    const _getConfig = async () => {
+      setState('loading')
+      try {
+        setConfig(await fetchConfig());
+        setState('loaded')
+      } catch(e) {
+        setState('error')
+        if (e instanceof Error) {
+          setError((e as Error).message)
+        } else {
+          setError('failed to fetch config');
+        }
+      }
+    };
+    _getConfig();
+  }, []);
+
+  return [config, state, error];
+}
+
+// Private
+
+async function fetchConfig(): Promise<Config> {
+  let cached = localStorage.getItem(configKey);
+  if (!cached) {
+    const res = await fetch(configUrl);
+    if (!res.ok) {
+      throw new Error('failed to fetch config settings');
+    }
+    const text = await res.text();
+    localStorage.setItem(configKey, text);
+    cached = text;
+  }
+
+  return cached && JSON.parse(cached);
+}
+
+export default useConfig;
