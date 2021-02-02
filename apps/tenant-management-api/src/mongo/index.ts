26ab3bcce0e9b1fa1e88755174b7ec27fdca35f0
athena.chen <athena.chen@gov.ab.ca>
2021-02-02 12:01:35 -0700

    add user and password to mongo option

--- apps/tenant-management-api/src/mongo/index.ts
+++ apps/tenant-management-api/src/mongo/index.ts
@@ -4,20 +4,27 @@ import { logger } from '../middleware/logger';
 
 export const connectMongo = async () => {
   try {
-     const mongoHost = environment.MONGO_URI || process.env.MONGO_URI;
-     const mongoDb = environment.MONGO_DB || process.env.MONGO_DB;
+    const mongoHost = environment.MONGO_URI || process.env.MONGO_URI;
+    const mongoDb = environment.MONGO_DB || process.env.MONGO_DB;
+    const mongoUser = environment.MONGO_USER || process.env.MONGO_USER;
+    const mongoPassword = environment.MONGO_PASSWORD || process.env.MONGO_PASSWORD;
 
     const mongoURI = `${mongoHost}/${mongoDb}`;
+
     logger.info(`Mondodb URI is  ${mongoURI}`);
     logger.info(`environment.MONGO_URI   ${mongoHost}`);
+
     const options: ConnectionOptions = {
+      user:mongoUser,
+      pass:mongoPassword,
       useNewUrlParser: true,
-      useCreateIndex: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
     };
+
     await connect(mongoURI, options);
     logger.info('MongoDB Connected...');
+    
   } catch (err) {
     // Exit process with failure
     logger.error(`MongoDB has error, ${err.message} will exit ...`);
