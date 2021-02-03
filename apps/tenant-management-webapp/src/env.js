857480d9391d34972f1e2c26436b93313d9116df
Jonathan.Weyermann <jonathan.weyermann@gov.ab.ca>
2021-02-02 17:14:14 -0700

    Linter fix

--- apps/tenant-management-webapp/src/env.js
+++ apps/tenant-management-webapp/src/env.js
@@ -9,12 +9,14 @@ const localEnvs = {
 }
 
 const env = () => {
-  if (process.env.NODE_ENV !== 'development'){
-    const [envs,,] = getConfig();
+  if (process.env.NODE_ENV !== 'development') {
+    const [envs, ,] = getConfig();
     return envs
   } else {
     return localEnvs;
   }
 }
+
+
 
 export default env;
