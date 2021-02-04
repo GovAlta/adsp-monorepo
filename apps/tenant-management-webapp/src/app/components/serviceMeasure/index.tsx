57e113a1dc7581afa6876d8394426caab06e6b06
Jonathan Weyermann <jonathan.weyermann@gov.ab.ca>
2021-02-04 09:50:18 -0800

    should hopefully fix env variables

--- apps/tenant-management-webapp/src/app/components/serviceMeasure/index.tsx
+++ apps/tenant-management-webapp/src/app/components/serviceMeasure/index.tsx
@@ -11,6 +11,8 @@ function ServiceMeasure() {
   return (
     <Container>
       <h2>Future Service Measures</h2>
+      You are running this application in <b>{process.env.NODE_ENV}</b> mode.
+      <div>{JSON.stringify(process.env)}</div>
       <button onClick={() => dispatch(serviceMeasure.getUptime())}>
         Refresh Uptime
       </button>
--- apps/tenant-management-webapp/src/app/store/sagas/index.ts
+++ apps/tenant-management-webapp/src/app/store/sagas/index.ts
@@ -1,6 +1,8 @@
 import { uptimeFetch } from './serviceMeasure';
-import { fork, all } from "redux-saga/effects";
+import { fork, all, takeEvery } from "redux-saga/effects";
+import { UPTIME_FETCH } from '../actions/types';
 
 export function* watchSagas() {
   yield all([fork(uptimeFetch)]);
+  yield takeEvery(UPTIME_FETCH, uptimeFetch);
 }
