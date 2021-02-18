c0e80042192442ee83fb677b8f7562373dbc4929
Chris Olsen <chris.olsen@gov.ab.ca>
2021-02-18 13:33:16 -0700

    add interfaces and consistent letter casing for the persistent app state

--- apps/tenant-management-webapp/src/app/store/reducers/index.ts
+++ apps/tenant-management-webapp/src/app/store/reducers/index.ts
@@ -1,17 +1,22 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 
-import File from "./file";
-import serviceMeasure from "./serviceMeasure";
-import User from "./user";
-import Config from "./config";
+import File from './file';
+import ServiceMeasure from './serviceMeasure';
+import User from './user';
+import Config from './config';
 import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'
+import storage from 'redux-persist/lib/storage';
 
-const rootReducer = combineReducers({ File, serviceMeasure, User, Config });
+const rootReducer = combineReducers({
+  file: File,
+  serviceMeasure: ServiceMeasure,
+  user: User,
+  config: Config,
+});
 const persistConfig = {
   key: 'root',
   storage,
-}
+};
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
