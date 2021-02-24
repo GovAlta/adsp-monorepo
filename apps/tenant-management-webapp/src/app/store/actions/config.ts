ac3fafb2b2c6b116013a1fb5f19b39023f23aaa6
Paul.Li <paul.li@gov.ab.ca>
2021-02-24 15:20:29 -0700

    Update config.ts

--- apps/tenant-management-webapp/src/app/store/actions/config.ts
+++ apps/tenant-management-webapp/src/app/store/actions/config.ts
@@ -1,17 +1,6 @@
-<<<<<<< Updated upstream
 import { Keycloak } from '../reducers/config.contract';
 
 export const CONFIG_SET_KEYCLOAK = 'tenant/config/set-keycloak';
-=======
-export const CONFIG_SET_KEYCLOAK = 'tenant/config/set-keycloak';
-
-export interface Keycloak {
-  realm: string;
-  url: string;
-  clientId: string;
-}
-
->>>>>>> Stashed changes
 export interface KeyCloakAction {
   type: string;
   payload: Keycloak;
