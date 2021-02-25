94c24c14cbad3dee26b0416370f5f9c0474a5f27
Paul.Li <paul.li@gov.ab.ca>
2021-02-25 14:31:45 -0700

    Fix tenant router bug

--- apps/tenant-management-api/src/app/router/index.ts
+++ apps/tenant-management-api/src/app/router/index.ts
@@ -7,11 +7,12 @@ import realmRouter from './realm';
 import { tenantPublicRouter, tenantRouter } from './tenant';
 
 export const apiRouter = Router();
+export const apiPublicRouter = Router();
+
 const passportMiddleware = passport.authenticate(['jwt'], { session: false });
 
+apiPublicRouter.use('/tenant/v1', tenantPublicRouter);
 apiRouter.use('/discovery/v1', [passportMiddleware, directoryRouter]);
 apiRouter.use('/realm', [passportMiddleware, realmRouter]);
-apiRouter.use('/file/v1', [passportMiddleware, realmRouter]);
-apiRouter.use('/tenant/v1', [passportMiddleware, realmRouter]);
-
-apiRouter.use('/tenant/v1', tenantPublicRouter);
+apiRouter.use('/file/v1', [passportMiddleware, fileRouter]);
+apiRouter.use('/tenant/v1', [passportMiddleware, tenantRouter]);
