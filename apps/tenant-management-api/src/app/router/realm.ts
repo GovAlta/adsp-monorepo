fb128b50a2e2c814a1f5b60cc2bfabc2fcb4e99e
Sam Huang <huang.c.sam@gmail.com>
2021-02-26 06:16:05 -0700

    CS-193:  updated tenant-management-api server side.  Realm created successfully with GOA SSO

--- apps/tenant-management-api/src/app/router/realm.ts
+++ apps/tenant-management-api/src/app/router/realm.ts
@@ -5,6 +5,7 @@ import * as HttpStatusCodes from 'http-status-codes';
 import * as TenantModel from '../services/tenant/model';
 import validationMiddleware from '../../middleware/requestValidator';
 import { createkcAdminClient } from '../../keycloak';
+import { environment } from '../../environments/environment';
 
 const realmRouter = Router();
 class CreateRealmDto {
@@ -51,7 +52,61 @@ async function createRealm(req, res) {
           .json({ error: 'Create Realm failed!' });
       }
 
-      logger.info('Starting create tenant');
+      logger.info('Starting create IdentityProvider...');
+      const idp = await kcAdminClient.identityProviders.create({
+        realm: realmName,
+        alias: 'core',
+        providerId: 'keycloak-oidc',
+        enabled: true,
+        trustEmail: true,
+        firstBrokerLoginFlowAlias: 'first broker login',
+        displayName: 'GOA Single SignOn',
+        storeToken: false,
+        linkOnly: false,
+        addReadTokenRoleOnCreate: false,
+        config: {
+          loginHint: 'true',
+          clientId: 'broker',
+          tokenUrl:
+            environment.KEYCLOAK_CORE_TOKEN_URL ||
+            process.env.KEYCLOAK_CORE_TOKEN_URL,
+          authorizationUrl:
+            environment.KEYCLOAK_CORE_AUTH_URL ||
+            process.env.KEYCLOAK_CORE_AUTH_URL,
+          clientAuthMethod: 'client_secret_basic',
+          syncMode: 'IMPORT',
+          clientSecret:
+            environment.KEYCLOAK_CORE_CLIENT_SECRET ||
+            process.env.KEYCLOAK_CORE_CLIENT_SECRET,
+          prompt: 'login',
+          useJwksUrl: 'true',
+        },
+      });
+
+      const brokerClient = await kcAdminClient.clients.findOne({
+        id:
+          environment.KEYCLOAK_CORE_BROKER_CLIENT_ID ||
+          process.env.KEYCLOAK_CORE_BROKER_CLIENT_ID,
+        realm: environment.KEYCLOAK_REALM || process.env.KEYCLOAK_REALM,
+      });
+
+      logger.info('Starting add redirectURI to broker...');
+      const updatedBroker = await kcAdminClient.clients.update(
+        {
+          id:
+            environment.KEYCLOAK_CORE_BROKER_CLIENT_ID ||
+            process.env.KEYCLOAK_CORE_BROKER_CLIENT_ID,
+          realm: environment.KEYCLOAK_REALM || process.env.KEYCLOAK_REALM,
+        },
+        {
+          redirectUris: brokerClient.redirectUris.concat(
+            (environment.KEYCLOAK_ROOT_URL || process.env.KEYCLOAK_ROOT_URL) +
+              '/auth/realms/' +
+              realmName +
+              '/broker/core/endpoint'
+          ),
+        }
+      );
 
       return res.status(HttpStatusCodes.OK).json(data);
     } catch (err) {
