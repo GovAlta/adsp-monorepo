e7ef3b2fb051fa7df5dff25121e8fd8e50b76655
athena.chen <athena.chen@gov.ab.ca>
2021-03-09 15:38:36 -0700

    Add userservice and refactor login and logout

--- apps/tenant-management-webapp/src/app/api/tenant.ts
+++
@@ -1,5 +0,0 @@
-import { executeGetWithAccessToken } from './http';
-
-export const getTenantByRealm = async (url) => {
-  return executeGetWithAccessToken(url);
-};
--- apps/tenant-management-webapp/src/app/components/login/loginSSO.tsx
+++ apps/tenant-management-webapp/src/app/components/login/loginSSO.tsx
@@ -2,55 +2,38 @@ import React, { useEffect } from 'react';
 import Header from '../../header';
 import { useSelector, useDispatch } from 'react-redux';
 import { TYPES, tenant } from '../../store/actions';
-import Keycloak from 'keycloak-js';
 import { Redirect } from 'react-router-dom';
 import { RootState } from '../../store/reducers';
-import { setAuthToken } from '../../api/http';
+import UserService from '../../service/userSevice';
 
 function LoginSSO() {
-  const keycloakConfig = useSelector(
-    (state: RootState) => state.config.keycloak
-  );
+  const isAuthenticated = useSelector((state: RootState) => state.user.authenticated);
 
-  const isAuthenticated = useSelector(
-    (state: RootState) => state.user.authenticated
-  );
-
-  const tenentName = useSelector(
-    (state: RootState) => state.tenant.tenant.name
-  );
+  const tenentName = useSelector((state: RootState) => state.tenant.tenant.name);
   const dispatch = useDispatch();
 
+  const loginHandler = (keycloak) => {
+    if (keycloak.realm) {
+      dispatch(tenant.getTenantInfo(keycloak.realm));
+    }
+    if (tenentName !== null) {
+      dispatch({ type: TYPES.USER_LOGIN_SUCCESS, keycloak });
+    } else {
+      //  TODO: need UI design
+      alert('Get tenant name Failed');
+    }
+  };
+  const loginFailed = () => {
+    // TODO: need UI design
+    alert('Login Failed');
+  };
+
   const login = () => {
-    const keycloak = Keycloak(keycloakConfig);
-    keycloak.init({ onLoad: 'login-required' }).then((authenticated) => {
-      // TODO: Add error handling if the keycloak server is down.
+    // TODO: Add error handling if the keycloak server is down.
 
-      // Get token to check is tenant exist in db
-      // if yes, login sucessful otherwise login failed.
-
-      if (authenticated) {
-        setAuthToken(keycloak.token);
-
-        if (keycloak.realm) {
-          dispatch(tenant.getTenantInfo(keycloak.realm));
-        }
-
-        keycloak.loadUserInfo().then(() => {
-          // User roles are 1in the keycloak.tokenParsed
-
-          if (tenentName !== null) {
-            dispatch({ type: TYPES.USER_LOGIN_SUCCESS, keycloak });
-          } else {
-            //  TODO: need UI design
-            alert('Get tenant name Failed');
-          }
-        });
-      } else {
-        // TODO: need UI design
-        alert('Login Failed');
-      }
-    });
+    // Get token to check is tenant exist in db
+    // if yes, login sucessful otherwise login failed.
+    UserService.login(loginHandler, loginFailed);
   };
 
   useEffect(login);
