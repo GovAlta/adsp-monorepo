cb7c1bb2bf97c54e907b0cd1d78e7575e507686b
Chris Olsen <chris.olsen@gov.ab.ca>
2021-03-15 13:22:45 -0600

    add redux devtools

--- apps/tenant-management-webapp/src/app/store/index.ts
+++ apps/tenant-management-webapp/src/app/store/index.ts
@@ -1,12 +1,13 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { persistStore } from 'redux-persist';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 
 import { persistedReducer } from './reducers';
 import { watchSagas } from './sagas';
 
 const saga = createSagaMiddleware();
-const enhancer = compose(applyMiddleware(saga));
+const enhancer = composeWithDevTools(applyMiddleware(saga));
 
 export type RootState = ReturnType<typeof persistedReducer>;
 export const store = createStore(persistedReducer, enhancer);
