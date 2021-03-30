b0012d927fa40b43deb3da5eba2d2178f4cee42b
Paul.Li <paul.li@gov.ab.ca>
2021-03-30 10:33:58 -0600

    Remove the secrets from the environment.ts - tenant-api

--- apps/tenant-management-api/src/configuration-management/index.ts
+++ apps/tenant-management-api/src/configuration-management/index.ts
@@ -2,36 +2,27 @@ import { Application } from 'express';
 import { createLogger } from '@core-services/core-common';
 import { applyConfigMiddleware } from './configuration';
 import { createRepositories } from './mongo';
-import { environment } from '../environments/environment';
 
-export const createConfigService = (
-  app: Application,
+const environment = {
+  MONGO_URI: process.env.MONGO_URI,
+  MONGO_DB: process.env.MONGO_DB,
+  MONGO_USER: process.env.MONGO_USER,
+  MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+};
 
-  ) => {
+export const createConfigService = (app: Application) => {
+  const logger = createLogger('configuration-management-service', process.env.LOG_LEVEL || 'info');
 
-  const logger = createLogger(
-    'configuration-management-service',
-    environment.LOG_LEVEL || 'info'
-  );
-
-  Promise.all([
-    createRepositories({ ...environment, logger })
-  ]).then(([repositories]) => {
-    app.get(
-      '/health',
-      (req, res) => res.json({
+  Promise.all([createRepositories({ ...environment, logger })]).then(([repositories]) => {
+    app.get('/configuration/health', (req, res) =>
+      res.json({
         db: repositories.isConnected(),
       })
     );
 
-    applyConfigMiddleware(
-      app,
-      {
-        ...repositories,
-        logger,
-      }
-    );
-  }
-  );
-}
-
+    applyConfigMiddleware(app, {
+      ...repositories,
+      logger,
+    });
+  });
+};
--- apps/tenant-management-api/src/environments/environment.ts
+++ apps/tenant-management-api/src/environments/environment.ts
@@ -1,24 +1,6 @@
 const env = process.env;
 
 export const environment = {
-  MONGO_URI: 'mongodb://localhost:27017',
-  MONGO_DB: 'tenantDb',
-  MONGO_USER: null,
-  MONGO_PASSWORD: null,
-  KEYCLOAK_ROOT_URL: 'https://access-dev.os99.gov.ab.ca',
-  KEYCLOAK_REALM: 'core',
-  KEYCLOAK_ADMIN_CLIENT_ID: 'admin-cli',
-  KEYCLOAK_CLIENT_ID: 'tenant-admin-backend-dev-qa',
-  KEYCLOAK_CLIENT_SECRET: '767995af-158a-43b2-aa1f-dd7cd9ba4ffc',
-  REALM_ADMIN_USERNAME: 'admin_dev',
-  REALM_ADMIN_PASSWORD: 'llZgcQyN+rrEjCEe8lfbTA==',
-  KEYCLOAK_CORE_TOKEN_URL: 'https://access-dev.os99.gov.ab.ca/auth/realms/core/protocol/openid-connect/token',
-  KEYCLOAK_CORE_AUTH_URL: 'https://access-dev.os99.gov.ab.ca/auth/realms/core/protocol/openid-connect/auth',
-  KEYCLOAK_CORE_CLIENT_SECRET: 'b8fafee1-455a-43a0-a141-3b93fc853cb0',
-  KEYCLOAK_CORE_BROKER_CLIENT_ID: 'b85e98c4-2793-4509-9f80-57c4e5b4125e',
-  ...env,
-  production: env.NODE_ENV === 'production',
   LOG_LEVEL: 'debug',
-
-  CONFIG_SRV_PORT: 3340,
+  ...env,
 };
