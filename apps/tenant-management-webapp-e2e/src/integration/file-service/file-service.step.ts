b7a1b0fea6cf6336311137280bae3118f4b1801b
Howard Zhang <howard.zhang@gov.ab.ca>
2021-04-01 14:25:11 -0600

    Increased the api request for inserting a mapping from 30sec to 120sec

--- apps/tenant-management-webapp-e2e/src/integration/file-service/file-service.step.ts
+++ apps/tenant-management-webapp-e2e/src/integration/file-service/file-service.step.ts
@@ -2,29 +2,32 @@ import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 
 let responseObj: Cypress.Response;
 
-Given(
-  'a testing mapping of {string}, {string} and {string} is inserted with {string}',
-  function (urnname, urnservice, serviceurl, request) {
-    const requestURL = Cypress.env('tenantManagementApi') + request;
-    const name = urnname;
-    const service = urnservice;
-    const host = serviceurl;
+Given('a testing mapping of {string}, {string} and {string} is inserted with {string}', function (
+  urnname,
+  urnservice,
+  serviceurl,
+  request
+) {
+  const requestURL = Cypress.env('tenantManagementApi') + request;
+  const name = urnname;
+  const service = urnservice;
+  const host = serviceurl;
 
-    cy.request({
-      method: 'POST',
-      url: requestURL,
-      auth: {
-        bearer: Cypress.env('token'),
-      },
-      body: {
-        name,
-        services: [{ service, host }],
-      },
-    }).then(function (response) {
-      expect(response.status).equals(201);
-    });
-  }
-);
+  cy.request({
+    method: 'POST',
+    url: requestURL,
+    auth: {
+      bearer: Cypress.env('token'),
+    },
+    body: {
+      name,
+      services: [{ service, host }],
+    },
+    timeout: 1200000,
+  }).then(function (response) {
+    expect(response.status).equals(201);
+  });
+});
 
 When('the user sends a discovery request with {string}', function (request) {
   const requestURL = Cypress.env('tenantManagementApi') + request;
@@ -39,30 +42,20 @@ When('the user sends a discovery request with {string}', function (request) {
   });
 });
 
-Then(
-  'the user should get a map of logical names to urls for all services',
-  function () {
-    // Verify the response has 200 status and an array of mappings
-    expect(responseObj.status).to.eq(200);
-    expect(responseObj.body.length).is.least(1);
-  }
-);
+Then('the user should get a map of logical names to urls for all services', function () {
+  // Verify the response has 200 status and an array of mappings
+  expect(responseObj.status).to.eq(200);
+  expect(responseObj.body.length).is.least(1);
+});
 
-Then(
-  'the user should get {string} with a mapped URL for the individual service',
-  function (fileServiceURL) {
-    // Verify that response has 200 status and url of file service
-    expect(responseObj.status).to.eq(200);
-    expect(responseObj.body).to.have.property('url').to.contain(fileServiceURL);
-  }
-);
+Then('the user should get {string} with a mapped URL for the individual service', function (fileServiceURL) {
+  // Verify that response has 200 status and url of file service
+  expect(responseObj.status).to.eq(200);
+  expect(responseObj.body).to.have.property('url').to.contain(fileServiceURL);
+});
 
-When('the user sends a delete request of {string} with {string}', function (
-  urnname,
-  request
-) {
-  const requestURL =
-    Cypress.env('tenantManagementApi') + request + '/' + urnname;
+When('the user sends a delete request of {string} with {string}', function (urnname, request) {
+  const requestURL = Cypress.env('tenantManagementApi') + request + '/' + urnname;
   cy.request({
     method: 'DELETE',
     url: requestURL,
