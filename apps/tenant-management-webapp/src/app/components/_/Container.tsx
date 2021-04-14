3cadd96c41f51ae9869802a7edbbe7bee54ffc09
Paul.Li <paul.li@gov.ab.ca>
2021-04-14 13:12:58 -0600

    Update the tenant creation pages

--- apps/tenant-management-webapp/src/app/components/_/Container.tsx
+++ apps/tenant-management-webapp/src/app/components/_/Container.tsx
@@ -12,6 +12,21 @@ const Container = styled.div`
     margin: 0 auto;
     width: calc(1024px - 4rem);
   }
+
+  h2 {
+    font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold) var(--unnamed-font-size-48) /
+      var(--unnamed-line-spacing-56) var(--unnamed-font-family-acumin-pro-semicondensed);
+    color: #333333;
+    font-size: 48px;
+    font-weight: bold;
+  }
+
+  h3 {
+    font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold) var(--unnamed-font-size-48) /
+      var(--unnamed-line-spacing-56) var(--unnamed-font-family-acumin-pro-semicondensed);
+    color: #333333;
+    font-size: 24px;
+  }
 `;
 
 export default Container;
