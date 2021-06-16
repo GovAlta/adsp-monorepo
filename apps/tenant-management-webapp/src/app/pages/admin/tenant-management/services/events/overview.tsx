783da5ff40e102f65dc46a8ccf489542c317ef77
Ting.Zuge <ting.zuge@gov.ab.ca>
2021-06-16 18:51:08 +0000

    feat: stub of event service and some tweaking of the event log views.

---
+++ apps/tenant-management-webapp/src/app/pages/admin/tenant-management/services/events/overview.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from 'react';
+
+export const EventsOverview: FunctionComponent = () => {
+  return (
+    <div>
+      <p>
+        The event service provides tenant applications with the ability to send domain events. Applications are able to
+        leverage additional capabilities as side effects through these events. For example, the event log provides
+        traceability by recording the sequence of domain events for tenants aside from application components.
+      </p>
+      <h3>Event Definitions</h3>
+      <p>
+        Event definitions are optional metadata descriptions of domain events. If a definition is configured for a
+        domain event, the event service will perform schema validation when that event is sent. The definition can be
+        utilized by services consuming events. For example, notification template configuration can validate template
+        variables as matching the payload from a triggering event.
+      </p>
+    </div>
+  );
+};
