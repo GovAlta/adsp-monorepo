58a427fd78df031398c538a3109962f8216a3b19
Chris Olsen <chris.olsen@gov.ab.ca>
2021-06-30 16:41:21 -0600

    remove global goa styles

--- apps/tenant-management-webapp/src/app/app.tsx
+++ apps/tenant-management-webapp/src/app/app.tsx
@@ -2,9 +2,6 @@ import React, { useEffect } from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Provider, useDispatch, useSelector } from 'react-redux';
 
-// TODO: remove this
-import '@abgov/core-css/goa-core.css';
-
 import LandingPage from '@pages/public/Landing';
 import { SignInError } from '@pages/public/SignInError';
 
