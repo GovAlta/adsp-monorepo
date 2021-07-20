09e43b2c885dc3f701fe7e15ddc519e1c11502fa
Chris Olsen <olsen.chris@gmail.com>
2021-07-20 14:42:47 -0600

    init tenant mongo tests

---
+++ apps/tenant-management-api/src/tenant/mongo/tenant.spec.ts
@@ -0,0 +1,30 @@
+import { connect, disconnect, createMockData } from '@core-services/core-common';
+import { TenantEntity } from '../models';
+import { TenantMongoRepository } from './tenant';
+
+describe('Mongo: Tenant', () => {
+  const repo = new TenantMongoRepository();
+
+  beforeEach(async (done) => {
+    await connect();
+    done();
+  });
+
+  afterEach(async (done) => {
+    await disconnect();
+    done();
+  });
+
+  it('should create a tenant', async () => {
+    const data = await createMockData<TenantEntity>(repo, [
+      {
+        tokenIssuer: 'ti',
+        adminEmail: 'ae',
+        realm: 'r',
+        name: 'n',
+      },
+    ]);
+    const results = await repo.find();
+    expect(results.length).toEqual(data.length);
+  });
+});
