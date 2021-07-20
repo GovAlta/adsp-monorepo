1d49ffc145f8c83b8cee3c2e7dd629e2c81ac458
Chris Olsen <olsen.chris@gmail.com>
2021-07-20 14:45:54 -0600

    init configuration router tests

--- apps/tenant-management-api/src/configuration-management/configuration/router/tenantConfig.spec.ts
+++ apps/tenant-management-api/src/configuration-management/configuration/router/tenantConfig.spec.ts
@@ -3,20 +3,57 @@ import { Mock } from 'moq.ts';
 import { TenantConfigurationRepository } from '../repository';
 import { createTenantConfigurationRouter } from './tenantConfig';
 import * as request from 'supertest';
+import { adspId } from '@abgov/adsp-service-sdk';
+import { TenantConfigEntity } from '../model';
 
 describe('Tenant Config Router', () => {
   const app = express();
   const mockRepo = new Mock<TenantConfigurationRepository>();
-  app.use(
-    createTenantConfigurationRouter({
-      tenantConfigurationRepository: mockRepo.object(),
-    })
-  );
+  const router = createTenantConfigurationRouter({
+    tenantConfigurationRepository: mockRepo.object(),
+  });
+
+  const tenantId = adspId`urn:ads:foobar`;
+  const entity: TenantConfigEntity = new TenantConfigEntity(mockRepo.object(), {
+    configurationSettingsList: null,
+    id: '',
+    tenantName: '',
+  });
+
+  app.use((req, res, next) => {
+    req.user = {
+      id: '',
+      email: '',
+      name: '',
+      roles: [],
+      tenantId: tenantId,
+      token: {
+        aud: '',
+        azp: '',
+        bearer: '',
+        iss: '',
+      },
+      isCore: true,
+    };
+    next();
+  });
+  app.use(router);
 
   describe('GET /', () => {
-    it('returns a 200 OK', () => {
-      request(app).get('/').expect(200);
+    it('returns a 200 OK', async () => {
+      mockRepo.setup((inst) => inst.getTenantConfig(`${tenantId}`)).returns(Promise.resolve(entity));
+
+      const res = await request(app).get('/');
+      expect(res.status).toBe(200);
     });
+
+    it('returns a 404 for invalid tenantIds', async () => {
+      mockRepo.setup((inst) => inst.getTenantConfig(`${tenantId}`)).returns(Promise.resolve(null));
+
+      const res = await request(app).get('/');
+      expect(res.status).toBe(404);
+    });
+
 
     it.skip('returns the top 10 results for blank service', () => {
       //
