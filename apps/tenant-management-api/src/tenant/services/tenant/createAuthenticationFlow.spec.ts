5840a0c0d82490f606295bb7d9ad95ebcc371101
Chris Olsen <olsen.chris@gmail.com>
2021-07-20 14:42:00 -0600

    init tenant service tests

---
+++ apps/tenant-management-api/src/tenant/services/tenant/createAuthenticationFlow.spec.ts
@@ -0,0 +1,7 @@
+import { testCreateAuthenticationFlow } from './createAuthenticationFlow';
+
+describe('Tenant:Services:createAuthenticationFlow', () => {
+  test('Test stub', async () => {
+    expect(testCreateAuthenticationFlow()).toBe(true);
+  });
+});
