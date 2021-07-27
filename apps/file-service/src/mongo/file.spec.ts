33a0c8654b49f73822c8f69bac01362294edadd7
Jonathan Weyermann <jonathan.weyermann@gov.ab.ca>
2021-07-27 11:49:52 -0600

    test wip..

--- apps/file-service/src/mongo/file.spec.ts
+++ apps/file-service/src/mongo/file.spec.ts
@@ -2,12 +2,14 @@ import { MongoFileRepository } from './file';
 import { MongoFileSpaceRepository } from './space';
 import * as NodeCache from 'node-cache';
 import { connect, disconnect, createMockData } from '@core-services/core-common';
-import { logger } from '@core-common/src/logger';
+import { createLogger } from '@core-services/core-common';
 import { FileEntity } from '../file/model';
-import { adspId, User } from '@abgov/adsp-service-sdk';
+import { adspId } from '@abgov/adsp-service-sdk';
 import { FileCriteria } from '../file/types';
+import { environment } from '../environments/environment';
 
 describe('Mongo: FileEntity', async () => {
+  const logger = createLogger('file-service', environment.LOG_LEVEL || 'info');
   const cache = new NodeCache({ stdTTL: 86400, useClones: false });
   const fileRepo = new MongoFileSpaceRepository(logger, cache);
   const repo = new MongoFileRepository(fileRepo);
@@ -16,9 +18,6 @@ describe('Mongo: FileEntity', async () => {
     id: adspId`urn:ads:platform:tenant-service:v2:/tenants/test`,
     realm: '12323123',
   });
-  if (!spaceId) {
-    throw new NotFoundError(`Space Not Found`, spaceId);
-  }
   const criteria: FileCriteria = {
     spaceEquals: spaceId,
     deleted: false,
@@ -35,14 +34,14 @@ describe('Mongo: FileEntity', async () => {
   });
 
   it('should create an option', async () => {
-    const data = await createMockData<FileEntity>(repo, [{ id: 1 }]);
-    const { results } = await repo.find(99, '');
+    const data = await createMockData<FileEntity>(repo, [{ id: '1' }]);
+    const { results } = await repo.find(99, '', criteria);
     expect(results.length).toEqual(data.length);
   });
 
   it('finds a defined record count ', async () => {
     const data = await createMockData<FileEntity>(repo, [{}, {}, {}]);
-    const { results } = await repo.find(2, '');
+    const { results } = await repo.find(2, '', criteria);
     expect(data.length).toEqual(3);
     expect(results.length).toEqual(2);
   });
