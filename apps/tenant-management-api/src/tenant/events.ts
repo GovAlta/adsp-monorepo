956aeb42e50ad75442da4fd6bfcee5ba2173bc82
Paul.Li <paul.li@gov.ab.ca>
2021-08-03 10:30:23 -0600

    Update the tenant creation api

--- apps/tenant-management-api/src/tenant/events.ts
+++ apps/tenant-management-api/src/tenant/events.ts
@@ -42,7 +42,7 @@ export const TenantDeletedDefinition: DomainEventDefinition = {
   },
 };
 
-export function tenantCreated(user: User, tenant: Tenant): DomainEvent {
+export function tenantCreated(user: User, tenant: Tenant, newTenant: boolean): DomainEvent {
   return {
     name: 'tenant-created',
     timestamp: new Date(),
@@ -50,6 +50,7 @@ export function tenantCreated(user: User, tenant: Tenant): DomainEvent {
     tenantId: tenant.id,
     payload: {
       tenant,
+      newTenant,
       createdBy: {
         id: user.id,
         name: user.name,
