c44ee954c558147089b9c640ba13de8a21619d5e
Chris Olsen <chris.olsen@gov.ab.ca>
2021-08-11 15:07:32 -0600

    pre css polish

--- apps/tenant-management-webapp/src/app/components/DataTable.tsx
+++ apps/tenant-management-webapp/src/app/components/DataTable.tsx
@@ -18,6 +18,11 @@ const ScrollWrapper = styled.div`
 `;
 
 const Table = styled.table`
+  --color-row-border: var(--color-gray-300);
+  --color-header-border: var(--color-gray-300);
+  --color-row--selected: var(--color-gray-200);
+  --color-th: var(--color-gray-900);
+
   border-collapse: collapse;
   width: 100%;
 
@@ -25,23 +30,20 @@ const Table = styled.table`
     padding: 0.5rem;
   }
 
-  tr {
-    border-bottom: 1px solid var(--color-table-border);
+  tr + tr {
+    border-top: 1px solid var(--color-row-border);
   }
 
   tr.selected {
-    background-color: var(--color-table-tr-selected);
+    background-color: var(--color-row--selected);
   }
 
   th {
-    padding: 0.5rem;
-    font-weight: var(--fw-bold);
-    color: var(--color-font-header);
-  }
-
-  thead th {
+    border-bottom: 2px solid var(--color-header-border);
+    color: var(--color-th);
     font-size: var(--fs-base);
-    border-bottom: 2px solid var(--color-table-border);
+    font-weight: var(--fw-bold);
+    padding: 0.5rem;
     text-align: left;
     white-space: nowrap;
   }
