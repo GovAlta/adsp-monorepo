bce2c48933634ae7af92f4f1cd7c84c33a7b9c86
Howard Zhang <howard.zhang@gov.ab.ca>
2021-09-20 10:11:19 -0600

    Updated the  event details test

--- apps/tenant-management-webapp-e2e/src/integration/events/events.page.ts
+++ apps/tenant-management-webapp-e2e/src/integration/events/events.page.ts
@@ -2,6 +2,30 @@ class eventsPage {
   eventsOverviewh3Title() {
     return cy.xpath('//h2[contains(text(), Events)]/following-sibling::div[2]//h3');
   }
+
+  event(namespace, eventName) {
+    return cy.xpath(
+      `//div[@class="group-name"][contains(text(), "${namespace}")]/following-sibling::div//td[@data-testid="name"][contains(text(), "${eventName}")]`
+    );
+  }
+
+  showDetailsIcon(namespace, eventName) {
+    return cy.xpath(
+      `//div[@class="group-name"][contains(text(), "${namespace}")]/following-sibling::div//td[@data-testid="name"][contains(text(), "${eventName}")]/following-sibling::td//*[@data-testid="toggle-details-visibility"]/*[@data-testid="icon-eye"]`
+    );
+  }
+
+  hideDetailsIcon(namespace, eventName) {
+    return cy.xpath(
+      `//div[@class="group-name"][contains(text(), "${namespace}")]/following-sibling::div//td[@data-testid="name"][contains(text(), "${eventName}")]/following-sibling::td//*[@data-testid="toggle-details-visibility"]/*[@data-testid="icon-eyeOff"]`
+    );
+  }
+
+  eventDetails(namespace, eventName) {
+    return cy.xpath(
+      `//div[@class="group-name"][contains(text(), "${namespace}")]/following-sibling::div//td[@data-testid="name"][contains(text(), "${eventName}")]/parent::tr/following-sibling::tr//*[@data-testid="details"]`
+    );
+  }
 }
 
 export default eventsPage;
