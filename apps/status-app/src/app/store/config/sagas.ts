f03de8f93b4052a1bdc7fbe1432fdc344e51ea7f
Jonathan Weyermann <jonathan.weyermann@gov.ab.ca>
2021-10-19 13:31:42 -0600

    remove some files we don't need

--- apps/status-app/src/app/store/config/sagas.ts
+++ apps/status-app/src/app/store/config/sagas.ts
@@ -2,16 +2,15 @@ import axios from 'axios';
 import { put, call } from 'redux-saga/effects';
 import { fetchConfigSuccess } from './actions';
 import { SagaIterator } from '@redux-saga/core';
-import { addErrorMessage } from '@store/session/actions';
+import { addErrorMessage } from '@store/session/actions'
 import { ConfigState } from './models';
 
 export function* fetchConfig(): SagaIterator {
   try {
     const config = (yield call(axios.get, '/config/config.json')).data as ConfigState;
-    config.serviceUrls.serviceStatusApiUrl = 'http://localhost:3338';
-    yield put(fetchConfigSuccess({ ...config, envLoaded: true }));
+    yield put(fetchConfigSuccess({...config, envLoaded: true }));
   } catch (e) {
-    console.error(e.message);
-    yield put(addErrorMessage({ message: e.message }));
+    console.error(e.message)
+    yield put(addErrorMessage({ message: e.message }))
   }
 }
