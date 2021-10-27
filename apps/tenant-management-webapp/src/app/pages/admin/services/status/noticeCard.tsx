96b354d4366ac714d0c9d5a77e7f003667fa6169
Paul <haipeng3@outlook.com>
2021-10-26 21:42:31 -0600

    Remove the timezone from date

--- apps/tenant-management-webapp/src/app/pages/admin/services/status/noticeCard.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/status/noticeCard.tsx
@@ -110,7 +110,6 @@ export const NoticeCard = (props: NoticeCardProps): JSX.Element => {
 
   const FormatNoticeDate = (date: Date): string => {
     const options: Intl.DateTimeFormatOptions = {
-      timeZone: 'Canada/Mountain',
       weekday: 'short',
       month: 'short',
       day: 'numeric',
@@ -118,6 +117,7 @@ export const NoticeCard = (props: NoticeCardProps): JSX.Element => {
       minute: '2-digit',
       year: 'numeric'
     };
+
     const timeParts = new Date(date).toLocaleString('en-US', options).split(',');
     const localTime = `${timeParts[0]}, ${timeParts[1]}, ${timeParts[2]} at ${timeParts[3].toLowerCase()}`;
     return localTime;
