a5922cb74d0fc729ec71c2a19841f861f6d1f7af
Paul <haipeng3@outlook.com>
2021-11-03 13:26:20 -0600

    Update the job definition of healthcheck

--- apps/status-service/src/app/repository/serviceStatus.ts
+++ apps/status-service/src/app/repository/serviceStatus.ts
@@ -4,9 +4,6 @@ import { ServiceStatusApplicationFilter, ServiceStatusApplication } from '../typ
 
 export interface ServiceStatusRepository extends Repository<ServiceStatusApplicationEntity, ServiceStatusApplication> {
   findEnabledApplications(): Promise<ServiceStatusApplicationEntity[]>;
-  findQueuedDisabledApplications(queuedApplicationIds: string[]): Promise<ServiceStatusApplicationEntity[]>;
-  findNonQueuedApplications(queuedApplicationIds: string[]): Promise<ServiceStatusApplicationEntity[]>;
-  findQueuedDeletedApplicationIds(queuedApplicationIds: string[]): Promise<string[]>;
   find(filter: Partial<ServiceStatusApplicationFilter>): Promise<ServiceStatusApplicationEntity[]>;
   enable(entity: ServiceStatusApplicationEntity): Promise<ServiceStatusApplicationEntity>;
   disable(entity: ServiceStatusApplicationEntity): Promise<ServiceStatusApplicationEntity>;
