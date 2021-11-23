74bc606b01af7b4a712be7cdab3bf5b5d8f9ba65
Subbu <subbumettu007@gmail.com>
2021-11-23 12:29:20 -0700

    feat: added UI landing age and redux saga to the app

--- apps/subscriber-app/src/main.tsx
+++ apps/subscriber-app/src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
 import App from './app/app';
+import { store } from './app/store/index';
 
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
