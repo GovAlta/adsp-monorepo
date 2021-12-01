168345b17b8f2d6695984cd471e15cdbe45af337
Howard Zhang <howard.zhang@gov.ab.ca>
2021-12-01 15:34:56 -0700

    Added some initial code

--- apps/tenant-management-webapp-e2e/src/integration/notifications/notifications.page.ts
+++ apps/tenant-management-webapp-e2e/src/integration/notifications/notifications.page.ts
@@ -1,3 +1,7 @@
-class notificationsPage {}
+class notificationsPage {
+  addANotificationTypeButtonOnOverview() {
+    return cy.get('[data-testid="add-notification-overview"]');
+  }
+}
 
 export default notificationsPage;
--- apps/tenant-management-webapp-e2e/src/integration/notifications/notifications.step.ts
+++ apps/tenant-management-webapp-e2e/src/integration/notifications/notifications.step.ts
@@ -5,3 +5,19 @@ import notificationsPage from './notifications.page';
 
 const commonObj = new common();
 const notificationsObj = new notificationsPage();
+
+Given('a service owner user is on notification overview page', function () {
+  commonlib.tenantAdminDirectURLLogin(
+    Cypress.config().baseUrl,
+    Cypress.env('realm'),
+    Cypress.env('email'),
+    Cypress.env('password')
+  );
+  commonObj
+    .adminMenuItem('/admin/services/notifications')
+    .click()
+    .then(function () {
+      cy.url().should('include', '/admin/services/notifications');
+      cy.wait(4000);
+    });
+});
