d696c6bab8def3ec92cd456dc642b01e10c0943b
Subbu <subbumettu007@gmail.com>
2021-12-27 12:52:48 -0700

    feat: added code necessry to edit contact info and unsubscribe user from subscriptions

--- apps/subscriber-app/src/app/pages/private/Subscriptions/Subscriptions.tsx
+++ apps/subscriber-app/src/app/pages/private/Subscriptions/Subscriptions.tsx
@@ -1,27 +1,130 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Main } from '@components/Html';
 import Container from '@components/Container';
 import styled from 'styled-components';
 import DataTable from '@components/DataTable';
-import { GoAButton, GoACard, GoAPageLoader } from '@abgov/react-components';
+import { GoAButton, GoACallout, GoACard, GoAPageLoader } from '@abgov/react-components';
+import {
+  GoAInput,
+  GoAModal,
+  GoAModalActions,
+  GoAModalContent,
+  GoAModalTitle,
+} from '@abgov/react-components/experimental';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { getMySubscriberDetails } from '@store/subscription/actions';
+import { getMySubscriberDetails, patchSubscriber, unsubscribe } from '@store/subscription/actions';
 import { RootState } from '@store/index';
 import SubscriptionsList from './SubscriptionsList';
-import { SubscriberChannel } from '@store/subscription/models';
+import { SubscriberChannel, Subscription } from '@store/subscription/models';
 
 const Subscriptions = (): JSX.Element => {
   const dispatch = useDispatch();
+  const EMAIL = 'email';
   const { subscriber } = useSelector((state: RootState) => ({
     subscriber: state.subscription.subscriber,
   }));
-  const subscriberEmail = subscriber?.channels.filter((chn: SubscriberChannel) => chn.channel === 'email')[0]?.address;
+  const subscriberEmail = subscriber?.channels.filter((chn: SubscriberChannel) => chn.channel === EMAIL)[0]?.address;
+  const [emailContactInformation, setEmailContactInformation] = useState(subscriberEmail);
+  const [editContactInformation, setEditContactInformation] = useState(false);
+  const [showUnSubscribeModal, setShowUnSubscribeModal] = useState(false);
+  const [selectedUnsubscribeSub, setSelectedUnsubscribeSub] = useState<Subscription>();
+
   useEffect(() => {
     dispatch(getMySubscriberDetails());
   }, []);
 
+  const unSubscribe = (typeId: string) => {
+    setShowUnSubscribeModal(true);
+    setSelectedUnsubscribeSub(subscriber?.subscriptions.filter((item) => item.typeId === typeId)[0]);
+  };
+  const resetSelectedUnsubscribe = () => {
+    setShowUnSubscribeModal(false);
+    setSelectedUnsubscribeSub(undefined);
+  };
+  const setValue = (name: string, value: string) => {
+    switch (name) {
+      case EMAIL:
+        setEmailContactInformation(value);
+        break;
+    }
+  };
+  const unSubscribeModal = () => {
+    return (
+      <GoAModal isOpen={true} key={1} testId="unsubscribe-modal">
+        <GoAModalTitle>Are you sure you want unsubscribe?</GoAModalTitle>
+        <GoAModalContent testId="unsubscribe-modal-content">
+          If you decide to unsubscribe from “{selectedUnsubscribeSub.type.name}” you won’t receive any updates from the
+          service in the future.{' '}
+        </GoAModalContent>
+        <GoAModalActions>
+          <GoAButton
+            buttonType="secondary"
+            data-testid="unsubscribe-modal-cancel-button"
+            onClick={() => {
+              resetSelectedUnsubscribe();
+            }}
+          >
+            Cancel
+          </GoAButton>
+          <GoAButton
+            buttonType="primary"
+            data-testid="unsubscribe-modal-okay-button"
+            onClick={() => {
+              dispatch(
+                unsubscribe({
+                  type: selectedUnsubscribeSub.typeId,
+                  subscriberId: selectedUnsubscribeSub.subscriberId,
+                })
+              );
+              resetSelectedUnsubscribe();
+            }}
+          >
+            Unsubscribe
+          </GoAButton>
+        </GoAModalActions>
+      </GoAModal>
+    );
+  };
+  const updateContactInfoButtons = () => {
+    return (
+      <div>
+        <GoAButton
+          buttonSize="small"
+          buttonType="secondary"
+          data-testid="edit-contact-cancel-button"
+          onClick={() => {
+            setEditContactInformation(!editContactInformation);
+          }}
+        >
+          Cancel
+        </GoAButton>
+        <GoAButton
+          buttonSize="small"
+          data-testid="edit-contact-save-button"
+          onClick={async () => {
+            if (subscriberEmail !== emailContactInformation) {
+              dispatch(
+                patchSubscriber(
+                  [
+                    {
+                      channel: EMAIL,
+                      address: emailContactInformation,
+                    },
+                  ],
+                  subscriber.id
+                )
+              );
+            }
+            setEditContactInformation(!editContactInformation);
+          }}
+        >
+          Save
+        </GoAButton>
+      </div>
+    );
+  };
   return subscriber?.subscriptions ? (
     <Main>
       <Container hs={2} vs={4} xlHSpacing={12}>
@@ -32,19 +135,44 @@ const Subscriptions = (): JSX.Element => {
         </p>
         <br />
         <br />
-        <GoACard title="Contact information" data-testid="contact-information">
+        {showUnSubscribeModal ? unSubscribeModal() : ''}
+        <GoACard title="Contact information" data-testid="contact-information-card">
           <Label>Email</Label>
           <ContactInformationContainer>
             <div>
-              <p>{subscriberEmail}</p>
+              {editContactInformation ? (
+                <GoAInput
+                  aria-label="email"
+                  name="email"
+                  size={emailContactInformation.length + 2}
+                  input="text"
+                  value={emailContactInformation}
+                  onChange={setValue}
+                  test-id="edit-contact-input-text"
+                />
+              ) : (
+                <p>{subscriberEmail}</p>
+              )}
             </div>
             <div>
-              <GoAButton buttonSize="small" data-testid="edit-contact">
-                Edit contact information
-              </GoAButton>
+              {editContactInformation ? (
+                updateContactInfoButtons()
+              ) : (
+                <GoAButton
+                  buttonSize="small"
+                  data-testid="edit-contact-button"
+                  onClick={() => {
+                    setEmailContactInformation(subscriberEmail);
+                    setEditContactInformation(!editContactInformation);
+                  }}
+                >
+                  Edit contact information
+                </GoAButton>
+              )}
             </div>
           </ContactInformationContainer>
         </GoACard>
+        <GoAModal></GoAModal>
         <Container hs={1} vs={5}>
           <SubscriptionListContainer>
             <DataTable data-testid="subscriptions-table">
@@ -56,7 +184,7 @@ const Subscriptions = (): JSX.Element => {
                 </tr>
               </TableHeaders>
               <tbody>
-                <SubscriptionsList subscriptions={subscriber.subscriptions} />
+                <SubscriptionsList onUnsubscribe={unSubscribe} subscriptions={subscriber.subscriptions} />
               </tbody>
             </DataTable>
           </SubscriptionListContainer>
--- apps/subscriber-app/src/app/store/subscription/actions.ts
+++ apps/subscriber-app/src/app/store/subscription/actions.ts
@@ -1,26 +1,83 @@
-import { Subscriber } from './models';
+import { Notification } from '@store/notifications/models';
+import { Subscriber, SubscriberChannel } from './models';
 
 export const GET_MY_SUBSCRIBER_DETAILS_SUCCESS = 'tenant/notification-service/get-mySubscriber-success';
 export const GET_MY_SUBSCRIBER_DETAILS = 'tenant/notification-service/get-mySubscriber';
+
+export const UNSUBSCRIBE_SUCCESS = 'tenant/notification-service/unsubscribe-success';
+export const UNSUBSCRIBE_FAILED = 'tenant/notification-service/unsubscribe-failed';
+export const UNSUBSCRIBE = 'tenant/notification-service/unsubscribe';
+
+export const PATCH_SUBSCRIBER = 'tenant/notification-service/patch-subscriber';
+export const PATCH_SUBSCRIBER_SUCCESS = 'tenant/notification-service/patch-subscriber-success';
 
 // =============
 // Actions Types
 // =============
 
-export type ActionTypes = GetMySubscriberActionSuccess;
+export type ActionTypes = PatchSubscriberActionSuccess | GetMySubscriberActionSuccess | UnsubscribeActionSuccess;
 export interface GetMySubscriberActionSuccess {
   type: typeof GET_MY_SUBSCRIBER_DETAILS_SUCCESS;
   payload: {
     subscriber: Subscriber;
   };
 }
+export interface UnsubscribeActionSuccess {
+  type: typeof UNSUBSCRIBE_SUCCESS;
+  payload: {
+    typeId: string;
+  };
+}
 export interface GetMySubscriberAction {
   type: typeof GET_MY_SUBSCRIBER_DETAILS;
+}
+export interface UnsubscribeAction {
+  type: typeof UNSUBSCRIBE;
+  payload: { type: string; subscriberId: string };
+}
+export interface PatchSubscriberAction {
+  type: typeof PATCH_SUBSCRIBER;
+  payload: { channels: SubscriberChannel[]; subscriberId: string };
+}
+export interface PatchSubscriberActionSuccess {
+  type: typeof PATCH_SUBSCRIBER_SUCCESS;
+  payload: {
+    subscriber: Subscriber;
+  };
 }
 
 // ==============
 // Action Methods
 // ==============
+
+export const unsubscribe = (subscriptionInfo: { type: string; subscriberId: string }): UnsubscribeAction => ({
+  type: UNSUBSCRIBE,
+  payload: {
+    ...subscriptionInfo,
+  },
+});
+
+export const UnsubscribeSuccess = (typeId: string): UnsubscribeActionSuccess => ({
+  type: UNSUBSCRIBE_SUCCESS,
+  payload: {
+    typeId,
+  },
+});
+
+export const PatchSubscriberSuccess = (subscriber: Subscriber): PatchSubscriberActionSuccess => ({
+  type: PATCH_SUBSCRIBER_SUCCESS,
+  payload: {
+    subscriber,
+  },
+});
+
+export const patchSubscriber = (channels: SubscriberChannel[], subscriberId: string): PatchSubscriberAction => ({
+  type: PATCH_SUBSCRIBER,
+  payload: {
+    channels,
+    subscriberId,
+  },
+});
 
 export const GetMySubscriberDetailsSuccess = (subscriber: Subscriber): GetMySubscriberActionSuccess => ({
   type: GET_MY_SUBSCRIBER_DETAILS_SUCCESS,
