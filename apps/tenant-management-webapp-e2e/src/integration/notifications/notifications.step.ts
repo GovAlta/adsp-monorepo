0a57cefc83211608b64c0e8fd61a4c28a928e421
Howard Zhang <howard.zhang@gov.ab.ca>
2022-01-04 17:00:48 -0700

    Add a wait before validating an event in a type due to saving an event being delayed usually

--- apps/tenant-management-webapp-e2e/src/integration/notifications/notifications.step.ts
+++ apps/tenant-management-webapp-e2e/src/integration/notifications/notifications.step.ts
@@ -155,6 +155,7 @@ When('the user clicks Add button in Add an email template page', function () {
 });
 
 Then('the user {string} the event of {string} in {string}', function (viewOrNot, event, cardTitle) {
+  cy.wait(1000); // To wait for the record to show up in the grid before validating the record existence
   let numOfMatch = 0;
   if (viewOrNot == 'views') {
     notificationsObj.notificationTypeEvents(cardTitle).then((elements) => {
