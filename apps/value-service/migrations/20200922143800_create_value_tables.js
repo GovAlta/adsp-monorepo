5d4aabff4e18ebc37e29deb5faa948ca6473da5e
Ting Zuge <ting.zuge@gov.ab.ca>
2022-01-05 14:21:35 -0700

    feat(value-service): reverting back to create extension for timescaledb.

--- apps/value-service/migrations/20200922143800_create_value_tables.js
+++ apps/value-service/migrations/20200922143800_create_value_tables.js
@@ -2,7 +2,6 @@
 
 exports.up = function (knex) {
   return knex.schema
-    .raw('ALTER EXTENSION timescaledb UPDATE;')
     .raw('CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;')
     .createTable('values', function (table) {
       table.string('namespace').notNullable();
