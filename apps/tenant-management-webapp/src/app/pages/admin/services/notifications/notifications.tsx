d681c6a3ee9139fd77ad142e39735a2fb58ff41e
Jonathan Weyermann <jonathan.weyermann@gov.ab.ca>
2022-01-11 15:19:54 -0700

    CS-1030 -> if tenant-admin is not available, show a Callout

--- apps/tenant-management-webapp/src/app/pages/admin/services/notifications/notifications.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/notifications/notifications.tsx
@@ -2,6 +2,7 @@ import { Aside, Main, Page } from '@components/Html';
 import SupportLinks from '@components/SupportLinks';
 import { Tab, Tabs } from '@components/Tabs';
 import { RootState } from '@store/index';
+import { GoACallout } from '@abgov/react-components';
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { NotificationsOverview } from './overview';
@@ -10,8 +11,13 @@ import { Subscriptions } from './subscriptions';
 import { Subscribers } from './subscribers';
 
 export const Notifications: FunctionComponent = () => {
+  const tenantAdminRole = 'tenant-admin';
   const tenantId = useSelector((state: RootState) => state.tenant?.id);
   const docBaseUrl = useSelector((state: RootState) => state.config.serviceUrls?.docServiceApiUrl);
+  const adminEmail = useSelector((state: RootState) => state.tenant.adminEmail);
+  const hasAdminRole = useSelector((state: RootState) =>
+    state.session?.resourceAccess?.['urn:ads:platform:tenant-service']?.roles?.includes(tenantAdminRole)
+  );
 
   const [activateEditState, setActivateEditState] = useState<boolean>(false);
   const [activeIndex, setActiveIndex] = useState<number>(0);
@@ -27,24 +33,48 @@ export const Notifications: FunctionComponent = () => {
     }
   }, [activeIndex]);
 
+  const calloutMessage = () => {
+    return (
+      <Main>
+        <Tabs activeIndex={activeIndex}>
+          <Tab label="Overview">
+            <NotificationsOverview setActiveEdit={activateEdit} disabled />
+          </Tab>
+        </Tabs>
+
+        <GoACallout type="important" data-testid="delete-modal">
+          <h3>Access to tenant admin app requires tenant-admin role</h3>
+          <p>
+            You require administrator role to access the admin interface and will need to contact the tenant created at{' '}
+            <a href={`mailto: ${adminEmail}`}>{adminEmail}</a>
+          </p>
+        </GoACallout>
+      </Main>
+    );
+  };
+
   return (
     <Page>
       <Main>
         <h1>Notifications</h1>
-        <Tabs activeIndex={activeIndex}>
-          <Tab label="Overview">
-            <NotificationsOverview setActiveEdit={activateEdit} />
-          </Tab>
-          <Tab label="Notification types">
-            <NotificationTypes activeEdit={activateEditState} activateEdit={activateEdit} />
-          </Tab>
-          <Tab label="Subscriptions">
-            <Subscriptions />
-          </Tab>
-          <Tab label="Subscribers">
-            <Subscribers />
-          </Tab>
-        </Tabs>
+        {hasAdminRole ? (
+          <Tabs activeIndex={activeIndex}>
+            <Tab label="Overview">
+              <NotificationsOverview setActiveEdit={activateEdit} />
+            </Tab>
+            <Tab label="Notification types">
+              <NotificationTypes activeEdit={activateEditState} activateEdit={activateEdit} />
+            </Tab>
+            <Tab label="Subscriptions">
+              <Subscriptions />
+            </Tab>
+            <Tab label="Subscribers">
+              <Subscribers />
+            </Tab>
+          </Tabs>
+        ) : (
+          calloutMessage()
+        )}
       </Main>
       <Aside>
         <h3>Helpful links</h3>
