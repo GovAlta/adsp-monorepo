5c2572e7023238b7cdb8f82dabd3472fd964a2c0
athena.chen <athena.chen@gov.ab.ca>
2022-01-19 10:04:39 -0700

    feat: Add html wrapper in notification serivce

--- libs/shared/src/utils/html.ts
+++ libs/shared/src/utils/html.ts
@@ -1,3 +1,17 @@
+import { getHeaderPreview, getFooterPreview } from '../events';
 export const hasProperHtmlWrapper = (content: string): boolean => {
-  return content.startsWith('<!doctype html>') || content.startsWith('<html>');
+  return content.trim().startsWith('<!doctype html>') || content.trim().startsWith('<html>');
+};
+export const hasHeaderAndFooter = (content: string): boolean => {
+  return content.includes('<header>') && content.includes('<footer>');
+};
+
+export const insertAt = (content: string, subString: string, position: number) =>
+  `${content.slice(0, position)}${subString}${content.slice(position)}`;
+
+export const getTemplateBody = (body: string, serviceName: string) => {
+  if (!hasProperHtmlWrapper(body)) {
+    return `<!doctype html><html>${getHeaderPreview()}${body}${getFooterPreview(serviceName)}</html>`;
+  }
+  return body;
 };
