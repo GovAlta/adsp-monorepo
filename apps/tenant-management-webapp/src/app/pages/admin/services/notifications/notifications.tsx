a409c0a0e154b96374a0fb360b929f4bcfcda11c
Subbu <subbumettu007@gmail.com>
2022-02-01 10:58:20 -0700

    feat: added copy login path for subscriber notifications and made dashboard text to link

--- apps/tenant-management-webapp/src/app/pages/admin/services/notifications/notifications.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/notifications/notifications.tsx
@@ -2,16 +2,25 @@ import { Aside, Main, Page } from '@components/Html';
 import SupportLinks from '@components/SupportLinks';
 import { Tab, Tabs } from '@components/Tabs';
 import { RootState } from '@store/index';
+import ReactTooltip from 'react-tooltip';
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { NotificationsOverview } from './overview';
 import { NotificationTypes } from './notificationTypes';
 import { Subscriptions } from './subscriptions';
 import { Subscribers } from './subscribers';
+import { GoAButton } from '@abgov/react-components';
 
 export const Notifications: FunctionComponent = () => {
   const tenantId = useSelector((state: RootState) => state.tenant?.id);
   const docBaseUrl = useSelector((state: RootState) => state.config.serviceUrls?.docServiceApiUrl);
+  const { session, subscriberWebApp } = useSelector((state: RootState) => {
+    return {
+      subscriberWebApp: state.config.serviceUrls.subscriberWebApp,
+      session: state.session,
+    };
+  });
+  const loginUrl = `${subscriberWebApp}/${session.realm}/login`;
 
   const [activateEditState, setActivateEditState] = useState<boolean>(false);
   const [activeIndex, setActiveIndex] = useState<number>(0);
@@ -19,6 +28,10 @@ export const Notifications: FunctionComponent = () => {
   const activateEdit = (edit: boolean) => {
     setActiveIndex(1);
     setActivateEditState(edit);
+  };
+
+  const _afterShow = (copyText) => {
+    navigator.clipboard.writeText(copyText);
   };
 
   useEffect(() => {
@@ -64,6 +77,18 @@ export const Notifications: FunctionComponent = () => {
           See the code
         </a>
         <SupportLinks />
+        <div className="copy-url"><a target="_blank" href={loginUrl} rel="noreferrer">{loginUrl}</a></div>
+        <GoAButton data-tip="Copied!" data-for="registerTipUrl">
+          Click to copy
+        </GoAButton>
+        <ReactTooltip
+              id="registerTipUrl"
+              place="top"
+              event="click"
+              eventOff="blur"
+              effect="solid"
+              afterShow={() => _afterShow(loginUrl)}
+            />
       </Aside>
     </Page>
   );
