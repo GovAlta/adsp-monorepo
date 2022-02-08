982eeea97ad6f100e363986fd3f4445c8a2cb234
tzuge <47162374+tzuge@users.noreply.github.com>
2022-02-08 13:43:44 -0700

    chore(tenant-management-api): cleaning up tenant model entity and repository behaviours. (#337)

--- apps/tenant-management-api/src/tenant/router/tenantV2.ts
+++ apps/tenant-management-api/src/tenant/router/tenantV2.ts
@@ -1,29 +1,44 @@
-import { adspId } from '@abgov/adsp-service-sdk';
+import { adspId, Tenant } from '@abgov/adsp-service-sdk';
 import { Router } from 'express';
 import { requirePlatformService } from '../../middleware/authentication';
 import * as tenantService from '../services/tenant';
 import { logger } from '../../middleware/logger';
-import { TenantEntity } from '../models';
-import { TenantRepository } from '..';
-
-const mapTenant = (entity: TenantEntity) => ({
-  id: `${entity.id}`,
-  realm: entity.realm,
-  adminEmail: entity.adminEmail,
-  name: entity.name,
-});
+import { TenantRepository } from '../repository';
+import { TenantCriteria } from '../types';
 
 interface TenantRouterProps {
   tenantRepository: TenantRepository;
 }
 
+function mapTenant(tenant: Tenant) {
+  return {
+    id: `${tenant.id}`,
+    realm: tenant.realm,
+    name: tenant.name,
+  };
+}
+
 export const createTenantV2Router = ({ tenantRepository }: TenantRouterProps): Router => {
   const tenantV2Router: Router = Router();
 
-  tenantV2Router.get('/tenants', requirePlatformService, async (_req, res, next) => {
+  tenantV2Router.get('/tenants', requirePlatformService, async (req, res, next) => {
     try {
+      const { name, realm, adminEmail } = req.query;
+      const criteria: TenantCriteria = {};
+      if (name) {
+        criteria.nameEquals = `${name}`;
+      }
+
+      if (realm) {
+        criteria.realmEquals = `${realm}`;
+      }
+
+      if (adminEmail) {
+        criteria.adminEmailEquals = `${adminEmail}`;
+      }
+
       // FIXME: accessing a non-injected dependency makes this hard to test
-      const tenants = await tenantService.getTenants(tenantRepository);
+      const tenants = await tenantService.getTenants(tenantRepository, criteria);
 
       res.json({
         results: tenants.map(mapTenant),
