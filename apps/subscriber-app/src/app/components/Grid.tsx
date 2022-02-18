514ab51a1b44019b16809ea5b87105b5ed546c81
Jonathan Weyermann <jonathan.weyermann@gov.ab.ca>
2022-02-17 17:31:42 -0700

    Overview Page - UI

---
+++ apps/subscriber-app/src/app/components/Grid.tsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+const Grid = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: stretch;
+`;
+
+export interface GridItemProps {
+  sm?: number;
+  md?: number;
+  lg?: number;
+  xl?: number;
+
+  hSpacing?: number;
+  vSpacing?: number;
+}
+
+const GridItem = styled.div<GridItemProps>`
+  margin-bottom: ${(props: GridItemProps) => `${props.vSpacing ?? 0}rem`};
+
+  > .goa-card {
+    height: 100% !important;
+    width: 100% !important;
+  }
+
+  > img {
+    width: 100%;
+  }
+
+  flex: 0 1
+    ${(props: GridItemProps) =>
+      props.sm === 12 || props.sm === undefined
+        ? '100%'
+        : `calc(${(100 * (props?.sm ?? 12)) / 12}% - ${props.hSpacing ?? 0}rem)`};
+
+  @media (min-width: 768px) {
+    flex-basis: ${(props: GridItemProps) =>
+      props.md === 12 ? '100%' : `calc(${(100 * (props?.md ?? props.sm ?? 12)) / 12}% - ${props.hSpacing ?? 0}rem)`};
+  }
+  @media (min-width: 1024px) {
+    flex-basis: ${(props: GridItemProps) =>
+      props.lg === 12
+        ? '100%'
+        : `calc(${(100 * (props?.lg ?? props?.md ?? props.sm ?? 12)) / 12}% - ${props.hSpacing ?? 0}rem)`};
+  }
+  @media (min-width: 1280px) {
+    flex-basis: ${(props: GridItemProps) =>
+      props.xl === 12
+        ? '100%'
+        : `calc(${(100 * (props?.xl ?? props?.md ?? props.sm ?? 12)) / 12}% - ${props.hSpacing ?? 0}rem)`};
+  }
+`;
+
+export { Grid, GridItem };
