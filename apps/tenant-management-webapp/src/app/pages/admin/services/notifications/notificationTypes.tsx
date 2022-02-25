90bcdb3e1494c041bfc5d8bb5476f7c83bc119a8
athena.chen <athena.chen@gov.ab.ca>
2022-02-25 16:43:17 -0700

    feat: add subscriber on autocomplate

--- apps/tenant-management-webapp/src/app/pages/admin/services/notifications/notificationTypes.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/notifications/notificationTypes.tsx
@@ -39,7 +39,7 @@ import {
 import { TemplateEditor } from './emailPreviewEditor/TemplateEditor';
 import { PreviewTemplate } from './emailPreviewEditor/PreviewTemplate';
 import { dynamicGeneratePayload } from '@lib/dynamicPlaceHolder';
-import { convertEventToSuggestion } from '@lib/autoComplete';
+import { convertToSuggestion } from '@lib/autoComplete';
 
 const emptyNotificationType: NotificationItem = {
   name: '',
@@ -85,14 +85,13 @@ export const NotificationTypes: FunctionComponent<ParentCompProps> = ({ activeEd
   const dispatch = useDispatch();
   const eventDefinitions = useSelector((state: RootState) => state.event.definitions);
   const eventDef = eventDefinitions[`${selectedEvent?.namespace}:${selectedEvent?.name}`];
-
   const htmlPayload = dynamicGeneratePayload(eventDef);
   const serviceName = `${selectedEvent?.namespace}:${selectedEvent?.name}`;
   const TEMPALTE_RENDER_DEBOUNCE_TIMER = 500; // ms
 
   const getEventSuggestion = () => {
     if (eventDef) {
-      return convertEventToSuggestion(eventDef);
+      return convertToSuggestion(eventDef);
     }
     return [];
   };
