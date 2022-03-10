04c45265ece0c6c8fabe9780657f13153b36c4ee
LanaDIO <lana.danilova@gov.ab.ca>
2022-03-10 15:58:39 -0500

    added Directory-service feature, page, step

--- apps/tenant-management-webapp-e2e/src/integration/service-status/service-status.step.ts
+++ apps/tenant-management-webapp-e2e/src/integration/service-status/service-status.step.ts
@@ -512,31 +512,3 @@ Then(
       });
   }
 );
-
-When('the user enters {string} as name and {string} as description fields', function (appName, description) {
-  statusObj.addApplicationNameModalField().clear().type(appName);
-  statusObj.addApplicationDescriptionModalField().clear().type(description);
-});
-
-Then('the user views the Directory service overview content {string}', function (paragraphText) {
-  statusObj.directoryOverviewContent().invoke('text').should('contain', paragraphText);
-});
-
-Then('the user views the Overview aside item {string}', function (listitem) {
-  statusObj.directoryAsideItem(listitem).should('exist');
-});
-
-Then('the user views the Overview aside item with link {string}', function (listitem) {
-  statusObj.directoryAsideItem(listitem).should('have.attr', 'href');
-});
-
-Then('the user views the service entry of {string} and {string}', function (directoryName, fileUrl) {
-  let url = '';
-  const envFileApi = fileUrl.match(/(?<={).+(?=})/g);
-  if (envFileApi == '') {
-    url = fileUrl;
-  } else {
-    url = Cypress.env(String(envFileApi));
-  }
-  statusObj.directoryTable().contains('td', directoryName).siblings().contains(url);
-});
