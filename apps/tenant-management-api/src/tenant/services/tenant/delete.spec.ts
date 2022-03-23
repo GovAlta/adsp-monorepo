04e4c71e3f4fb71d4101625ed102fdb8fa5899ea
tzuge <47162374+tzuge@users.noreply.github.com>
2022-03-23 17:18:35 -0600

    chore(tenant-management-api): more unit test coverage of tenant service. (#493)

--- apps/tenant-management-api/src/tenant/services/tenant/delete.spec.ts
+++ apps/tenant-management-api/src/tenant/services/tenant/delete.spec.ts
@@ -1,7 +1,157 @@
-import { testDelete } from './delete';
+import KeycloakAdminClient from '@keycloak/keycloak-admin-client';
+import * as keycloak from '../../../keycloak';
+import { TenantRepository } from '../../repository';
+import { deleteTenant } from './delete';
 
-describe('Tenant:Services:delete', () => {
-  test('Test stub', async () => {
-    expect(testDelete()).toBe(true);
+jest.mock('../../../keycloak');
+const keycloakMock = keycloak as jest.Mocked<typeof keycloak>;
+
+describe('deleteTenant', () => {
+  const repositoryMock = {
+    delete: jest.fn(),
+  };
+
+  beforeEach(() => {
+    repositoryMock.delete.mockReset();
+  });
+
+  it('can delete tenant', async () => {
+    const realm = 'my-realm';
+    const brokerClientId = `broker-${realm}`;
+    const keycloakClientMock = {
+      realms: {
+        del: jest.fn(),
+      },
+      clients: {
+        find: jest.fn(),
+        del: jest.fn(),
+      },
+    };
+    keycloakMock.createkcAdminClient.mockResolvedValue(keycloakClientMock as unknown as KeycloakAdminClient);
+    keycloakClientMock.clients.find.mockResolvedValueOnce([{ id: 'broker-123', clientId: brokerClientId }]);
+    const result = await deleteTenant(repositoryMock as unknown as TenantRepository, realm);
+    expect(result).toMatchObject({
+      keycloakRealm: expect.objectContaining({ isDeleted: true }),
+      IdPBrokerClient: expect.objectContaining({ isDeleted: true }),
+      db: expect.objectContaining({ isDeleted: true }),
+    });
+    expect(keycloakClientMock.realms.del).toHaveBeenCalledWith(expect.objectContaining({ realm }));
+    expect(keycloakClientMock.clients.find).toHaveBeenCalledWith(expect.objectContaining({ realm: 'core' }));
+    expect(keycloakClientMock.clients.del).toHaveBeenCalledWith(
+      expect.objectContaining({ realm: 'core', id: 'broker-123' })
+    );
+    expect(repositoryMock.delete).toHaveBeenCalledWith(realm);
+  });
+
+  it('can handle realm delete error', async () => {
+    const realm = 'my-realm';
+    const brokerClientId = `broker-${realm}`;
+    const keycloakClientMock = {
+      realms: {
+        del: jest.fn(),
+      },
+      clients: {
+        find: jest.fn(),
+        del: jest.fn(),
+      },
+    };
+    keycloakMock.createkcAdminClient.mockResolvedValue(keycloakClientMock as unknown as KeycloakAdminClient);
+    keycloakClientMock.realms.del.mockRejectedValueOnce(new Error('oh noes!'));
+    keycloakClientMock.clients.find.mockResolvedValueOnce([{ id: 'broker-123', clientId: brokerClientId }]);
+    const result = await deleteTenant(repositoryMock as unknown as TenantRepository, realm);
+    expect(result).toMatchObject({
+      keycloakRealm: expect.objectContaining({ isDeleted: false, errors: expect.any(Array) }),
+      IdPBrokerClient: expect.objectContaining({ isDeleted: true }),
+      db: expect.objectContaining({ isDeleted: true }),
+    });
+    expect(keycloakClientMock.realms.del).toHaveBeenCalledWith(expect.objectContaining({ realm }));
+    expect(keycloakClientMock.clients.find).toHaveBeenCalledWith(expect.objectContaining({ realm: 'core' }));
+    expect(keycloakClientMock.clients.del).toHaveBeenCalledWith(
+      expect.objectContaining({ realm: 'core', id: 'broker-123' })
+    );
+    expect(repositoryMock.delete).toHaveBeenCalledWith(realm);
+  });
+
+  it('can handle broker client delete error', async () => {
+    const realm = 'my-realm';
+    const brokerClientId = `broker-${realm}`;
+    const keycloakClientMock = {
+      realms: {
+        del: jest.fn(),
+      },
+      clients: {
+        find: jest.fn(),
+        del: jest.fn(),
+      },
+    };
+    keycloakMock.createkcAdminClient.mockResolvedValue(keycloakClientMock as unknown as KeycloakAdminClient);
+    keycloakClientMock.clients.find.mockResolvedValueOnce([{ id: 'broker-123', clientId: brokerClientId }]);
+    keycloakClientMock.clients.del.mockRejectedValueOnce(new Error('oh noes!'));
+    const result = await deleteTenant(repositoryMock as unknown as TenantRepository, realm);
+    expect(result).toMatchObject({
+      keycloakRealm: expect.objectContaining({ isDeleted: true }),
+      IdPBrokerClient: expect.objectContaining({ isDeleted: false, errors: expect.any(Array) }),
+      db: expect.objectContaining({ isDeleted: true }),
+    });
+    expect(keycloakClientMock.realms.del).toHaveBeenCalledWith(expect.objectContaining({ realm }));
+    expect(keycloakClientMock.clients.find).toHaveBeenCalledWith(expect.objectContaining({ realm: 'core' }));
+    expect(keycloakClientMock.clients.del).toHaveBeenCalledWith(
+      expect.objectContaining({ realm: 'core', id: 'broker-123' })
+    );
+    expect(repositoryMock.delete).toHaveBeenCalledWith(realm);
+  });
+
+  it('can handle broker client not found', async () => {
+    const realm = 'my-realm';
+    const keycloakClientMock = {
+      realms: {
+        del: jest.fn(),
+      },
+      clients: {
+        find: jest.fn(),
+        del: jest.fn(),
+      },
+    };
+    keycloakMock.createkcAdminClient.mockResolvedValue(keycloakClientMock as unknown as KeycloakAdminClient);
+    keycloakClientMock.clients.find.mockResolvedValueOnce([]);
+    const result = await deleteTenant(repositoryMock as unknown as TenantRepository, realm);
+    expect(result).toMatchObject({
+      keycloakRealm: expect.objectContaining({ isDeleted: true }),
+      IdPBrokerClient: expect.objectContaining({ isDeleted: true }),
+      db: expect.objectContaining({ isDeleted: true }),
+    });
+    expect(keycloakClientMock.realms.del).toHaveBeenCalledWith(expect.objectContaining({ realm }));
+    expect(keycloakClientMock.clients.find).toHaveBeenCalledWith(expect.objectContaining({ realm: 'core' }));
+    expect(keycloakClientMock.clients.del).not.toHaveBeenCalled();
+    expect(repositoryMock.delete).toHaveBeenCalledWith(realm);
+  });
+
+  it('can handle tenant delete error', async () => {
+    const realm = 'my-realm';
+    const brokerClientId = `broker-${realm}`;
+    const keycloakClientMock = {
+      realms: {
+        del: jest.fn(),
+      },
+      clients: {
+        find: jest.fn(),
+        del: jest.fn(),
+      },
+    };
+    keycloakMock.createkcAdminClient.mockResolvedValue(keycloakClientMock as unknown as KeycloakAdminClient);
+    keycloakClientMock.clients.find.mockResolvedValueOnce([{ id: 'broker-123', clientId: brokerClientId }]);
+    repositoryMock.delete.mockRejectedValueOnce(new Error('oh noes!'));
+    const result = await deleteTenant(repositoryMock as unknown as TenantRepository, realm);
+    expect(result).toMatchObject({
+      keycloakRealm: expect.objectContaining({ isDeleted: true }),
+      IdPBrokerClient: expect.objectContaining({ isDeleted: true }),
+      db: expect.objectContaining({ isDeleted: false, errors: expect.any(Array) }),
+    });
+    expect(keycloakClientMock.realms.del).toHaveBeenCalledWith(expect.objectContaining({ realm }));
+    expect(keycloakClientMock.clients.find).toHaveBeenCalledWith(expect.objectContaining({ realm: 'core' }));
+    expect(keycloakClientMock.clients.del).toHaveBeenCalledWith(
+      expect.objectContaining({ realm: 'core', id: 'broker-123' })
+    );
+    expect(repositoryMock.delete).toHaveBeenCalledWith(realm);
   });
 });
