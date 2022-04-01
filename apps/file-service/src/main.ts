6a094cbb153d6dcbbe01c77eb1e402194a67d517
Ting Zuge <ting.zuge@gov.ab.ca>
2022-03-31 20:21:11 -0600

    chore: adding name and description to service metadata in platform services.
    Also adjusting typing on domain event to make tenantId optional.

--- apps/file-service/src/main.ts
+++ apps/file-service/src/main.ts
@@ -165,6 +165,8 @@ async function initializeApp(): Promise<express.Application> {
   app.get('/', async (req, res) => {
     const rootUrl = new URL(`${req.protocol}://${req.get('host')}`);
     res.json({
+      name: 'File service',
+      description: 'Service for upload and download of files.',
       _links: {
         self: { href: new URL(req.originalUrl, rootUrl).href },
         health: { href: new URL('/health', rootUrl).href },
