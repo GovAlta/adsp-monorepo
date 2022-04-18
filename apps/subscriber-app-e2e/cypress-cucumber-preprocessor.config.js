7111945141df08738f30e51e1fb7764d8f98c3b6
Howard Zhang <howard.zhang@gov.ab.ca>
2022-04-18 16:33:48 -0600

    Set up subscriber-app-e2e

---
+++ apps/subscriber-app-e2e/cypress-cucumber-preprocessor.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const stepDefinitionsPath = path.resolve(process.cwd(), './src/integration');
+const outputFolder = path.resolve(process.cwd(), '../../dist/cypress/apps/subscriber-app-e2e/cucumber-json');
+
+module.exports = {
+  nonGlobalStepDefinitions: true,
+  stepDefinitions: stepDefinitionsPath,
+  cucumberJson: {
+    generate: true,
+    outputFolder: outputFolder,
+    filePrefix: '',
+    fileSuffix: '.cucumber',
+  },
+};
