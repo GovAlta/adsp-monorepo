27200b4dfebdc3dad7ce693c78bef93b5ee060f6
tzuge <47162374+tzuge@users.noreply.github.com>
2022-04-27 11:37:45 -0600

    feat(directory-service): stubbing a new directory service that will provide the directory v2 API.

---
+++ apps/directory-service/webpack.config.js
@@ -0,0 +1,15 @@
+const SwaggerJSDocWebpackPlugin = require('swagger-jsdoc-webpack-plugin');
+const swaggerDefinition = require('./swagger.config');
+
+module.exports = (config) => {
+  return {
+    ...config,
+    plugins: [
+      ...config.plugins,
+      new SwaggerJSDocWebpackPlugin({
+        swaggerDefinition,
+        apis: ['apps/directory-service/src/**/*.ts', 'apps/directory-service/src/**/*.swagger.yml'],
+      }),
+    ],
+  };
+};
