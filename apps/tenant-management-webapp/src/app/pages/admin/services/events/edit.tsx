ab5e9962e67880c982e57c0f0ab8c572f8161488
Roy Styan <rstyan@telus.net>
2022-05-02 11:19:49 -0600

    fix(directory-service): fix input cleansing algorithm (#639)
    * fix(directory-service): fix input cleansing algorithm
    
    * fix(directory-service): Renaming check input concepts
    
    * fix(directory-service): Name changes to reflect usage

--- apps/tenant-management-webapp/src/app/pages/admin/services/events/edit.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/events/edit.tsx
@@ -4,12 +4,8 @@ import type { EventDefinition } from '@store/event/models';
 import { GoAButton } from '@abgov/react-components';
 import { GoAModal, GoAModalActions, GoAModalContent, GoAModalTitle } from '@abgov/react-components/experimental';
 import { GoAForm, GoAFormItem } from '@abgov/react-components/experimental';
-import {
-  reportCleansing,
-  ReactCleansingReporter,
-  serviceItemNameCleanser,
-  serviceNamespaceCleanser,
-} from '@lib/inputCleansers';
+import { wordCheck, characterCheck, validationPattern, checkInput, isNotEmptyCheck } from '@lib/checkInput';
+import { ReactInputHandler } from '@lib/ReactInputHandler';
 import { updateEventDefinition } from '@store/event/actions';
 import { useDispatch } from 'react-redux';
 
@@ -33,11 +29,13 @@ export const EventDefinitionModalForm: FunctionComponent<EventDefinitionFormProp
   const [definition, setDefinition] = useState<EventDefinition>(initialValue);
   const [errors, setErrors] = useState<Record<string, string>>({});
   const dispatch = useDispatch();
-  const reporter = new ReactCleansingReporter(errors, setErrors);
+
   const hasFormErrors = () => {
     return Object.keys(errors).length !== 0;
   };
-  const forbidden = coreNamespaces.concat('platform');
+  const forbiddenWords = coreNamespaces.concat('platform');
+  const checkForConflicts = wordCheck(forbiddenWords);
+  const checkForBadChars = characterCheck(validationPattern.mixedArrowCase);
 
   return (
     <GoAModal testId="definition-form" isOpen={open}>
@@ -54,8 +52,12 @@ export const EventDefinitionModalForm: FunctionComponent<EventDefinitionFormProp
               data-testid="form-namespace"
               aria-label="nameSpace"
               onChange={(e) => {
-                const notice = serviceNamespaceCleanser(e.target.value, 'namespace', forbidden);
-                reportCleansing(notice, 'namespace', reporter);
+                const errorHandler = new ReactInputHandler(errors, setErrors, 'namespace');
+                checkInput(
+                  e.target.value,
+                  [checkForConflicts, checkForBadChars, isNotEmptyCheck('namespace')],
+                  errorHandler
+                );
                 setDefinition({ ...definition, namespace: e.target.value });
               }}
             />
@@ -70,7 +72,8 @@ export const EventDefinitionModalForm: FunctionComponent<EventDefinitionFormProp
               data-testid="form-name"
               aria-label="name"
               onChange={(e) => {
-                reportCleansing(serviceItemNameCleanser(e.target.value), 'name', reporter);
+                const errorHandler = new ReactInputHandler(errors, setErrors, 'name');
+                checkInput(e.target.value, [checkForBadChars, isNotEmptyCheck('name')], errorHandler);
                 setDefinition({ ...definition, name: e.target.value });
               }}
             />
