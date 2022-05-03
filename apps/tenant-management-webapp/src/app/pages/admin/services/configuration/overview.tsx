73dad306ab4d63aa0e90a48f5ce1de93bdb4e9bb
Roy Styan <rstyan@telus.net>
2022-05-03 10:37:56 -0600

    fix(configuration-service): On add definition, switch tabs first (#648)
    * fix(configuration-service): On add definition, switch tabs first
    
    * fix(configuration-service): Ensure modal doesn't auto-open after switching tabs

--- apps/tenant-management-webapp/src/app/pages/admin/services/configuration/overview.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/configuration/overview.tsx
@@ -1,61 +1,38 @@
-import React, { FunctionComponent, useEffect, useState } from 'react';
-import { AddEditConfigDefinition } from './definitions/addEditDefinition';
+import React, { FunctionComponent, useEffect } from 'react';
 import { GoAButton } from '@abgov/react-components';
-import { defaultConfigDefinition } from '@store/configuration/model';
-import { useDispatch, useSelector } from 'react-redux';
-import { updateConfigurationDefinition } from '@store/configuration/action';
-import { RootState } from '@store/index';
 
 interface ConfigurationOverviewProps {
-  updateActiveIndex?: (index: number) => void;
+  setActiveEdit: (boolean) => void;
+  setActiveIndex: (number) => void;
+  disabled?: boolean;
 }
-export const ConfigurationOverview: FunctionComponent<ConfigurationOverviewProps> = ({ updateActiveIndex }) => {
-  const [openAddDefinition, setOpenAddDefinition] = useState(false);
-  const [selectedDefinition, setSelectedDefinition] = useState(defaultConfigDefinition);
-  const isAddedFromOverviewPage = useSelector((state: RootState) => state.configuration?.isAddedFromOverviewPage);
+export const ConfigurationOverview: FunctionComponent<ConfigurationOverviewProps> = (props) => {
+  const { setActiveEdit, setActiveIndex, disabled } = props;
 
-  const dispatch = useDispatch();
-
-  // set index to 0(overview tab) when switching back to it
   useEffect(() => {
-    updateActiveIndex(0);
+    setActiveEdit(false);
+    setActiveIndex(0);
   }, []);
-
-  const reset = () => {
-    setOpenAddDefinition(false);
-    setSelectedDefinition(defaultConfigDefinition);
-  };
-
-  // If a definition is added on overview page, then switch to definition tab
-  if (isAddedFromOverviewPage) {
-    updateActiveIndex(1);
-  }
 
   return (
     <div>
-      <AddEditConfigDefinition
-        open={openAddDefinition}
-        onClose={reset}
-        isEdit={false}
-        initialValue={selectedDefinition}
-        onSave={(definition) => {
-          dispatch(updateConfigurationDefinition(definition, true));
-        }}
-      />
-      <p>
-        The configuration service provides a generic json document store for storage and revisioning of infrequently
-        changing configuration. Store configuration against namespace and name keys, and optionally define configuration
-        schemas for write validation.
-      </p>
-      <br />
-      <GoAButton
-        data-testid="add-definition"
-        onClick={() => {
-          setOpenAddDefinition(true);
-        }}
-      >
-        Add definition
-      </GoAButton>
+      <section>
+        <p>
+          The configuration service provides a generic json document store for storage and revisioning of infrequently
+          changing configuration. Store configuration against namespace and name keys, and optionally define
+          configuration schemas for write validation.
+        </p>
+        <br />
+        <GoAButton
+          data-testid="add-definition"
+          disabled={disabled}
+          onClick={() => {
+            setActiveEdit(true);
+          }}
+        >
+          Add definition
+        </GoAButton>
+      </section>
     </div>
   );
 };
