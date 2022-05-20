cf971290ed8ccd15bd9a383418119ec5c5e5e0f7
Ting Zuge <ting.zuge@gov.ab.ca>
2022-05-20 13:34:51 -0600

    feat(adsp-service-sdk): adding registration for value definitions.
    Also adding more documentation around SDK interfaces.

--- libs/adsp-service-sdk/src/registration/index.ts
+++ libs/adsp-service-sdk/src/registration/index.ts
@@ -6,6 +6,7 @@ import { DomainEventDefinition } from '../event';
 import { FileType } from '../file';
 import { Stream } from '../push';
 import { AdspId } from '../utils';
+import { ValueDefinition } from '../value';
 import { ServiceRegistrarImpl } from './registration';
 
 export interface ServiceRole {
@@ -16,24 +17,111 @@ export interface ServiceRole {
 
 export interface ServiceRegistration {
   /** Service ID: ADSP ID of the service. */
+
+  /**
+   * Service ID: ADSP ID of the service.
+   *
+   * The service ID is an ADSP URN that uniquely identifies the service being registered.
+   * Associated configuration will be stored against the namespace and service in the URN.
+   *
+   * This value is also used as the client ID for the service.
+   *
+   * @type {AdspId}
+   * @memberof ServiceRegistration
+   */
   serviceId: AdspId;
-  /** Display Name: Human friendly name for the service.. */
+  /**
+   * Display name: Human friendly name for the service.
+   *
+   * @type {string}
+   * @memberof ServiceRegistration
+   */
   displayName: string;
-  /** Description: Description of the service.. */
+  /**
+   * Description: Description of the service.
+   *
+   * @type {string}
+   * @memberof ServiceRegistration
+   */
   description: string;
-  /** Roles: Roles for tenant access of the service. */
+  /**
+   * Roles: Service roles that control access into the service capabilities.
+   *
+   * For platform services, these roles are automatically created under the service client
+   * when a new tenant is created.
+   *
+   * @type {((string | ServiceRole)[])}
+   * @memberof ServiceRegistration
+   */
   roles?: (string | ServiceRole)[];
-  /** Configuration Schema: JSON schema for tenant configuration of the service. */
+  /**
+   * Configuration schema: JSON schema for configuration of the service.
+   *
+   * The configuration schema provides a write schema for service configuration in the
+   * configuration service. Service associated configuration is stored at namespace and
+   * (service) name.
+   *
+   * @type {Record<string, unknown>}
+   * @memberof ServiceRegistration
+   */
   configurationSchema?: Record<string, unknown>;
-  /** Events: Domain events of the service. */
+  /**
+   * Events: Definitions of domain events of the service.
+   *
+   * Domain events represent domain significant occurrences like state transitions.
+   * Events are recorded to the event log and can trigger side effects such as notifications.
+   *
+   * @type {DomainEventDefinition[]}
+   * @memberof ServiceRegistration
+   */
   events?: DomainEventDefinition[];
-  /** File Types: File types of the service. */
-  fileTypes?: FileType[];
-  /** Event Streams: Push mode event streams of the service. */
+  /**
+   * Event streams: Push mode event streams of the service.
+   *
+   * Streams make sets of events available over push mode endpoints in push service.
+   * Clients can connect to streams over socket.io or Server side events (SSE).
+   *
+   * @type {Stream[]}
+   * @memberof ServiceRegistration
+   */
   eventStreams?: Stream[];
-  /** Notifications: Subscribable notification types of the service. */
+  /**
+   * File types: File types of the service.
+   *
+   * File types represent sets of files with specific access rules; i.e. the roles allowed to
+   * upload and download the files of the type.
+   *
+   * @type {FileType[]}
+   * @memberof ServiceRegistration
+   */
+  fileTypes?: FileType[];
+  /**
+   * Notifications: Subscribable notification types of the service.
+   *
+   * Notification types represent subscribable collection of notifications. Each type can include
+   * multiple notifications with their own templates and triggered by specific events.
+   *
+   * @type {NotificationType[]}
+   * @memberof ServiceRegistration
+   */
   notifications?: NotificationType[];
-  /** Service Configurations: Additional configurations for services to apply at registration. */
+  /**
+   * Values: Definitions of time-series values of the service.
+   *
+   * @type {ValueDefinition[]}
+   * @memberof ServiceRegistration
+   */
+  values?: ValueDefinition[];
+  /**
+   * Service configurations: Additional configurations for services to apply at registration.
+   *
+   * This property can be used to configuration services without a defined registration property.
+   * Configuration for each entry is applied to the configuration service at the namespace and
+   * (service) name.
+   *
+   * @type {{ serviceId: AdspId; configuration: Record<string, unknown> }[]}
+   * @memberof ServiceRegistration
+   */
   serviceConfigurations?: { serviceId: AdspId; configuration: Record<string, unknown> }[];
 }
 
