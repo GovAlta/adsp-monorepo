a2ddecf1a3084f77664e0b1c2edf80c29c62a634
Subbu <subbumettu007@gmail.com>
2022-05-24 15:24:07 -0600

    fix: tweaked actions column heading position

--- apps/tenant-management-webapp/src/app/pages/admin/services/events/stream/streamTable.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/events/stream/streamTable.tsx
@@ -1,7 +1,7 @@
 import DataTable from '@components/DataTable';
 import React from 'react';
 import { Streams } from '@store/stream/models';
-import { TableWrapper } from './styleComponents';
+import { TableDiv, TableWrapper } from './styleComponents';
 import { renderNoItem } from '@components/NoItem';
 import { StreamTableItem } from './streamTableItem';
 interface StreamTableProps {
@@ -22,43 +22,45 @@ export const StreamTable = ({
   const hasContent = Object.entries(streams).length > 0;
   return (
     <TableWrapper key={`${namespace}-stream-table`}>
-      <DataTable key={`${namespace}-stream-table`} data-testid={`${namespace}-stream-table`}>
-        <thead data-testid="stream-table-header">
-          <tr>
-            <th id="stream-service-name" data-testid="stream-table-header-name">
-              Name
-            </th>
-
-            <th id="stream-service-subscriber-roles" data-testid="stream-table-header-subscriber-roles">
-              Subscriber roles
-            </th>
-
-            <th id="stream-service-actions" data-testid="stream-table-header-actions">
-              Action
-            </th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {hasContent &&
-            Object.entries(streams).map(([id, stream]) => {
-              return (
-                <StreamTableItem
-                  onDelete={onDelete}
-                  onEdit={onEdit}
-                  isTenantSpecificStream={isTenantSpecificStream}
-                  key={id}
-                  stream={stream}
-                />
-              );
-            })}
-          {!hasContent && (
+      <TableDiv key={`${namespace}-stream-table`}>
+        <DataTable key={`${namespace}-stream-table`} data-testid={`${namespace}-stream-table`}>
+          <thead data-testid="stream-table-header">
             <tr>
-              <td colSpan={3}>{renderNoItem('stream', true)}</td>
+              <th id="stream-service-name" data-testid="stream-table-header-name">
+                Name
+              </th>
+
+              <th id="stream-service-subscriber-roles" data-testid="stream-table-header-subscriber-roles">
+                Subscriber roles
+              </th>
+
+              <th id="stream-service-actions" data-testid="stream-table-header-actions">
+                Action
+              </th>
             </tr>
-          )}
-        </tbody>
-      </DataTable>
+          </thead>
+
+          <tbody>
+            {hasContent &&
+              Object.entries(streams).map(([id, stream]) => {
+                return (
+                  <StreamTableItem
+                    onDelete={onDelete}
+                    onEdit={onEdit}
+                    isTenantSpecificStream={isTenantSpecificStream}
+                    key={id}
+                    stream={stream}
+                  />
+                );
+              })}
+            {!hasContent && (
+              <tr>
+                <td colSpan={3}>{renderNoItem('stream', true)}</td>
+              </tr>
+            )}
+          </tbody>
+        </DataTable>
+      </TableDiv>
     </TableWrapper>
   );
 };
