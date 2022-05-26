f6c9aa84d736f36eb0dbdeac2ae566a277b86101
Paul.Li <paul.li@gov.ab.ca>
2022-05-26 15:29:59 -0600

    feat: update the servie role tab

--- apps/tenant-management-webapp/src/app/pages/admin/services/access/serviceRoleList.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/access/serviceRoleList.tsx
@@ -15,7 +15,7 @@ export const ServiceRoleList = ({ roles }: ServiceRoleListProps): JSX.Element =>
           <tr>
             <th>Name</th>
             <th>Description</th>
-            <th>Composite role?</th>
+            <th>Part of tenant-admin composite</th>
           </tr>
         </thead>
         <tbody>
