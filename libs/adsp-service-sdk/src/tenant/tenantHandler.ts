a4f67778da5c156c59b95cb3dcc7f1ee84ea52af
Ting Zuge <ting.zuge@gov.ab.ca>
2022-05-28 17:00:26 -0600

    feat(adsp-service-sdk): adding export of metrics value definition and adding benching of sdk handlers.

--- libs/adsp-service-sdk/src/tenant/tenantHandler.ts
+++ libs/adsp-service-sdk/src/tenant/tenantHandler.ts
@@ -1,10 +1,12 @@
 import type { RequestHandler } from 'express';
+import { benchmark } from '../metrics';
 import { AdspId } from '../utils';
 import { TenantService } from './tenantService';
 
 export const createTenantHandler =
   (service: TenantService): RequestHandler =>
   async (req, _res, next) => {
+    benchmark(req, 'get-tenant-time');
     const { tenantId: tenantIdValue } = req.query;
     const tenantId = req.user?.isCore && tenantIdValue ? AdspId.parse(tenantIdValue as string) : req.user?.tenantId;
     if (tenantId) {
@@ -16,5 +18,6 @@ export const createTenantHandler =
         return;
       }
     }
+    benchmark(req, 'get-tenant-time');
     next();
   };
