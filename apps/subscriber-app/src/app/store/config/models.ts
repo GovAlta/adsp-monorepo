e981a0241d3fe1ebe72cd3e364c5f01ade001bb2
Ting Zuge <ting.zuge@gov.ab.ca>
2022-06-15 10:03:09 -0600

    chore(tenant-management-webapp, subscriber-app): updating tenant app to use v2 for some requests and removing parts of config json that are no longer used.

--- apps/subscriber-app/src/app/store/config/models.ts
+++ apps/subscriber-app/src/app/store/config/models.ts
@@ -1,14 +1,3 @@
-export interface TenantApi {
-  host: string;
-  endpoints: {
-    spaceAdmin: string;
-    tenantNameByRealm: string;
-    tenantByName: string;
-    tenantByEmail: string;
-    tenantConfig: string;
-  };
-}
-
 export interface KeycloakApi {
   realm: string;
   url: string;
@@ -16,6 +5,7 @@ export interface KeycloakApi {
   checkLoginIframe?: boolean;
   flow?: string;
 }
+
 export interface ServiceUrls {
   eventServiceApiUrl: string;
   notificationServiceUrl: string;
@@ -34,7 +24,6 @@ export interface ServiceUrls {
 
 export interface ConfigState {
   keycloakApi?: KeycloakApi;
-  tenantApi?: TenantApi;
   serviceUrls?: ServiceUrls;
 }
 
--- apps/tenant-management-webapp/src/app/store/config/models.ts
+++ apps/tenant-management-webapp/src/app/store/config/models.ts
@@ -1,14 +1,5 @@
 export interface TenantApi {
   host: string;
-  endpoints: {
-    spaceAdmin: string;
-    createTenant: string;
-    tenantNameByRealm: string;
-    tenantByName: string;
-    tenantByEmail: string;
-    tenantConfig: string;
-    directory: string;
-  };
 }
 
 export interface KeycloakApi {
@@ -18,14 +9,14 @@ export interface KeycloakApi {
   checkLoginIframe?: boolean;
   flow?: string;
 }
+
 export interface FileApi {
   host: string;
   endpoints: {
-    spaceAdmin: string;
-    fileTypeAdmin: string;
     fileAdmin: string;
   };
 }
+
 export interface ServiceUrls {
   subscriberWebApp: string;
   eventServiceApiUrl: string;
