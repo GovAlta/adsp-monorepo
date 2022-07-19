37f739c42e89b8deb316450e2e4124ea5322b618
Subbu <subbumettu007@gmail.com>
2022-07-19 15:55:35 -0600

    fix: added sorting on role names

--- apps/tenant-management-webapp/src/app/store/sharedSelectors/roles.ts
+++ apps/tenant-management-webapp/src/app/store/sharedSelectors/roles.ts
@@ -6,8 +6,19 @@ const createDeepEqualSelector = createSelectorCreator(defaultMemoize, isEqual);
 
 // selector that returns realm roles and tenant clients and its roles
 export const tenantRolesAndClients = createDeepEqualSelector(
-  (state: RootState) => state.tenant?.realmRoles,
-  (state: RootState) => state.serviceRoles?.keycloak,
+  (state: RootState) => {
+    return state.tenant?.realmRoles?.sort((a, b) => a.name.localeCompare(b.name));
+  },
+  (state: RootState) => {
+    const clients = state.serviceRoles?.keycloak;
+    const keys = clients ? Object.keys(clients) : [];
+    keys.forEach((key: string) => {
+      for (const key of keys) {
+        clients[key].roles = clients[key]?.roles?.sort((a, b) => a.role.localeCompare(b.role));
+      }
+    });
+    return clients;
+  },
   (realmRoles, tenantClients) => {
     return {
       realmRoles,
