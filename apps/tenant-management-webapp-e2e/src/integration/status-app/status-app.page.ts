39b9d0223882598f8ad16732ac4bb1f65356862c
LanaDIO <lana.danilova@gov.ab.ca>
2022-07-25 19:21:01 -0400

    moved objects

--- apps/tenant-management-webapp-e2e/src/integration/status-app/status-app.page.ts
+++ apps/tenant-management-webapp-e2e/src/integration/status-app/status-app.page.ts
@@ -1,6 +1,10 @@
 class StatusAppPage {
-  dashboardTitle() {
-    return cy.get('.name');
+  statusNotificationPleaseContact() {
+    return cy.xpath('//*[@class="section-vs"]/a');
+  }
+
+  statusNotificationSignupDescription() {
+    return cy.xpath('//h3[text()="Sign up for notifications"]/following-sibling::div/a');
   }
 }
 export default StatusAppPage;
