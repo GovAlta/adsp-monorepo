abc81ae7e06eeb2911e7ce41d57c09c4d32c6dc8
LanaDIO <lana.danilova@gov.ab.ca>
2022-07-27 13:29:06 -0400

    added url and tenant varialbe

--- apps/tenant-management-webapp-e2e/src/integration/status-app/status-app.step.ts
+++ apps/tenant-management-webapp-e2e/src/integration/status-app/status-app.step.ts
@@ -1,21 +1,28 @@
-import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { Then } from 'cypress-cucumber-preprocessor/steps';
 import StatusAppPage from './status-app.page';
 
 const statusAppObj = new StatusAppPage();
 
-Then('the public status app displays {string} as support email', function (email) {
-  cy.visit('statusAppUrl');
-  cy.wait(3000);
-  statusAppObj
-    .statusNotificationPleaseContact()
-    .should('have.attr', 'href')
-    .then((href) => {
-      expect(href).to.contain(email);
-    });
-  statusAppObj
-    .statusNotificationSignupDescription()
-    .should('have.attr', 'href')
-    .then((href) => {
-      expect(href).to.contain(email);
-    });
-});
+Then(
+  'the user should be able to view {string} as support email in the status app for {string} tenant',
+  function (email, tenant) {
+    const urlToTenantLogin = Cypress.env().statusAppUrl + '/' + tenant;
+    cy.visit(urlToTenantLogin);
+    cy.wait(3000);
+    statusAppObj.statusNotificationPleaseContact();
+    cy.contains(tenant, { matchCase: false });
+
+    statusAppObj
+      .statusNotificationPleaseContact()
+      .should('have.attr', 'href')
+      .then((href) => {
+        expect(href).to.contain(email);
+      });
+    statusAppObj
+      .statusNotificationSignupDescription()
+      .should('have.attr', 'href')
+      .then((href) => {
+        expect(href).to.contain(email);
+      });
+  }
+);
