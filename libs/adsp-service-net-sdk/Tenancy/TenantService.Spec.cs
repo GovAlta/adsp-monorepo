7c101baf9d1b1f113ca9eced0f9d8e86eff49bfc
Ting Zuge <ting.zuge@gov.ab.ca>
2022-08-02 14:42:55 -0600

    chore(adsp-service-net-sdk): adding ReportGenerator dependency for unit test code coverage reporting.

---
+++ libs/adsp-service-net-sdk/Tenancy/TenantService.Spec.cs
@@ -0,0 +1,101 @@
+using Adsp.Sdk.Access;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using RestSharp;
+using RichardSzalay.MockHttp;
+using Xunit;
+
+namespace Adsp.Sdk.Tenancy;
+public class TenantServiceTests
+{
+  [Fact]
+  public void CanCreateTenantService()
+  {
+    var logger = Mock.Of<ILogger<TenantService>>();
+    var cache = Mock.Of<IMemoryCache>();
+    var serviceDirectory = Mock.Of<IServiceDirectory>();
+    var tokenProvider = Mock.Of<ITokenProvider>();
+    var client = Mock.Of<RestClient>();
+
+    using var middleware = new TenantService(logger, cache, serviceDirectory, tokenProvider, client);
+    middleware.Should().NotBeNull();
+  }
+
+  [Fact]
+  public async Task CanGetTenants()
+  {
+    var logger = Mock.Of<ILogger<TenantService>>();
+    var cache = new MemoryCache(new MemoryCacheOptions());
+    var serviceDirectory = new Mock<IServiceDirectory>();
+    var tokenProvider = new Mock<ITokenProvider>();
+
+    serviceDirectory
+      .Setup((d) => d.GetServiceUrl(AdspId.Parse("urn:ads:platform:tenant-service:v2")))
+      .ReturnsAsync(new Uri("https://tenant-service/tenants/v2"));
+
+    tokenProvider.Setup((t) => t.GetAccessToken()).ReturnsAsync("token");
+
+    var tenantId = AdspId.Parse("urn:ads:platform:tenant-service:v2:/tenants/test");
+
+    var requestUrl = new Uri("https://tenant-service/tenants/v2/tenants");
+    using var mockHttp = new MockHttpMessageHandler();
+    mockHttp
+      .When(HttpMethod.Get, requestUrl.AbsoluteUri)
+      .Respond(
+        "application/json",
+        "{\"results\":[{\"id\": \"urn:ads:platform:tenant-service:v2:/tenants/test\", \"name\": \"test\", \"realm\": \"test\", \"adminEmail\": \"tester@test.co\" }]}"
+      );
+
+    using var client = new RestClient(
+      new RestClientOptions
+      {
+        BaseUrl = requestUrl,
+        ConfigureMessageHandler = _ => mockHttp
+      }
+    );
+    using var middleware = new TenantService(logger, cache, serviceDirectory.Object, tokenProvider.Object, client);
+    var tenants = await middleware.GetTenants();
+    tenants.Should().Contain((t) => t.Id!.Equals(tenantId));
+  }
+
+  [Fact]
+  public async Task CanGetTenant()
+  {
+    var logger = Mock.Of<ILogger<TenantService>>();
+    var cache = new MemoryCache(new MemoryCacheOptions());
+    var serviceDirectory = new Mock<IServiceDirectory>();
+    var tokenProvider = new Mock<ITokenProvider>();
+
+    serviceDirectory
+      .Setup((d) => d.GetServiceUrl(AdspId.Parse("urn:ads:platform:tenant-service:v2")))
+      .ReturnsAsync(new Uri("https://tenant-service/tenants/v2"));
+
+    tokenProvider.Setup((t) => t.GetAccessToken()).ReturnsAsync("token");
+
+    var tenantId = AdspId.Parse("urn:ads:platform:tenant-service:v2:/tenants/test");
+
+    var requestUrl = new Uri("https://tenant-service/tenants/v2/tenants/test");
+    using var mockHttp = new MockHttpMessageHandler();
+    mockHttp
+      .When(HttpMethod.Get, requestUrl.AbsoluteUri)
+      .Respond(
+        "application/json",
+        "{\"id\": \"urn:ads:platform:tenant-service:v2:/tenants/test\", \"name\": \"test\", \"realm\": \"test\", \"adminEmail\": \"tester@test.co\" }"
+      );
+
+    using var client = new RestClient(
+      new RestClientOptions
+      {
+        BaseUrl = requestUrl,
+        ConfigureMessageHandler = _ => mockHttp
+      }
+    );
+    using var middleware = new TenantService(logger, cache, serviceDirectory.Object, tokenProvider.Object, client);
+    var tenant = await middleware.GetTenant(tenantId);
+    tenant!.Id.Should().Be(tenantId);
+  }
+}
