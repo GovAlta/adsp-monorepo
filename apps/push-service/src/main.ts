809019b2ca71616d61ac89508c1817471a67ba33
Ting Zuge <ting.zuge@gov.ab.ca>
2022-08-08 10:05:08 -0600

    fix(push-service): updating middleware wrapper to handle end() by provide unauthorized error (based on expected passport JS behaviour).

--- apps/push-service/src/main.ts
+++ apps/push-service/src/main.ts
@@ -1,5 +1,10 @@
 import { AdspId, initializePlatform, User } from '@abgov/adsp-service-sdk';
-import { createLogger, createAmqpConfigUpdateService, createErrorHandler } from '@core-services/core-common';
+import {
+  createLogger,
+  createAmqpConfigUpdateService,
+  createErrorHandler,
+  UnauthorizedError,
+} from '@core-services/core-common';
 import { createAdapter as createIoAdapter } from '@socket.io/redis-adapter';
 import * as compression from 'compression';
 import * as cors from 'cors';
@@ -96,7 +101,10 @@ const initializeApp = async (): Promise<Server> => {
   const wrapForIo = (handler: express.RequestHandler) => (socket: Socket, next) =>
     handler(
       socket.request as express.Request,
-      { end: () => socket.disconnect(true) } as unknown as express.Response,
+      {
+        // Passport JS calls end w/ 401 when all authenticators fail.
+        end: () => next(new UnauthorizedError('User not authorized to connect.')),
+      } as unknown as express.Response,
       next
     );
 
