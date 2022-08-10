34a2f76e204e35a9d7cdef5368ed8c38dbc4eb3b
Ting Zuge <ting.zuge@gov.ab.ca>
2022-08-10 08:37:40 -0600

    chore(file-service): changing configuration schema to use pattern property for type keys (ids).

--- apps/file-service/src/file/configuration/schema.ts
+++ apps/file-service/src/file/configuration/schema.ts
@@ -1,15 +1,18 @@
 export const configurationSchema = {
   type: 'object',
-  additionalProperties: {
-    type: 'object',
-    properties: {
-      id: { type: 'string', pattern: '^[a-zA-Z0-9-_ ]{1,50}$' },
-      name: { type: 'string' },
-      anonymousRead: { type: 'boolean' },
-      readRoles: { type: 'array', items: { type: 'string' } },
-      updateRoles: { type: 'array', items: { type: 'string' } },
+  patternProperties: {
+    '^[a-zA-Z0-9-_ ]{1,50}$': {
+      type: 'object',
+      properties: {
+        id: { type: 'string', pattern: '^[a-zA-Z0-9-_ ]{1,50}$' },
+        name: { type: 'string' },
+        anonymousRead: { type: 'boolean' },
+        readRoles: { type: 'array', items: { type: 'string' } },
+        updateRoles: { type: 'array', items: { type: 'string' } },
+      },
+      required: ['id', 'name', 'anonymousRead', 'readRoles', 'updateRoles'],
+      additionalProperties: false,
     },
-    required: ['id', 'name', 'anonymousRead', 'readRoles', 'updateRoles'],
-    additionalProperties: false,
   },
+  additionalProperties: false,
 };
