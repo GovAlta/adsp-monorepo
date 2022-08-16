fafabc8040fb5aa7cad7168d50a322220a38ae73
Subbu <subbumettu007@gmail.com>
2022-08-16 12:30:28 -0600

    feat: Added new tets to handle ArgumentException for ServiceDirectory functions

--- libs/adsp-service-net-sdk/Directory/ServiceDirectory.Spec.cs
+++ libs/adsp-service-net-sdk/Directory/ServiceDirectory.Spec.cs
@@ -55,7 +55,7 @@ public class ServiceDirectoryTests
   }
 
   [Fact]
-  public async Task NoServiceUrl()
+  public async Task NoServiceUrlArgumentException()
   {
     var logger = Mock.Of<ILogger<ServiceDirectory>>();
     var cache = new MemoryCache(new MemoryCacheOptions());
@@ -89,7 +89,47 @@ public class ServiceDirectoryTests
         }
       );
     using var middleware = new ServiceDirectory(logger, cache, options.Object, client);
-    var tenantUrl = await middleware.GetServiceUrl(tenantId);
-    tenantUrl.Should().Be(null);
+    ArgumentException ex = await Assert.ThrowsAsync<ArgumentException>(() => middleware.GetServiceUrl(tenantId));
+    ex.Message.Should().Be($"No service url for {tenantId}");
+  }
+
+  [Fact]
+  public void NoDirectoryUrlInOptionsArgumentException()
+  {
+    var logger = Mock.Of<ILogger<ServiceDirectory>>();
+    var serviceDirectory = new Mock<IServiceDirectory>();
+
+    var requestBaseUrl = new Uri("https://directory-service/");
+    var tenantId = AdspId.Parse("urn:ads:platform:test-service:v2");
+
+    using var mockHttp = new MockHttpMessageHandler();
+    mockHttp
+      .When("https://directory-service/directory/v2/namespaces/platform/entries")
+      .Respond(
+        "application/json",
+        "[{\"namespace\":\"platform\",\"service\":\"tenant-service:v2\",\"url\":\"https://tenant-service\",\"urn\":\"urn:ads:platform:tenant-service:v2\"}]"
+      );
+
+    using var client = new RestClient(
+      new RestClientOptions
+      {
+        BaseUrl = requestBaseUrl,
+        ConfigureMessageHandler = _ => mockHttp
+      }
+    );
+    var options = new Mock<IOptions<AdspOptions>>();
+    options
+      .Setup(o => o.Value)
+      .Returns(
+        new AdspOptions
+        {
+        }
+      );
+    ArgumentException ex = Assert.Throws<ArgumentException>(() =>
+    {
+      var cache = new MemoryCache(new MemoryCacheOptions());
+      using var middleware = new ServiceDirectory(logger, cache, options.Object, client);
+    });
+    ex.Message.Should().Contain("Provided options must include value for DirectoryUrl.");
   }
 }
