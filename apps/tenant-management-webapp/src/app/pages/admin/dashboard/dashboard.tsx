ba47d688da7c611c01dd0038b041a52ed39bd8cc
Paul.Li <paul.li@gov.ab.ca>
2022-08-22 11:03:21 -0600

    update the admin dashboard

--- apps/tenant-management-webapp/src/app/pages/admin/dashboard/dashboard.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/dashboard/dashboard.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GoAButton, GoACallout } from '@abgov/react-components';
-import { GoACard } from '@abgov/react-components/experimental';
+import { GoACard, GoAIcon, GoAInput } from '@abgov/react-components/experimental';
 import { Link } from 'react-router-dom';
 import ReactTooltip from 'react-tooltip';
 import { Grid, GridItem } from '@components/Grid';
@@ -9,6 +9,58 @@ import { useSelector } from 'react-redux';
 import { RootState } from '@store/index';
 import styled from 'styled-components';
 import CopyIcon from '@icons/copy-outline.svg';
+import { GoAButton as GoAButtonV2, GoAChip } from '@abgov/react-components-new';
+
+interface ExternalLink {
+  link: string;
+  name: string;
+}
+
+const ExternalLink = ({ link, name }: ExternalLink): JSX.Element => {
+  return (
+    <ExternalLinkWrapper>
+      <a href={link} rel="noopener noreferrer" target="_blank">
+        {name}
+      </a>
+    </ExternalLinkWrapper>
+  );
+};
+
+interface LinkCopyComponentProps {
+  link: string;
+}
+
+const LinkCopyComponentWrapper = styled.div`
+  input {
+    width: 80%;
+    height: 48px;
+    border-width: 2px 2px 2px 0px;
+  }
+`;
+
+const LinkCopyComponent = ({ link }: LinkCopyComponentProps): JSX.Element => {
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  return (
+    <LinkCopyComponentWrapper>
+      {isCopied && <GoAChip content="Link copied to clipboard" leadingIcon="checkmark-circle" />}
+      <GoAButtonV2
+        type="tertiary"
+        leadingIcon="open"
+        onClick={() => {
+          navigator.clipboard.writeText(link);
+          setIsCopied(true);
+        }}
+      >
+        Copy link
+      </GoAButtonV2>
+    </LinkCopyComponentWrapper>
+  );
+};
+
+interface StaticLinkInputProps {
+  link: string;
+}
 
 const Dashboard = (): JSX.Element => {
   const tenantAdminRole = 'tenant-admin';
@@ -155,33 +207,25 @@ const Dashboard = (): JSX.Element => {
             <p>To give another user limited access to your realm:</p>
 
             <p>
-              1. Add the 'tenant-admin' role to the user's assigned roles from{' '}
-              <a href={getKeycloakAdminPortalUsers()} rel="noopener noreferrer" target="_blank">
-                here
-              </a>
+              <ListWrapper>
+                <li>
+                  Share the login URL below and have your user <ExternalLink link={loginUrl} name="login" /> once to
+                  create their account.
+                </li>
+                <li>
+                  Add the 'tenant-admin' role to the user's assigned roles from{' '}
+                  <ExternalLink link={getKeycloakAdminPortalUsers()} name="here" />
+                  <br />
+                  (Role Mapping › Client Roles › urn:ads:platform:tenant-service › Add selected)
+                </li>
+                <li>
+                  Once granted the role, the user can access tenant admin using the URL below.
+                  <br />
+                  <LinkCopyComponent link={loginUrl} />
+                </li>
+              </ListWrapper>
             </p>
-            <div className="small-font mt-2">
-              (Role Mapping &#8250; Client Roles &#8250; urn:ads:platform:tenant-service &#8250; Add selected)
-            </div>
-
-            <p>2. Share the following URL to complete the process.</p>
-
-            <div className="copy-url">
-              <a target="_blank" href={loginUrl} rel="noreferrer">
-                {loginUrl}
-              </a>
-            </div>
-            <GoAButton data-tip="Copied!" data-for="registerTipUrl">
-              Click to copy
-            </GoAButton>
-            <ReactTooltip
-              id="registerTipUrl"
-              place="top"
-              event="click"
-              eventOff="blur"
-              effect="solid"
-              afterShow={() => _afterShow(loginUrl)}
-            />
+            <br />
           </DashboardAside>
         </Page>
         <footer>
@@ -228,7 +272,7 @@ const DashboardAside = styled(Aside)`
   padding-top: 1.6em;
 
   .copy-url {
-            font - size: var(--fs-sm);
+    font - size: var(--fs-sm);
     background-color: var(--color-gray-100);
     border: 1px solid var(--color-gray-300);
     border-radius: 1px;
@@ -244,7 +288,7 @@ const DashboardAside = styled(Aside)`
   }
 
   .mt-2 {
-            margin - top: 2em;
+    margin - top: 2em;
   }
 `;
 const DashboardDiv = styled.div`
@@ -254,4 +298,19 @@ const DashboardDiv = styled.div`
     }
   }
   margin-bottom: 2.5rem;
+`;
+
+const ExternalLinkWrapper = styled.div`
+  display: inline-block;
+  .goa-icon {
+    display: inline !important;
+  }
+`;
+
+const ListWrapper = styled.ul`
+  list-style-type: value;
+  margin-left: 1rem;
+  li {
+    margin-bottom: 0.5rem;
+  }
 `;
