45024f8e2752c18008ec885ad54b7440063c6c44
Ting Zuge <ting.zuge@gov.ab.ca>
2022-08-24 10:58:37 -0600

    fix(adsp-service-sdk): adding a workaround in the realm strategy to skip jwks key retrieval when the issuer does not match expected.

--- libs/core-common/src/errors/validationHandler.ts
+++ libs/core-common/src/errors/validationHandler.ts
@@ -1,12 +1,18 @@
+import { benchmark } from '@abgov/adsp-service-sdk';
 import { RequestHandler } from 'express';
 import { ValidationChain, validationResult } from 'express-validator';
 import { ValidationFailedError } from './validationFailed';
 
 export function createValidationHandler(...chain: (ValidationChain | RequestHandler)[]): RequestHandler[] {
   return [
+    (req, _res, next) => {
+      benchmark(req, 'validation-time');
+      next();
+    },
     ...chain,
     (req, _res, next) => {
       const result = validationResult(req);
+      benchmark(req, 'validation-time');
       if (!result?.isEmpty()) {
         next(new ValidationFailedError(result));
       } else {
