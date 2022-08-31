5772faebb6a5cf6ac08316d9abbf50959aea1c74
Ting Zuge <ting.zuge@gov.ab.ca>
2022-08-31 15:24:09 -0600

    feat(script-service): adding domain event for script execution failure and refactoring event send for triggered executions.

--- apps/script-service/Services/ILuaScriptService.cs
+++ apps/script-service/Services/ILuaScriptService.cs
@@ -4,5 +4,12 @@ using Adsp.Sdk;
 namespace Adsp.Platform.ScriptService.Services;
 public interface ILuaScriptService
 {
-  Task<IEnumerable<object>> RunScript(AdspId tenantId, ScriptDefinition definition, IDictionary<string, object?> inputs, string? token);
+  Task<IEnumerable<object>> RunScript(
+    AdspId tenantId,
+    ScriptDefinition definition,
+    IDictionary<string, object?> inputs,
+    string token,
+    string? correlationId = null,
+    UserIdentifier? user = null
+  );
 }
