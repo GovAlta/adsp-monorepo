ee4ceef02d8271c122443806779cea1e430fb43a
tzuge <47162374+tzuge@users.noreply.github.com>
2022-09-01 13:55:47 -0600

    feat(file-service): signal a scanned event when uploaded files are scanned.

--- apps/file-service/src/file/events.ts
+++ apps/file-service/src/file/events.ts
@@ -3,6 +3,7 @@ import type { File } from './types';
 
 export const FILE_UPLOADED_EVENT = 'file-uploaded';
 export const FILE_DELETED_EVENT = 'file-deleted';
+export const FILE_SCANNED_EVENT = 'file-scanned';
 
 const fileSchema = {
   type: 'object',
@@ -63,6 +64,23 @@ export const FileDeletedDefinition: DomainEventDefinition = {
   },
 };
 
+export const FileScannedDefinition: DomainEventDefinition = {
+  name: FILE_SCANNED_EVENT,
+  description: 'Signalled when a file is scanned.',
+  payloadSchema: {
+    type: 'object',
+    properties: {
+      infected: { type: 'boolean' },
+      file: fileSchema,
+    },
+  },
+  interval: {
+    namespace: 'file-service',
+    name: FILE_UPLOADED_EVENT,
+    metric: ['file-service', 'file-scan-time'],
+  },
+};
+
 export function fileUploaded(tenantId: AdspId, user: User, file: File): DomainEvent {
   return {
     name: FILE_UPLOADED_EVENT,
@@ -91,6 +109,19 @@ export function fileDeleted(user: User, file: File): DomainEvent {
         id: user.id,
         name: user.name,
       },
+    },
+  };
+}
+
+export function fileScanned(tenantId: AdspId, file: File, infected: boolean): DomainEvent {
+  return {
+    name: FILE_SCANNED_EVENT,
+    tenantId,
+    timestamp: new Date(),
+    correlationId: file.recordId || file.id,
+    payload: {
+      file,
+      infected,
     },
   };
 }
