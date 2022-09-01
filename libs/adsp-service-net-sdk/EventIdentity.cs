703536ae55898e1445c50ee129687c92d0b17c58
Ting Zuge <ting.zuge@gov.ab.ca>
2022-09-01 08:24:09 -0600

    chore(adsp-service-net-sdk): adjusting json serialization behaviour for event identity.

--- libs/adsp-service-net-sdk/EventIdentity.cs
+++ libs/adsp-service-net-sdk/EventIdentity.cs
@@ -1,17 +1,20 @@
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
-using Adsp.Sdk.Amqp;
 
 namespace Adsp.Sdk;
 public class EventIdentity
 {
   [JsonPropertyName("namespace")]
+  [Required]
   public string Namespace { get; private set; }
 
   [JsonPropertyName("name")]
+  [Required]
   public string Name { get; private set; }
 
   [JsonPropertyName("criteria")]
+  [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
   public EventIdentityCriteria? Criteria { get; set; }
 
   public EventIdentity(string @namespace, string name)
@@ -25,8 +28,10 @@ public class EventIdentity
 public class EventIdentityCriteria
 {
   [JsonPropertyName("correlationId")]
+  [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
   public string? CorrelationId { get; set; }
 
   [JsonPropertyName("context")]
+  [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
   public IDictionary<string, object>? Context { get; set; }
 }
