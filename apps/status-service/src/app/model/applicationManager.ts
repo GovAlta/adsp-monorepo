20e62e62741c2e66c55c816592ec9f488e5831b1
Roy Styan <rstyan@telus.net>
2022-09-09 08:27:27 -0600

    fix: add back status-service configuration rebuilder

--- apps/status-service/src/app/model/applicationManager.ts
+++ apps/status-service/src/app/model/applicationManager.ts
@@ -1,6 +1,7 @@
-import { AdspId, ConfigurationService, TokenProvider } from '@abgov/adsp-service-sdk';
+import { AdspId, ConfigurationService, ServiceDirectory, TokenProvider } from '@abgov/adsp-service-sdk';
 import { Logger } from 'winston';
 import { ServiceStatusRepository } from '../repository/serviceStatus';
+import { updateConfiguration } from '../router/serviceStatus';
 import { ApplicationList } from './ApplicationList';
 import ServiceStatusApplicationEntity, {
   ApplicationData,
@@ -14,17 +15,22 @@ export class ApplicationManager {
   #configurationFinder: ConfigurationFinder;
   #repository: ServiceStatusRepository;
   #logger: Logger;
+  #tokenProvider: TokenProvider;
+  #directory: ServiceDirectory;
 
   constructor(
     tokenProvider: TokenProvider,
     service: ConfigurationService,
     serviceId: AdspId,
     repository: ServiceStatusRepository,
+    directory: ServiceDirectory,
     logger: Logger
   ) {
     this.#configurationFinder = this.#getConfigurationFinder(tokenProvider, service, serviceId);
     this.#repository = repository;
     this.#logger = logger;
+    this.#tokenProvider = tokenProvider;
+    this.#directory = directory;
   }
 
   getActiveApps = async () => {
@@ -93,9 +99,44 @@ export class ApplicationManager {
       if (app) {
         appData[status._id] = { ...status, ...app };
       } else {
+        // There should always be an app associated with
+        // the status, but...
+        appData[status._id] = { ...status, url: status.endpoint.url };
+        this.#saveConfiguration(status);
         this.#logger.warn(`could not find application configuration associated with id ${status._id}`);
       }
     });
     return appData;
+  };
+
+  // TODO remove this code when the status no longer has the
+  // needed properties for this.
+  #saveConfiguration = async (status: ServiceStatusApplicationEntity) => {
+    await updateConfiguration(this.#directory, this.#tokenProvider, AdspId.parse(status.tenantId), status._id, {
+      _id: status._id,
+      name: status.name,
+      url: status.endpoint.url,
+      description: status.description,
+    });
+  };
+
+  /**
+   * This should be run once by main, to update the service configuration
+   * with any apps in the database that are not yet there.
+   * Once all the apps have been added the call, and this method, can
+   * be removed.  Sept 1, 2022.
+   * @param logger - its a logger.
+   */
+  convertData = async (logger: Logger) => {
+    const statuses = await this.#repository.find({});
+    const tenants = this.#getActiveTenants(statuses);
+    const apps = this.#getConfigurations(tenants);
+    // Add configuration if it is missing
+    statuses.forEach(async (a) => {
+      if (!apps[a._id]) {
+        logger.info(`##########  Adding configuration for app ${a.name} in ${a.tenantName} tenant`);
+        await this.#saveConfiguration(a);
+      }
+    });
   };
 }
