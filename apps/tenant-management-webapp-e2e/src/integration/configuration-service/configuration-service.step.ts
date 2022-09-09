53215dffc8c005ea23b26e347f530fb6ffb7f1b5
Howard Zhang <howard_zhang@hotmail.com>
2022-09-09 16:52:59 -0600

    Added a configuration template test (#1149)
    * Added a subscriber test
    
    * Changed reset to .clear()
    
    * Added a negative test for tenant name in tenant creation
    
    * Added a validation on the second numbers
    
    * Added a test for application status change event
    
    * Updated some comments
    
    * Added a subscriber app test for an authenticated user
    
    * Updated Subscription management page to subscription management page
    
    * Core file types test
    
    * Added a event stream test
    
    * Added core streams in the validation steps
    
    * Added core streams in step definitions
    
    * Added a subscriber SMS test case and some modifications on existing steps
    
    * Complete subscriber phone number test
    
    * Restored original page object for edit icon
    
    * Moved edit icon page object to a different place
    
    * Added a check on phone number parameter
    
    * Added a test for directory entry event log
    
    * PR review updates
    
    * Added calendar overview test
    
    * Updated the test based on PR review feedback
    
    * Fixed indentation to be 2 spaces instead of 4
    
    * Added an api test for directory service
    
    * Added an api tag to the api test
    
    * Updated a comment
    
    * Added a service roles test
    
    * Updated error message if the role isn't found
    
    * Updated a code comment
    
    * Added the app status test
    
    * Application status change test and CodeQL fixes
    
    * Updated timestamp validation
    
    * Added configuration template test
    
    Co-authored-by: Howard Zhang <howard.zhang@gov.ab.ca>

--- apps/tenant-management-webapp-e2e/src/integration/configuration-service/configuration-service.step.ts
+++ apps/tenant-management-webapp-e2e/src/integration/configuration-service/configuration-service.step.ts
@@ -1,5 +1,6 @@
-import { Then, When } from 'cypress-cucumber-preprocessor/steps';
+import { Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
 import ConfigurationServicePage from './configuration-service.page';
+import commonlib from '../common/common-library';
 
 const configurationObj = new ConfigurationServicePage();
 
@@ -8,7 +9,7 @@ Then('the user views a heading of {string} namespace', function (namespace) {
 });
 
 Then('the user views a {string} under core-service configurations', function (definitionName) {
-  configurationObj.configurationDefinition(definitionName).should('exist');
+  configurationObj.configurationDefinitionWithName(definitionName).should('exist');
 });
 
 When('the user clicks eye icon of {string} under Platform to view the schema', function (definitionName) {
@@ -32,5 +33,127 @@ Then(
       default:
         expect(viewOrNot).to.be.oneOf(['views', 'should not view']);
     }
+  }
+);
+
+Given('a tenant admin user is on configuration overview page', function () {
+  commonlib.tenantAdminDirectURLLogin(
+    Cypress.config().baseUrl,
+    Cypress.env('realm'),
+    Cypress.env('email'),
+    Cypress.env('password')
+  );
+  commonlib.tenantAdminMenuItem('Configuration', 4000);
+  cy.wait(2000);
+});
+
+When('the user clicks Add definition button on configuration overview page', function () {
+  configurationObj.addConfigurationDefinitionBtn().click();
+});
+
+Then('the user views Add definition modal', function () {
+  configurationObj.configurationDefinitionModalTitle().invoke('text').should('eq', 'Add definition');
+});
+
+When('the user enters {string} in namespace field in configuration definition modal', function (namespace) {
+  configurationObj.addConfigurationDefinitionModalNamespaceField().clear().type(namespace);
+});
+
+When('the user enters {string} in name field in configuration definition modal', function (name) {
+  configurationObj.addConfigurationDefinitionModalNameField().clear().type(name);
+});
+
+Then(
+  'the user views the error message of {string} on namespace in configuration definition modal',
+  function (errorMsg) {
+    configurationObj.addConfigurationDefinitionModalNamespaceErrorMsg().invoke('text').should('contain', errorMsg);
+  }
+);
+
+Then('the user views the error message of {string} on name in configuration definition modal', function (errorMsg) {
+  configurationObj.addConfigurationDefinitionModalNameErrorMsg().invoke('text').should('contain', errorMsg);
+});
+
+When(
+  'the user enters {string} in namespace and {string} in name, {string} in description in configuration definition modal',
+  function (namespace, name, desc) {
+    configurationObj.addConfigurationDefinitionModalNamespaceField().clear().type(namespace);
+    configurationObj.addConfigurationDefinitionModalNameField().clear().type(name);
+    configurationObj.addConfigurationDefinitionModalDescField().clear().type(desc);
+  }
+);
+
+When('the user clicks Save button in configuration definition modal', function () {
+  configurationObj.configurationDefinitionModalSaveBtn().click();
+  cy.wait(2000); // Wait for the record to save and show in the grid
+});
+
+Then(
+  'the user {string} the configuration definition of {string}, {string} under {string}',
+  function (viewOrNot, name, desc, namespace) {
+    switch (viewOrNot) {
+      case 'views':
+        configurationObj.configurationDefinition(namespace, name, desc).should('exist');
+        break;
+      case 'should not view':
+        configurationObj.configurationDefinition(namespace, name, desc).should('not.exist');
+        break;
+      default:
+        expect(viewOrNot).to.be.oneOf(['views', 'should not view']);
+    }
+  }
+);
+
+When(
+  'the user clicks {string} button for the configuration definition of {string}, {string} under {string}',
+  function (buttonName, name, desc, namespace) {
+    switch (buttonName.toLowerCase()) {
+      case 'edit':
+        configurationObj.configurationDefinitionEditBtn(namespace, name, desc).click();
+        break;
+      case 'delete':
+        configurationObj.configurationDefinitionDeleteBtn(namespace, name, desc).click();
+        break;
+      case 'eye':
+        configurationObj.configurationDefinitionEyeBtn(namespace, name, desc).click();
+        break;
+      default:
+        expect(buttonName.toLowerCase()).to.be.oneOf(['edit', 'delete', 'eye']);
+    }
+  }
+);
+
+Then('the user views Edit definition modal', function () {
+  configurationObj.configurationDefinitionModalTitle().invoke('text').should('eq', 'Edit definition');
+});
+
+Then('the user views disabled namespace and name fields in configuration definition modal', function () {
+  configurationObj.addConfigurationDefinitionModalNamespaceField().should('be.disabled');
+  configurationObj.addConfigurationDefinitionModalNameField().should('be.disabled');
+});
+
+When('the user enters {string} in description in configuration definition modal', function (desc) {
+  configurationObj.addConfigurationDefinitionModalDescField().clear().type(desc);
+});
+
+When('the user enters "{string}" in payload schema in configuration definition modal', function (desc) {
+  configurationObj.addConfigurationDefinitionModalDescField().clear().type(desc);
+});
+
+When('the user enters {string} in payload schema in configuration definition modal', function (payload) {
+  configurationObj.configurationDefinitionModalPayloadEditor().click().focus().type('{ctrl}a').clear().type(payload);
+});
+
+// payload parameter has payload content without "{}"
+// for example:
+// payload variable would be: "name": "test"
+// for the complete payload of
+// {
+//   "name": "test"
+// }
+Then(
+  'the user views the payload schema containing {string} for {string}, {string} under {string}',
+  function (payload, name, desc, namespace) {
+    configurationObj.configurationDefinitionDetails(namespace, name, desc).invoke('text').should('contain', payload);
   }
 );
