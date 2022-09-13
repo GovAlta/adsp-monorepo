111c5f110abb6bba39d274f174773462f2d32b2b
Roy Styan <rstyan@telus.net>
2022-09-13 12:32:43 -0600

    fix: remove local, redux caching for new applications from status-app

--- apps/status-service/src/app/router/ApplicationCache.ts
+++ apps/status-service/src/app/router/ApplicationCache.ts
@@ -1,6 +1,14 @@
 import * as NodeCache from 'node-cache';
 import { StaticApplicationData } from '../model';
 
+/**
+ * ApplicationCache: The application definition is stored in the configuration
+ * service. However, when an app is first created/saved, the configuration-service
+ * cache may not be immediately refreshed.  Normally, the refresh is immediate, but
+ * there are enough exceptions to this that it can be annoying to the end user, who
+ * just added an application and the name comes back as 'unknown'.  So the status-
+ * service will cache new and updated application values to avoid this delay.
+ */
 export class ApplicationCache {
   #nodeCache: NodeCache;
 
