3590b78442cc5e30337b0615ba2b48b358b633cb
Ting Zuge <ting.zuge@gov.ab.ca>
2022-09-15 16:19:50 -0600

    feat(adsp-service-sdk): adding startBenchmark that returns an end function and updating services to use it instead of benchmark.

--- libs/adsp-service-sdk/src/metrics/benchmark.ts
+++ libs/adsp-service-sdk/src/metrics/benchmark.ts
@@ -1,5 +1,5 @@
 import { Request } from 'express';
-import { RequestBenchmark, REQ_BENCHMARK } from './types';
+import { EndBenchmark, RequestBenchmark, REQ_BENCHMARK } from './types';
 
 export function benchmark(req: Request, metric: string, value?: number): void {
   const benchmark: RequestBenchmark = req[REQ_BENCHMARK];
@@ -16,5 +16,21 @@ export function benchmark(req: Request, metric: string, value?: number): void {
         benchmark.timings[metric] = process.hrtime();
       }
     }
+  }
+}
+
+export function startBenchmark(req: Request, metric: string): EndBenchmark {
+  const benchmark: RequestBenchmark = req[REQ_BENCHMARK];
+  if (benchmark) {
+    const startAt = process.hrtime();
+    return () => {
+      const [sec, nano] = process.hrtime(startAt);
+      const value = sec * 1e3 + nano * 1e-6;
+      benchmark.metrics[metric] = (benchmark.metrics[metric] || 0) + value;
+    };
+  } else {
+    return () => {
+      // return a no-op in case there is no benchmark context.
+    };
   }
 }
