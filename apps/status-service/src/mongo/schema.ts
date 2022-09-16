a4a3eb6990fef918cb26d837d90d2e75686c5559
Roy Styan <rstyan@telus.net>
2022-09-16 15:20:25 -0600

    fix(status-service): remove name,url,desc from application repository (#1191)

--- apps/status-service/src/mongo/schema.ts
+++ apps/status-service/src/mongo/schema.ts
@@ -2,10 +2,6 @@ import { Schema } from 'mongoose';
 
 export const serviceStatusEndpointSchema = new Schema(
   {
-    url: {
-      type: String,
-      required: false,
-    },
     status: {
       type: String,
       default: 'disabled',
@@ -51,14 +47,6 @@ export const serviceStatusApplicationSchema = new Schema(
     tenantName: {
       type: String,
       required: true,
-    },
-    name: {
-      type: String,
-      required: false,
-    },
-    description: {
-      type: String,
-      required: false,
     },
     metadata: {
       type: Schema.Types.Mixed,
