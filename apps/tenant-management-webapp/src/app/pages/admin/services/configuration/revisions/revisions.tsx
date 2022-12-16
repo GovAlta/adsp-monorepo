fc7313076832413693d74cd9f66e8f4cb99d618d
Shuang chen <shuangchen@AthenaMacBook.local>
2022-12-16 14:50:56 -0700

    fix: realign icon and update configuration revision into new react componennt

--- apps/tenant-management-webapp/src/app/pages/admin/services/configuration/revisions/revisions.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/configuration/revisions/revisions.tsx
@@ -2,10 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@store/index';
 import { getConfigurationDefinitions, getConfigurationRevisions } from '@store/configuration/action';
-import { GoADropdownOption, GoADropdown } from '@abgov/react-components';
-import { GoAForm, GoAFormItem } from '@abgov/react-components/experimental';
+import { GoADropdownOption, GoADropdown, GoAFormItem, GoASkeleton } from '@abgov/react-components-new';
 import { RevisionTable } from './revisionsTable';
-import { GoASkeletonGridColumnContent } from '@abgov/react-components';
 
 export const ConfigurationRevisions = (): JSX.Element => {
   const { serviceList } = useSelector((state: RootState) => state.configuration);
@@ -21,36 +19,33 @@ export const ConfigurationRevisions = (): JSX.Element => {
 
   return (
     <div>
-      <GoAForm>
-        <GoAFormItem>
-          <label aria-label="select-configuration-label">Select definition</label>
-          {indicator.show && serviceList?.length === 0 && (
-            <GoASkeletonGridColumnContent key={1} rows={1}></GoASkeletonGridColumnContent>
-          )}
-          {serviceList?.length > 0 && (
-            <GoADropdown
-              name="Configurations"
-              selectedValues={[selectedConfiguration]}
-              onChange={(name: string, selectedConfiguration: string | string[]) => {
-                setSelectedConfiguration(selectedConfiguration.toString());
-                dispatch(getConfigurationRevisions(selectedConfiguration.toString()));
-                setShowTable(true);
-              }}
-              aria-label="select-configuration-dropdown"
-              multiSelect={false}
-            >
-              {serviceList.map((item) => (
-                <GoADropdownOption
-                  key={item}
-                  label={item}
-                  value={item}
-                  data-testid={`${item}-get-configuration-options`}
-                />
-              ))}
-            </GoADropdown>
-          )}
-        </GoAFormItem>
-      </GoAForm>
+      <GoAFormItem label="Select definition">
+        {indicator.show && serviceList?.length === 0 && <GoASkeleton type="text" key={1}></GoASkeleton>}
+        {serviceList?.length > 0 && (
+          <GoADropdown
+            name="Configurations"
+            value={[selectedConfiguration]}
+            onChange={(name: string, selectedConfiguration: string | string[]) => {
+              setSelectedConfiguration(selectedConfiguration.toString());
+              dispatch(getConfigurationRevisions(selectedConfiguration.toString()));
+              setShowTable(true);
+            }}
+            aria-label="select-configuration-dropdown"
+            width="100%"
+          >
+            {serviceList.map((item) => (
+              <GoADropdownOption
+                name="Configurations"
+                key={item}
+                label={item}
+                value={item}
+                data-testid={`${item}-get-configuration-options`}
+              />
+            ))}
+          </GoADropdown>
+        )}
+      </GoAFormItem>
+
       {showTable && <RevisionTable service={selectedConfiguration} />}
     </div>
   );
