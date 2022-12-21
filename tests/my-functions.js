e26c93bae828d8837ad3ab86f2b70401f5059fd0
Subbu <subbumettu007@gmail.com>
2022-12-21 10:49:29 -0700

    revert artillery report action (#1446)
    * feat: reading artillery reports from github actions
    
    * feat: install dependencies for fakerjs
    
    * feat: setting node in artillery actions
    
    * feat: correction npm i
    
    * fix: adding heap size for node
    
    * fix: add npm ci build step
    
    * fix: using npm cache
    
    * feat: removed build step
    
    * fix: revert artillery action
    
    * fix: removed npm ci

--- tests/my-functions.js
+++ tests/my-functions.js
@@ -1,40 +1,36 @@
-// import { faker } from '@faker-js/faker/locale/en_CA';
-const { faker } = require('@faker-js/faker');
 function setJSONBody(requestParams, context, ee, next) {
-  context.vars.eventName = faker.name.firstName();
   let payload = {
     data: 'data',
   };
   payload = {
     operation: 'UPDATE',
-    update: {},
-  };
-  payload['update'][context.vars.eventName] = {
-    name: context.vars.eventName,
-    definitions: {
-      'artillery-load': {
-        name: 'artillery-load',
-        description: 'artillery',
-        payloadSchema: {
-          type: 'object',
-          properties: {},
-          required: [],
-          additionalProperties: true,
+    update: {
+      artillery: {
+        name: 'artillery',
+        definitions: {
+          'artillery-load': {
+            name: 'artillery-load',
+            description: 'artillery',
+            payloadSchema: {
+              type: 'object',
+              properties: {},
+              required: [],
+              additionalProperties: true,
+            },
+          },
         },
       },
     },
   };
 
   context.vars.payload = payload;
-  console.log(context.vars.eventName, ' context.vars.eventName ');
-
   return next(); // MUST be called for the scenario to continue
 }
 function deleteEvent(requestParams, context, ee, next) {
   let deletePayload = {};
   deletePayload = {
     operation: 'DELETE',
-    property: context.vars.eventName,
+    property: 'artillery',
   };
 
   context.vars.deletePayload = deletePayload;
@@ -43,7 +39,6 @@ function deleteEvent(requestParams, context, ee, next) {
 
 function logHeaders(requestParams, response, context, ee, next) {
   console.log(response.body, ' response ');
-  console.log(context.vars.eventName, ' context.vars.eventName ');
   return next(); // MUST be called for the scenario to continue
 }
 module.exports = {
