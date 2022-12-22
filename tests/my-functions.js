0469561ba2c9f8f6332a5b1e1d3cc9394d5bbab0
smettu007 <subbumettu007@gmail.com>
2022-12-22 10:36:59 -0700

    feat: adding local running support

--- tests/my-functions.js
+++ tests/my-functions.js
@@ -1,36 +1,40 @@
+// import { faker } from '@faker-js/faker/locale/en_CA';
+const { faker } = require('@faker-js/faker');
 function setJSONBody(requestParams, context, ee, next) {
+  context.vars.eventName = faker.name.firstName();
   let payload = {
     data: 'data',
   };
   payload = {
     operation: 'UPDATE',
-    update: {
-      artillery: {
-        name: 'artillery',
-        definitions: {
-          'artillery-load': {
-            name: 'artillery-load',
-            description: 'artillery',
-            payloadSchema: {
-              type: 'object',
-              properties: {},
-              required: [],
-              additionalProperties: true,
-            },
-          },
+    update: {},
+  };
+  payload['update'][context.vars.eventName] = {
+    name: context.vars.eventName,
+    definitions: {
+      'artillery-load': {
+        name: 'artillery-load',
+        description: 'artillery',
+        payloadSchema: {
+          type: 'object',
+          properties: {},
+          required: [],
+          additionalProperties: true,
         },
       },
     },
   };
 
   context.vars.payload = payload;
+  console.log(context.vars.eventName, ' context.vars.eventName ');
+
   return next(); // MUST be called for the scenario to continue
 }
 function deleteEvent(requestParams, context, ee, next) {
   let deletePayload = {};
   deletePayload = {
     operation: 'DELETE',
-    property: 'artillery',
+    property: context.vars.eventName,
   };
 
   context.vars.deletePayload = deletePayload;
@@ -39,6 +43,7 @@ function deleteEvent(requestParams, context, ee, next) {
 
 function logHeaders(requestParams, response, context, ee, next) {
   console.log(response.body, ' response ');
+  console.log(context.vars.eventName, ' context.vars.eventName ');
   return next(); // MUST be called for the scenario to continue
 }
 module.exports = {
