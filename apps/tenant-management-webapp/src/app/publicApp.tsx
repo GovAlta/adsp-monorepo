c18567692a2e8d893b1060475390263a3b4c6805
Paul.Li <paul.li@gov.ab.ca>
2023-02-07 14:46:19 -0700

    change the header in the login page

--- apps/tenant-management-webapp/src/app/publicApp.tsx
+++ apps/tenant-management-webapp/src/app/publicApp.tsx
@@ -1,15 +1,16 @@
 import React, { ReactNode } from 'react';
 
 import Container from '@components/Container';
-import { GoAHeader } from '@abgov/react-components';
 import styled from 'styled-components';
+import Header from '@components/AppHeader';
+
 interface publicAppProps {
   children: ReactNode;
 }
 export function PublicApp({ children }: publicAppProps): JSX.Element {
   return (
     <PublicCss>
-      <GoAHeader serviceHome="/" serviceLevel="beta" serviceName="" />
+      <Header />
       <hr />
       <Container hs={4} vs={2}>
         {children}
