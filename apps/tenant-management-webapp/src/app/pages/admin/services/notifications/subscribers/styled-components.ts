0b6a9735b0bd440eb13bb4ca3b7cafb18c00959c
subbaramaiah mettu <subbumettu007@gmail.com>
2023-03-03 12:21:34 -0500

    fix: code cleanup

---
+++ apps/tenant-management-webapp/src/app/pages/admin/services/notifications/subscribers/styled-components.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const DataTableStyle = styled.div`
+  .action {
+    width: 0;
+    text-align-last: right;
+  }
+
+  .spread {
+    width: 300px;
+  }
+
+  .no-wrap {
+    white-space: nowrap;
+  }
+`;
+
+export const RowFlex = styled.div`
+  display: flex;
+  flex-direction: row;
+  & > :not([data-account-link]):first-child {
+    visibility: hidden;
+  }
+`;
