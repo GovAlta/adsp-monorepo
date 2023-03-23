d12f6a2ce2668b935f0acbe299b3c42b61d6c841
athena-chen-chen <58053108+athena-chen-chen@users.noreply.github.com>
2023-03-23 09:29:29 -0600

    fix: add edit template modal discription issue (#1602)
    Co-authored-by: Shuang chen <shuangchen@AthenaMacBook.local>

--- apps/tenant-management-webapp/src/app/pages/admin/services/pdf/pdf.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/pdf/pdf.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { Aside, Main, Page } from '@components/Html';
 import { Tab, Tabs } from '@components/Tabs';
 import { PdfOverview } from './overview';
