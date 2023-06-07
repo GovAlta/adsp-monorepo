11133ec633400b195c38e2d874344cd06e7f39f1
Roy Styan <rstyan@telus.net>
2023-06-07 08:57:04 -0600

    fix: add overview to task-service swagger docs

--- apps/task-service/swagger.config.js
+++ apps/task-service/swagger.config.js
@@ -3,7 +3,22 @@ module.exports = {
   info: {
     title: 'Task service',
     version: '0.0.0',
-    description: 'The Task service provides a model for tasks, queues, and work assignment.',
+    description: `The Task Service gives applications a means for defining and managing tasks, and managing work assignments.
+A task is a _unit of work_ that is assigned to an individual for completion by a certain date.  Exactly what the unit of work is
+depends on the feature the application is trying to implement, but can be such things as:
+- response required,
+- signature required,
+- actions to be completed,
+- etc.
+
+You can combine tasks into  a _queue_ of work units that represents:
+- a specific job to be completed by one or more individuals,
+- an individual's daily workload,
+- reminders for a user,
+- etc.
+
+Queues are further organized into namespaces, which allows applications to keep tasks and queues separated by things like projects,
+applications, or features.`,
   },
   tags: [],
   components: {
