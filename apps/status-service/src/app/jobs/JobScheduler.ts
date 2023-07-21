c860991145c0c9b50366e229c0675d95f7d6e130
Paul.Li <paul.li@gov.ab.ca>
2023-07-21 11:31:47 -0600

    Add more try catch to the status service

--- apps/status-service/src/app/jobs/JobScheduler.ts
+++ apps/status-service/src/app/jobs/JobScheduler.ts
@@ -67,11 +67,16 @@ export class HealthCheckJobScheduler {
   };
 
   stopHealthChecks = (applicationId: string, cancelJob: (job: HealthCheckJob) => void = (j) => j.cancel()): void => {
-    if (this.#jobCache.exists(applicationId)) {
-      this.#jobCache.remove(applicationId, cancelJob);
-      this.#logger.info(`Cancelled job #${applicationId}`);
-    } else {
-      this.#logger.warn(`Asked to stop a job that isn't in the cache #${applicationId}`);
+    try {
+      this.#logger.info('Start to stop health checks');
+      if (this.#jobCache.exists(applicationId)) {
+        this.#jobCache.remove(applicationId, cancelJob);
+        this.#logger.info(`Cancelled job #${applicationId}`);
+      } else {
+        this.#logger.warn(`Asked to stop a job that isn't in the cache #${applicationId}`);
+      }
+    } catch (error) {
+      this.#logger.error(`Error stopping health checks: ${error.message}`);
     }
   };
 
@@ -79,41 +84,46 @@ export class HealthCheckJobScheduler {
   // We can get rid of this method all together by handling update and delete health check jobs
   // in the HealthCheckController.
   reloadCache = async (appManager: ApplicationManager): Promise<void> => {
-    const apps = await appManager.findEnabledApps();
-    const cachedIds = this.#jobCache.getApplicationIds();
-    const idsToRemove = [];
-    const idsToAdd = [];
-    const storedIds = apps.map((a): string => {
-      return a.appKey;
-    });
+    try {
+      this.#logger.info(`Start to reload cache.`);
+      const apps = await appManager.findEnabledApps();
+      const cachedIds = this.#jobCache.getApplicationIds();
+      const idsToRemove = [];
+      const idsToAdd = [];
+      const storedIds = apps.map((a): string => {
+        return a.appKey;
+      });
 
-    for (const app of apps) {
-      if (cachedIds.includes(app.appKey)) {
-        // Update: kill it and add it back, in case the URL has changed.
-        idsToRemove.push(app.appKey);
-        idsToAdd.push(app.appKey);
-      } else {
-        // New Application
-        idsToAdd.push(app.appKey);
+      for (const app of apps) {
+        if (cachedIds.includes(app.appKey)) {
+          // Update: kill it and add it back, in case the URL has changed.
+          idsToRemove.push(app.appKey);
+          idsToAdd.push(app.appKey);
+        } else {
+          // New Application
+          idsToAdd.push(app.appKey);
+        }
       }
-    }
 
-    for (const cachedId of cachedIds) {
-      // Deleted or disabled app
-      if (!storedIds.includes(cachedId)) {
-        idsToRemove.push(cachedId);
+      for (const cachedId of cachedIds) {
+        // Deleted or disabled app
+        if (!storedIds.includes(cachedId)) {
+          idsToRemove.push(cachedId);
+        }
       }
+
+      // Update -> delete first;
+      idsToRemove.forEach((id) => {
+        this.#jobCache.remove(id);
+      });
+
+      // then add back.
+      const appsToAdd = apps.filter((app) => {
+        return idsToAdd.includes(app.appKey);
+      });
+      this.#jobCache.addBatch(StatusApplications.fromArray(appsToAdd), getScheduler(this.#props));
+    } catch (error) {
+      this.#logger.error(`Failed reloading cache: ${error.message}.`);
     }
-
-    // Update -> delete first;
-    idsToRemove.forEach((id) => {
-      this.#jobCache.remove(id);
-    });
-
-    // then add back.
-    const appsToAdd = apps.filter((app) => {
-      return idsToAdd.includes(app.appKey);
-    });
-    this.#jobCache.addBatch(StatusApplications.fromArray(appsToAdd), getScheduler(this.#props));
   };
 }
