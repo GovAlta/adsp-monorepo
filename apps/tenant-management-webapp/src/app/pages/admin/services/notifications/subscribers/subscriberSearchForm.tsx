ab9734826763902d93aa39666c5285ce4df8a23d
RamaChandiniDevarapalli <rama.devarapalli@gov.ab.ca>
2023-07-24 12:22:48 -0600

    subscription fix

--- apps/tenant-management-webapp/src/app/pages/admin/services/notifications/subscribers/subscriberSearchForm.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/notifications/subscribers/subscriberSearchForm.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent, useState } from 'react';
 import type { SubscriberSearchCriteria } from '@store/subscription/models';
+import { isSmsValid, emailError, smsError } from '@lib/inputValidation';
 
 import {
   GoAForm,
@@ -11,6 +12,7 @@ import {
 } from '@abgov/react-components/experimental';
 import { GoAButton } from '@abgov/react-components';
 import '@abgov/core-css/src/lib/styles/v2/colors.scss';
+import { ErrorWrapper } from './editSubscriber';
 
 interface EventSearchFormProps {
   onSearch?: (searchCriteria: SubscriberSearchCriteria) => void;
@@ -26,49 +28,79 @@ export const SubscribersSearchForm: FunctionComponent<EventSearchFormProps> = ({
   onUpdate,
 }) => {
   const [criteria, setCriteria] = useState<SubscriberSearchCriteria>(searchCriteria);
+  const [formErrors, setFormErrors] = useState(null);
   const onChangeFn = (name: string, value: string) => {
     setCriteria({
       ...criteria,
       [name]: value,
     });
   };
+  const Search = (criteria) => {
+    const formErrorList = Object.assign(
+      {},
+      criteria?.email !== '' ? emailError(criteria?.email) : null,
+      smsError(criteria?.sms)
+    );
+    if (Object.keys(formErrorList).length === 0) {
+      onSearch(criteria);
+      onUpdate(criteria);
+      setFormErrors(null);
+    } else {
+      setFormErrors(formErrorList);
+    }
+  };
   return (
     <div>
       <GoAForm>
-        <GoAFlexRow gap="small">
-          <GoAFormItem>
-            <label htmlFor="name">Search subscriber address as</label>
-            <GoAInputText name="name" id="name" value={criteria?.name} onChange={onChangeFn} />
-          </GoAFormItem>
-          <GoAFormItem>
-            <label htmlFor="email">Search subscriber email</label>
-            <GoAInputEmail name="email" id="email" value={criteria?.email} onChange={onChangeFn} />
-          </GoAFormItem>
-          <GoAFormItem>
-            <label htmlFor="sms">Search subscriber phone</label>
-            <GoAInputText name="sms" id="sms" value={criteria?.sms} onChange={onChangeFn} />
-          </GoAFormItem>
-        </GoAFlexRow>
-        <GoAFormActions alignment="right">
-          <GoAButton
-            buttonType="secondary"
-            title="Reset"
-            onClick={() => {
-              reset();
-            }}
-          >
-            Reset
-          </GoAButton>
-          <GoAButton
-            title="Search"
-            onClick={() => {
-              onSearch(criteria);
-              onUpdate(criteria);
-            }}
-          >
-            Search
-          </GoAButton>
-        </GoAFormActions>
+        <ErrorWrapper>
+          <GoAFlexRow gap="small">
+            <GoAFormItem>
+              <label htmlFor="name">Search subscriber address as</label>
+              <GoAInputText name="name" id="name" value={criteria?.name} onChange={onChangeFn} />
+            </GoAFormItem>
+            <GoAFormItem error={formErrors?.['email']}>
+              <label>Search subscriber email</label>
+              <GoAInputEmail
+                name="email"
+                data-testid="form-email"
+                value={criteria?.email || ''}
+                aria-label="email"
+                onChange={(_, value) => {
+                  setCriteria({ ...criteria, email: value });
+                }}
+              />
+            </GoAFormItem>
+
+            <GoAFormItem error={formErrors?.['sms']}>
+              <label htmlFor="sms">Search subscriber phone</label>
+              <GoAInputText
+                name="sms"
+                id="sms"
+                value={criteria?.sms || ''}
+                onChange={(_, value) => {
+                  if (isSmsValid(value)) {
+                    setCriteria({ ...criteria, sms: value?.substring(0, 10) });
+                  }
+                }}
+              />
+            </GoAFormItem>
+          </GoAFlexRow>
+
+          <GoAFormActions alignment="right">
+            <GoAButton
+              buttonType="secondary"
+              title="Reset"
+              onClick={() => {
+                reset();
+              }}
+            >
+              Reset
+            </GoAButton>
+            <GoAButton title="Search" onClick={(e) => Search(criteria)}>
+              Search
+            </GoAButton>
+          </GoAFormActions>
+        </ErrorWrapper>
       </GoAForm>
     </div>
   );
