b59cba1ff6f1ea3117cf16e032682f3cfccea14a
Shuang chen <shuangchen@AthenaMacBook.local>
2023-08-08 09:32:11 -0600

    fix:style in subscribe-app and revert status application

--- apps/tenant-management-webapp/src/app/pages/admin/services/status/styled-components.ts
+++ apps/tenant-management-webapp/src/app/pages/admin/services/status/styled-components.ts
@@ -34,3 +34,20 @@ export const ToolTip = styled.div`
     left: -8px;
   }
 `;
+export const DropdownListContainer = styled.div`
+  max-height: 10rem;
+  overflow: hidden auto;
+  padding: 0rem;
+  scroll-behavior: smooth;
+`;
+
+export const DropdownList = styled.ul`
+  position: relative;
+  margin-top: 3px;
+  list-style-type: none;
+  background: var(--color-white);
+  border-radius: var(--input-border-radius);
+  box-shadow: 0 8px 8px rgb(0 0 0 / 20%), 0 4px 4px rgb(0 0 0 / 10%);
+  z-index: 99;
+  padding-left: 0rem !important;
+`;
