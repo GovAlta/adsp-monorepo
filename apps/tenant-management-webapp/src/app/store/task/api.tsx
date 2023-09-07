78745f576b87fe8fc63609dfdeac2a06d40393cd
Jonathan Weyermann <jonathan.weyermann@gov.ab.ca>
2023-09-06 21:25:36 -0600

    CS-1750 - Delete button

--- apps/tenant-management-webapp/src/app/store/task/api.tsx
+++ apps/tenant-management-webapp/src/app/store/task/api.tsx
@@ -1,9 +1,14 @@
 import axios from 'axios';
-import { TaskDefinition } from './model';
+import { TaskDefinition, DeleteTaskConfig } from './model';
 
 export const fetchTaskQueuesApi = async (token: string, url: string): Promise<Record<string, TaskDefinition>> => {
   const res = await axios.get(url, {
     headers: { Authorization: `Bearer ${token}` },
   });
+  return res.data;
+};
+
+export const deleteTaskQueuesApi = async (token: string, url: string, body: DeleteTaskConfig) => {
+  const res = await axios.patch(url, body, { headers: { Authorization: `Bearer ${token}` } });
   return res.data;
 };
