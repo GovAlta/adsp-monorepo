cea2e0477a447a7264adcc54a7d5a37f75d52d48
RamaChandiniDevarapalli <rama.devarapalli@gov.ab.ca>
2023-09-11 09:13:49 -0600

    feat: fix changes

--- apps/tenant-management-webapp/src/app/pages/admin/services/task/routers.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/task/routers.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Task } from './task';
 import { useRouteMatch } from 'react-router';
-import { QueueModalEditor } from './queueModalEditor';
+import { TaskDefinitionEditor } from './TaskDefinationEditor';
 
 export const TaskRouter = () => {
   const { url } = useRouteMatch();
@@ -11,8 +11,8 @@ export const TaskRouter = () => {
       <Route exact path={`${url}/`}>
         <Task />
       </Route>
-      <Route path={`${url}/edit/:id`} component={QueueModalEditor} />
-      <Route path={`${url}/new`} component={QueueModalEditor} />
+      <Route path={`${url}/edit/:id`} component={TaskDefinitionEditor} />
+      <Route path={`${url}/new`} component={TaskDefinitionEditor} />
     </Switch>
   );
 };
