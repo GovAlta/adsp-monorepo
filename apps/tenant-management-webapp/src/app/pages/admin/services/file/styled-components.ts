b25e528e4cab108e3a4e9294e96298c65a140a90
thdlam <47620431+thdlam@users.noreply.github.com>
2023-09-20 12:09:51 -0600

    fix linting and unit tests

--- apps/tenant-management-webapp/src/app/pages/admin/services/file/styled-components.ts
+++ apps/tenant-management-webapp/src/app/pages/admin/services/file/styled-components.ts
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 
 export const RetentionPolicyLabel = styled.label`
   font-size: 24px !important;
