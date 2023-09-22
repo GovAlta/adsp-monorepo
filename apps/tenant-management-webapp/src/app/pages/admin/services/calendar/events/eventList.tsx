fe7f8fcab2fa0cdd7c2b444e580adc7faa005c08
Paul.Li <paul.li@gov.ab.ca>
2023-09-22 11:57:38 -0600

    Update the calendar event dropdown list

--- apps/tenant-management-webapp/src/app/pages/admin/services/calendar/events/eventList.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/calendar/events/eventList.tsx
@@ -1,19 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectSelectedCalendarEvents } from '@store/calendar/selectors';
 import { CalendarEvent, EventAddEditModalType, EventDeleteModalType } from '@store/calendar/models';
-import { GoATable } from '@abgov/react-components-new';
+import { GoACheckbox } from '@abgov/react-components-new';
 import { renderNoItem } from '@components/NoItem';
 import { GoAContextMenuIcon } from '@components/ContextMenu';
 import { UpdateModalState } from '@store/session/actions';
 import moment from 'moment';
 import { RootState } from '@store/index';
-
+import {
+  TitleSpace,
+  EventDetailRow,
+  EventDetailTd,
+  EventDetailName,
+  EventDetailDescription,
+  EventDetailDate,
+} from './styled-components';
 import { DeleteModal } from './deleteModal';
+import DataTable from '@components/DataTable';
 
 interface EventListRowProps {
   event: CalendarEvent;
   calendarName: string;
+}
+
+interface EventDetailsProps {
+  event: CalendarEvent;
 }
 
 const eventDateFormat = (dateString: string) => {
@@ -21,68 +33,96 @@ const eventDateFormat = (dateString: string) => {
   return moment(date).format('DD-MMM-YY ddd HH:mm');
 };
 
-const dateTitleSpanStyle = {
-  width: '2.5rem',
-  display: 'inline-block',
+const getDateString = (date: Date): string => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
+
+const EventDetailTime = (start: string, end: string): string => {
+  const startDate = new Date(start);
+  const endDate = new Date(end);
+  const startWeekDay = startDate.toLocaleString('en-us', { weekday: 'long' });
+  const startDateDateString = getDateString(startDate);
+  const stateDateTimeString = startDate.toLocaleString('en-us', { hour: '2-digit', minute: '2-digit' });
+  const endDateDateString = getDateString(endDate);
+  const endDateTimeString = startDate.toLocaleString('en-us', { hour: '2-digit', minute: '2-digit' });
+  if (startDate.getDate() === endDate.getDate()) {
+    return `${startWeekDay}, ${startDateDateString}, ${stateDateTimeString} - ${endDateTimeString}`;
+  } else {
+    return `${startWeekDay}, ${startDateDateString} - ${endDateDateString}, ${stateDateTimeString} - ${endDateTimeString}`;
+  }
+};
+
+const EventDetails = ({ event }: EventDetailsProps): JSX.Element => {
+  return (
+    <EventDetailTd colSpan={4}>
+      <EventDetailName>{event.name}</EventDetailName>
+      <EventDetailDate>{EventDetailTime(event.start, event.end)}</EventDetailDate>
+      <EventDetailDescription>{event.description}</EventDetailDescription>
+      <GoACheckbox name="event-detail-is-all-day" checked={event?.isAllDay} text={'All day '} disabled={true} />
+      <GoACheckbox name="event-detail-is-public" checked={event?.isPublic} text={'Public '} disabled={true} />
+    </EventDetailTd>
+  );
 };
 
 const EventListRow = ({ event, calendarName }: EventListRowProps): JSX.Element => {
   const dispatch = useDispatch();
+  const [showDetails, setShowDetails] = useState(false);
   return (
-    <tr>
-      <td>{event?.name}</td>
-      <td>
-        <div>
-          <span style={dateTitleSpanStyle}>From</span>: {eventDateFormat(event.start)}
-        </div>
-        <div>
-          <span style={dateTitleSpanStyle}>to</span>: {eventDateFormat(event.end)}
-        </div>
-      </td>
-      <td>{event.description}</td>
-      <td headers="calendar-events-actions" data-testid="calendar-selected-events-actions">
-        {
-          <div style={{ display: 'flex' }}>
-            <GoAContextMenuIcon
-              testId="add-attendance-icon"
-              title="add-attendance"
-              type="person-circle"
-              // eslint-disable-next-line
-              onClick={() => {}}
-            />
+    <>
+      <tr>
+        <td>{event?.name}</td>
+        <td>{eventDateFormat(event.start)}</td>
+        <td>{eventDateFormat(event.end)}</td>
+        <td headers="calendar-events-actions" data-testid="calendar-selected-events-actions">
+          {
+            <div style={{ display: 'flex' }}>
+              <GoAContextMenuIcon
+                type={showDetails ? 'eye-off' : 'eye'}
+                onClick={() => setShowDetails(!showDetails)}
+                testId="directory-toggle-details-visibility"
+              />
 
-            <GoAContextMenuIcon
-              type="create"
-              title="Edit"
-              testId={`calendar-event-${event.id}`}
-              onClick={() => {
-                dispatch(
-                  UpdateModalState({
-                    type: EventAddEditModalType,
-                    id: `${event.id}`,
-                    isOpen: true,
-                  })
-                );
-              }}
-            />
-            <GoAContextMenuIcon
-              testId="delete-icon"
-              title="Delete"
-              type="trash"
-              onClick={() => {
-                dispatch(
-                  UpdateModalState({
-                    type: EventDeleteModalType,
-                    id: `${event.id}`,
-                    isOpen: true,
-                  })
-                );
-              }}
-            />
-          </div>
-        }
-      </td>
-    </tr>
+              <GoAContextMenuIcon
+                type="create"
+                title="Edit"
+                testId={`calendar-event-${event.id}`}
+                onClick={() => {
+                  dispatch(
+                    UpdateModalState({
+                      type: EventAddEditModalType,
+                      id: `${event.id}`,
+                      isOpen: true,
+                    })
+                  );
+                }}
+              />
+              <GoAContextMenuIcon
+                testId="delete-icon"
+                title="Delete"
+                type="trash"
+                onClick={() => {
+                  dispatch(
+                    UpdateModalState({
+                      type: EventDeleteModalType,
+                      id: `${event.id}`,
+                      isOpen: true,
+                    })
+                  );
+                }}
+              />
+            </div>
+          }
+        </td>
+      </tr>
+      {showDetails && (
+        <EventDetailRow>
+          <EventDetails event={event} />
+        </EventDetailRow>
+      )}
+    </>
   );
 };
 
@@ -101,14 +141,16 @@ export const EventList = ({ calendarName }: EventListProps): JSX.Element => {
 
   return (
     <>
+      <TitleSpace />
+      <h2>Event list</h2>
       <DeleteModal calendarName={calendarName} />
-      <GoATable testId="calendar-selected-event-table">
+      <DataTable testId="calendar-selected-event-table">
         <thead>
           <tr>
             <th>Name</th>
-            <th>Event time</th>
-            <th>Description</th>
-            <th>Actions</th>
+            <th>Start time</th>
+            <th>End time</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
@@ -116,7 +158,7 @@ export const EventList = ({ calendarName }: EventListProps): JSX.Element => {
             return <EventListRow event={event} calendarName={calendarName} key={`calendar-event-row-${event.id}`} />;
           })}
         </tbody>
-      </GoATable>
+      </DataTable>
     </>
   );
 };
--- apps/tenant-management-webapp/src/app/pages/admin/services/calendar/events/styled-components.ts
+++ apps/tenant-management-webapp/src/app/pages/admin/services/calendar/events/styled-components.ts
@@ -4,3 +4,49 @@ export const SelectCalendarHeader = styled.div`
   margin-bottom: 1rem;
   font-weight: var(--fw-bold);
 `;
+
+export const TitleSpace = styled.div`
+  height: var(--goa-spacing-m);
+`;
+
+export const EventDetail = styled.div`
+  background: #f3f3f3;
+  white-space: pre-wrap;
+  font-family: monospace;
+  font-size: 12px;
+  line-height: 16px;
+  text-align: left;
+  padding: 16px;
+`;
+
+export const EventDetailRow = styled.tr`
+  background: #f3f3f3 !important;
+  .event-name: {
+    font-size: var(--goa-font-size-7);
+  }
+`;
+
+export const EventDetailTd = styled.td`
+  padding-top: var(--goa-spacing-l) !important;
+  padding-right: var(--goa-spacing-l) !important;
+  padding-bottom: calc(var(--goa-spacing-l) - 10px)) !important;
+  padding-left: var(--goa-spacing-l) !important;
+`;
+
+export const EventDetailName = styled.div`
+  font-size: var(--goa-font-size-4);
+  padding-bottom: 0.875rem;
+`;
+
+export const EventDetailDescription = styled.div`
+  color: var(--goa-color-greyscale-700);
+  padding-bottom: calc(var(--goa-spacing-m) - 10px);
+  font-size: var(--goa-font-size-3);
+  line-height: var(--var);
+`;
+
+export const EventDetailDate = styled.div`
+  font-size: var(--goa-font-size-4);
+  color: var(--goa-color-greyscale-700);
+  padding-bottom: var(--goa-spacing-m);
+`;
