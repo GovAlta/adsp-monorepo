72b3ee23b661ee2fd191cf7826c05c868a57baac
RamaChandiniDevarapalli <rama.devarapalli@gov.ab.ca>
2023-10-19 09:55:02 -0600

    Fix : Revert migration change to comment

--- apps/comment-service/migrations/20230823152146_create_comment_tables.js
+++ apps/comment-service/migrations/20230823152146_create_comment_tables.js
@@ -3,49 +3,44 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  knex.schema.hasTable('topics').then((exists) => {
-    if (!exists) {
-      return knex.schema.createTable('topics', function (table) {
-        // Postgres versions 10+
-        // table.specificType('id', 'integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY');
-        table.increments('id');
-        table.string('tenant').notNullable();
-        table.string('type').notNullable();
-        table.string('resource').index();
-        table.string('name').notNullable();
-        table.string('description');
-        table.specificType('commenters', 'text[]');
+  const schema = knex.schema.createTable('topics', function (table) {
+    // Postgres versions 10+
+    // table.specificType('id', 'integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY');
+    table.increments('id');
+    table.string('tenant').notNullable();
+    table.string('type').notNullable();
+    table.string('resource').index();
+    table.string('name').notNullable();
+    table.string('description');
+    table.specificType('commenters', 'text[]');
 
-        table.unique(['tenant', 'id']);
-        table.index(['tenant', 'type']);
-      });
-    }
+    table.unique(['tenant', 'id']);
+    table.index(['tenant', 'type']);
   });
-  knex.schema.hasTable('comments').then((exists) => {
-    if (!exists) {
-      return knex.schema.createTable('comments', function (table) {
-        // Postgres versions 10+
-        // table.specificType('id', 'integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY');
-        table.increments('id');
-        table.string('tenant').notNullable();
-        table.integer('topic_id').notNullable();
-        table.string('title').notNullable();
-        table.string('content');
-        table.string('createdById').notNullable();
-        table.string('createdByName').notNullable();
-        table.timestamp('createdOn').notNullable();
-        table.string('lastUpdatedById').notNullable();
-        table.string('lastUpdatedByName').notNullable();
-        table.timestamp('lastUpdatedOn').notNullable();
 
-        table.unique(['tenant', 'topic_id', 'id']);
-        table.index(null, 'idx_title_full_text', "gin(to_tsvector('english', title)); SELECT NOW");
-        table.index(null, 'idx_content_full_text', "gin(to_tsvector('english', content)); SELECT NOW");
+  schema.createTable('comments', function (table) {
+    // Postgres versions 10+
+    // table.specificType('id', 'integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY');
+    table.increments('id');
+    table.string('tenant').notNullable();
+    table.integer('topic_id').notNullable();
+    table.string('title').notNullable();
+    table.string('content');
+    table.string('createdById').notNullable();
+    table.string('createdByName').notNullable();
+    table.timestamp('createdOn').notNullable();
+    table.string('lastUpdatedById').notNullable();
+    table.string('lastUpdatedByName').notNullable();
+    table.timestamp('lastUpdatedOn').notNullable();
 
-        table.foreign('topic_id').references('id').on('topics').onDelete('CASCADE');
-      });
-    }
+    table.unique(['tenant', 'topic_id', 'id']);
+    table.index(null, 'idx_title_full_text', "gin(to_tsvector('english', title)); SELECT NOW");
+    table.index(null, 'idx_content_full_text', "gin(to_tsvector('english', content)); SELECT NOW");
+
+    table.foreign('topic_id').references('id').on('topics').onDelete('CASCADE');
   });
+
+  return schema;
 };
 
 /**
