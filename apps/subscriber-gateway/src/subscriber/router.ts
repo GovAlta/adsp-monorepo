5b312cdf93a3d060899165436526d20cddd35503
Paul.Li <paul.li@gov.ab.ca>
2023-10-25 13:41:38 -0600

    Update the subscriber app validation

--- apps/subscriber-gateway/src/subscriber/router.ts
+++ apps/subscriber-gateway/src/subscriber/router.ts
@@ -209,7 +209,7 @@ export const createSubscriberRouter = ({
   router.post(
     '/application-status',
     verifyCaptcha(logger, RECAPTCHA_SECRET),
-    createValidationHandler(body('tenantName').isString(), body('email').isEmail()),
+    createValidationHandler(body('tenant').isString(), body('email').isEmail()),
     subscribeStatus(tenantService, tokenProvider, directory, logger)
   );
 
