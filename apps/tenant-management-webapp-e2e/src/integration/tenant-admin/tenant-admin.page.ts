6e256301dc2fe1363057908495aec1ffc002eee3
Howard Zhang <howard_zhang@hotmail.com>
2023-10-29 22:53:44 -0600

    e2e test update on load more button UI change on event log (#2234)
    * Updates
    
    * Test updates for button updates
    
    * More updates for buttons
    
    * Button updates
    
    * Test updates for UI components
    
    * Updates for event log, status, notifications
    
    * e2e updates
    
    * File service, configuration service, welcome
    
    * Updates on PDF, script and notifications
    
    * subscription and service status updates
    
    * Updates on events and directory
    
    * e2e updates for status, directory and tenant admin
    
    * directory and service status
    
    * Notifications test updates
    
    * One comment for roles in notification type modal
    
    * e2e test updates for status and notifications
    
    * Changed var to let
    
    * e2e test updates for configuration
    
    * e2e test updates for welcome page
    
    * Removed unused element
    
    * e2e updates
    
    * test updates for events and script
    
    * e2e updates on welcome page and notifications
    
    * Fixes for service status
    
    * updates for file service
    
    * e2e test updates for event, file, notification
    
    * Update for pdf
    
    * update for status
    
    * Update
    
    * e2e updates
    
    * e2e updates
    
    * e2e updates
    
    * e2e updates
    
    * Added a config setting
    
    * Commented out edit PDF template steps
    
    * Update
    
    * update
    
    * Updated file tests
    
    * Updated pdf test
    
    * Removed headless=true command parameter
    
    * Switch e2e test run from Chrome to Electron
    
    * Switch e2e test run from headless to headed
    
    * e2e fixes
    
    * e2e updates
    
    * e2e updates
    
    * Test fixes
    
    * Updated cs-322 with validation of open icon
    
    * Added validation of pdf template body
    
    * Added a couple of steps of selecting filters
    
    * Added a walkaround
    
    * Walkaround for 30 second refresh
    
    * Added more wait to avoid random test failures
    
    * File service test updates
    
    * Put accessiblity test step back
    
    * Commented out accessibility step for file types
    
    * Added support info validation on subscriber app
    
    * Fixed a timestamp bug
    
    * Added more delay on file type name
    
    * test updates
    
    * subscriber app test update
    
    * Updated load more button page object
    
    ---------
    
    Co-authored-by: Howard Zhang <howard.zhang@gov.ab.ca>

--- apps/tenant-management-webapp-e2e/src/integration/tenant-admin/tenant-admin.page.ts
+++ apps/tenant-management-webapp-e2e/src/integration/tenant-admin/tenant-admin.page.ts
@@ -118,7 +118,7 @@ class TenantAdminPage {
   }
 
   eventLoadMoreBtn() {
-    return cy.get('goa-button:contains("Load more...")');
+    return cy.get('goa-button:contains("Load more")');
   }
 
   eventToggleDetailsIcons() {
