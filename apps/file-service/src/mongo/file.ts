1ef368898d03311cf086f3b5af81cdbc6fa4ab32
thdlam <47620431+thdlam@users.noreply.github.com>
2023-11-03 19:34:37 -0600

    Remove file type securityclassification from file

--- apps/file-service/src/mongo/file.ts
+++ apps/file-service/src/mongo/file.ts
@@ -90,12 +90,6 @@ export class MongoFileRepository implements FileRepository {
   }
 
   fromDoc(type: FileTypeEntity, values: FileDoc): FileEntity {
-    let typeSecurityClassification = '';
-
-    if (values.typeId && type.securityClassification !== undefined) {
-      typeSecurityClassification = type.securityClassification;
-    }
-
     return values
       ? new FileEntity(this.storageProvider, this, type, {
           tenantId: values.spaceId ? AdspId.parse(values.spaceId) : null,
@@ -110,7 +104,7 @@ export class MongoFileRepository implements FileRepository {
           scanned: values.scanned,
           infected: values.infected,
           deleted: values.deleted,
-          securityClassification: typeSecurityClassification,
+          securityClassification: values.securityClassification,
         })
       : null;
   }
