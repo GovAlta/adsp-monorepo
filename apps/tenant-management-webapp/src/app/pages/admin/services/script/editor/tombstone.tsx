fd9c4d0c09a056bc7c221c4902973fd43bbf7b55
RamaChandiniDevarapalli <rama.devarapalli@gov.ab.ca>
2023-11-06 11:42:56 -0700

    Feat: redesign of script editor

---
+++ apps/tenant-management-webapp/src/app/pages/admin/services/script/editor/tombstone.tsx
@@ -0,0 +1,126 @@
+import React, { FunctionComponent, useEffect, useRef, useState } from 'react';
+
+import { TombStoneWrapper, Edit, Tooltip } from '../styled-components';
+import { ScriptItem } from '@store/script/models';
+import { GoAIconButton } from '@abgov/react-components-new';
+import { AddScriptModal } from '../addScriptModal';
+
+import { useSelector } from 'react-redux';
+import { tenantRolesAndClients } from '@store/sharedSelectors/roles';
+
+interface props {
+  selectedScript: ScriptItem;
+  onSave;
+}
+export const TombStone: FunctionComponent<props> = ({ selectedScript, onSave }) => {
+  const [openAddScript, setOpenAddScript] = useState(false);
+  const tenant = useSelector(tenantRolesAndClients);
+  const [isNameElipsisActive, setIsNameElipsisActive] = useState(false);
+  const tooltipNameElem = useRef<HTMLParagraphElement>();
+  const [isIdElipsisActive, setIsIdElipsisActive] = useState(false);
+  const tooltipIdElem = useRef<HTMLParagraphElement>();
+  const [isDescElipsisActive, setIsDescElipsisActive] = useState(false);
+  const tooltipDescElem = useRef<HTMLParagraphElement>();
+  useEffect(() => {
+    const elem = tooltipNameElem.current;
+    setIsNameElipsisActive(elem ? elem.offsetWidth < elem.scrollWidth || elem.offsetHeight < elem.scrollHeight : false);
+  }, []);
+  useEffect(() => {
+    const elem = tooltipNameElem.current;
+    setIsIdElipsisActive(elem ? elem.offsetWidth < elem.scrollWidth || elem.offsetHeight < elem.scrollHeight : false);
+  }, []);
+  useEffect(() => {
+    const elem = tooltipNameElem.current;
+    setIsDescElipsisActive(elem ? elem.offsetWidth < elem.scrollWidth || elem.offsetHeight < elem.scrollHeight : false);
+  }, []);
+  return (
+    <TombStoneWrapper data-testid="task-config-form">
+      <div className="nameColumn">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+            </tr>
+            <tr>
+              <td data-testid="queue-namespace">
+                <Tooltip>
+                  <div className="overflowContainer" ref={tooltipNameElem}>
+                    {selectedScript.name}
+                  </div>
+                  {isNameElipsisActive && <p>{selectedScript.name}</p>}
+                </Tooltip>
+              </td>
+            </tr>
+          </thead>
+        </table>
+      </div>
+      <div className="separator"></div>
+      <div className="nameColumn">
+        <table>
+          <thead>
+            <tr>
+              <th>Sctipt ID</th>
+            </tr>
+            <tr>
+              <td data-testid="queue-namespace">
+                <Tooltip>
+                  <div className="overflowContainer" ref={tooltipIdElem}>
+                    {selectedScript.id}
+                  </div>
+                  {isIdElipsisActive && <p>{selectedScript.id}</p>}
+                </Tooltip>
+              </td>
+            </tr>
+          </thead>
+        </table>
+      </div>
+      <div className="separator"></div>
+      <div className="descColumn">
+        <table>
+          <thead>
+            <tr>
+              <th>Description</th>
+            </tr>
+            <tr>
+              <td data-testid="queue-name">
+                <Tooltip>
+                  <div className="overflowContainer" ref={tooltipDescElem}>
+                    {selectedScript.description}
+                  </div>
+                  {isDescElipsisActive && <p>{selectedScript.description}</p>}
+                </Tooltip>
+              </td>
+            </tr>
+          </thead>
+        </table>
+      </div>
+      <div className="editColumn">
+        <Edit>
+          <a rel="noopener noreferrer" onClick={() => setOpenAddScript(true)}>
+            Edit
+          </a>
+          <GoAIconButton
+            icon="create"
+            testId="form-template-information-edit-icon"
+            title="Edit"
+            size="small"
+            onClick={() => setOpenAddScript(true)}
+          />
+        </Edit>
+      </div>
+      {openAddScript && (
+        <AddScriptModal
+          open={openAddScript}
+          isNew={false}
+          initialValue={selectedScript}
+          realmRoles={tenant.realmRoles}
+          tenantClients={tenant.tenantClients ? tenant.tenantClients : {}}
+          onCancel={() => {
+            setOpenAddScript(false);
+          }}
+          onSave={onSave}
+        />
+      )}
+    </TombStoneWrapper>
+  );
+};
--- apps/tenant-management-webapp/src/app/pages/admin/services/script/styled-components.ts
+++ apps/tenant-management-webapp/src/app/pages/admin/services/script/styled-components.ts
@@ -59,7 +59,8 @@ export const MonacoDivBody = styled.div`
   border-radius: 3px;
   padding: 0.15rem 0.15rem;
   min-height: 65px;
-  height: calc(92vh - 310px);
+  height: calc(72vh - 310px);
+  margin-bottom: 1rem;
 `;
 export const TestInputDivBody = styled.div`
   display: flex;
@@ -68,27 +69,29 @@ export const TestInputDivBody = styled.div`
   padding: 0.15rem 0.15rem;
   min-height: 65px;
   height: calc(57vh - 310px);
+  margin-top: -1.5rem;
 `;
 export const ScriptPanelContainer = styled.div`
   display: flex;
   flex: auto;
-  padding-left: 3rem;
-  width: 90%;
+  padding-left: 2rem;
+  padding-right: 2rem;
   height: 100vh;
+  margin-top: 90px;
   overflow: hidden;
   box-sizing: border-box;
 `;
 export const EditScriptActions = styled.div`
   display: flex;
-  justify-content: right;
+  justify-content: left;
   gap: 1rem;
-  padding-top: 1rem;
+  padding-top: 1.5rem;
+  border-top: 1px solid #adadad;
 `;
 
 export const ScriptEditorContainer = styled.div`
   width: 50%;
   padding-right: 1rem;
-  padding-left: 1rem;
   overflow: hidden;
   &:hover {
     overflow: auto;
@@ -113,7 +116,7 @@ export const ScriptEditorContainer = styled.div`
 export const EditModalStyle = styled.div`
   width: 100%;
   display: flex;
-  padding-top: 1rem;
+  padding-top: 2rem;
   .half-width {
     width: 50%;
     display: flex;
@@ -156,6 +159,10 @@ export const EditModalStyle = styled.div`
     color: #333;
     font-size: var(--fs-base);
     line-height: calc(var(--fs-base) + 1rem);
+  }
+
+  .hr-resize {
+    margin-top: var(--goa-spacing-s);
   }
 `;
 
@@ -215,7 +222,7 @@ export const ResponseTableStyles = styled.div`
     width: 40%;
   }
   td {
-    padding: 0.5rem;
+    padding: 0.75rem 0 0.5rem;
   }
 
   tr + tr {
@@ -259,5 +266,135 @@ export const ResponseTableStyles = styled.div`
   .mt-3 {
     margin-left: 1rem;
     margin-right: 1rem;
+  }
+`;
+
+export const TombStoneWrapper = styled.div`
+  margin-top: 2rem;
+  font-size: 16px;
+  color: #333333;
+  border-radius: 3px;
+  height: 7.375rem;
+  background-color: #f1f1f1;
+  border: 1px solid #adadad;
+
+  display: flex;
+  margin-bottom: 1.5rem;
+  .nameColumn {
+    width: 92px;
+    float: left;
+    margin: 1rem;
+  }
+  .idColumn {
+    width: 94px;
+    float: left;
+    height: 100%;
+    margin: 1rem;
+  }
+  .descColumn {
+    width: calc(100% - 304px);
+    float: left;
+    margin: 1rem;
+  }
+  .editColumn {
+    width: 57px;
+    float: right;
+    min-width: 50px;
+    margin-top: 1rem;
+    margin-right: 1rem;
+  }
+  .separator {
+    margin-top: 1rem;
+    width: 1px;
+    height: 5.375rem;
+
+    border-left: 1px solid #ccc;
+  }
+  .hideOverflow {
+    overflow: hidden;
+  }
+  table {
+  }
+  th {
+    text-align: left;
+    padding-bottom: 0.5rem;
+    font-size: 18px;
+  }
+
+  .overflowContainer {
+    height: 48px;
+    overflow: hidden;
+    vertical-align: top;
+    width: 100%;
+    -webkit-box-orient: vertical;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    font-size: 16px;
+    font-weight: 400;
+    font-family: 'acumin-pro-semi-condensed';
+    line-height: 24px;
+    word-wrap: break-word;
+    word-break: break-word;
+  }
+`;
+
+export const TaskEditorTitle = styled.div`
+  font-size: var(--fs-xl);
+  line-height: var(--lh-lg);
+  font-weight: var(--fw-regular);
+`;
+
+export const Edit = styled.div`
+  .flexRow {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .badgePadding {
+    margin: 6px 0 0 5px;
+  }
+
+  a {
+    margin-right: 4px;
+    text-decoration: underline;
+    line-height: 28px;
+    font-size: 18px;
+  }
+  display: flex;
+  flex-direction: row;
+`;
+export const ScrollPane = styled.div`
+  overflow-y: scroll;
+  max-height: calc(100vh - 230px);
+  width: 100%;
+`;
+export const TextLoadingIndicator = styled.div`
+  animation: blinker 1s linear infinite;
+  font-size: 16px;
+  font-style: italic;
+  text-align: center;
+  @keyframes blinker {
+    50% {
+      opacity: 0;
+    }
+  }
+`;
+
+export const Tooltip = styled.div`
+  position: relative;
+  p {
+    display: none;
+    position: absolute;
+  }
+  &:hover p {
+    display: block;
+    margin: 0;
+    padding: 3px 7px;
+    background: #ffffff;
+    border: 1px solid grey;
+    bottom: -37px;
+    width: max-content;
+    min-width: 200px;
+    font-size: 16px;
   }
 `;
