4ba3769a7913b3743ec5ee3d1b8c00e1bcfbd835
Ting Zuge <ting.zuge@gov.ab.ca>
2023-11-09 11:09:46 -0700

    chore(push-service): refactoring web-hooks for test coverage

---
+++ apps/push-service/src/push/model/utils.ts
@@ -0,0 +1,22 @@
+export function isValidUrl(urlString: string): boolean {
+  let url;
+  try {
+    url = new URL(urlString);
+  } catch (e) {
+    return false;
+  }
+  return url.protocol === 'http:' || url.protocol === 'https:';
+}
+
+export function getCircularReplacer() {
+  const seen = new WeakSet();
+  return (_key: string, value: unknown) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+}
