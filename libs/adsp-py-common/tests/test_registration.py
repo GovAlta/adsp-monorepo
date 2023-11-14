75fa7d6a0c5a1f9319507c1640f3f01dc15043ea
Ting Zuge <ting.zuge@gov.ab.ca>
2023-11-14 15:12:13 -0700

    fix(adsp-service-flask-sdk, adsp-service-django-sdk): using correct method in registration
    Also fixing metadata view in SDK for Django.

--- libs/adsp-py-common/tests/test_registration.py
+++ libs/adsp-py-common/tests/test_registration.py
@@ -28,7 +28,7 @@ def test_register():
 
 
 def test_register_configuration():
-    with patch("adsp_py_common.registration.post") as mock_post:
+    with patch("adsp_py_common.registration.patch") as mock_post:
         directory = Mock(ServiceDirectory)
         directory.get_service_url.return_value = "https://tenant-service"
         token_provider = Mock(TokenProvider)
@@ -48,7 +48,7 @@ def test_register_configuration():
 
 
 def test_register_roles():
-    with patch("adsp_py_common.registration.post") as mock_post:
+    with patch("adsp_py_common.registration.patch") as mock_post:
         directory = Mock(ServiceDirectory)
         directory.get_service_url.return_value = "https://tenant-service"
         token_provider = Mock(TokenProvider)
@@ -66,7 +66,7 @@ def test_register_roles():
 
 
 def test_register_events():
-    with patch("adsp_py_common.registration.post") as mock_post:
+    with patch("adsp_py_common.registration.patch") as mock_post:
         directory = Mock(ServiceDirectory)
         directory.get_service_url.return_value = "https://tenant-service"
         token_provider = Mock(TokenProvider)
@@ -88,7 +88,7 @@ def test_register_events():
 
 
 def test_register_streams():
-    with patch("adsp_py_common.registration.post") as mock_post:
+    with patch("adsp_py_common.registration.patch") as mock_post:
         directory = Mock(ServiceDirectory)
         directory.get_service_url.return_value = "https://tenant-service"
         token_provider = Mock(TokenProvider)
@@ -108,7 +108,7 @@ def test_register_streams():
 
 
 def test_register_file_types():
-    with patch("adsp_py_common.registration.post") as mock_post:
+    with patch("adsp_py_common.registration.patch") as mock_post:
         directory = Mock(ServiceDirectory)
         directory.get_service_url.return_value = "https://tenant-service"
         token_provider = Mock(TokenProvider)
@@ -126,7 +126,7 @@ def test_register_file_types():
 
 
 def test_register_request_error():
-    with patch("adsp_py_common.registration.post") as mock_post:
+    with patch("adsp_py_common.registration.patch") as mock_post:
         directory = Mock(ServiceDirectory)
         directory.get_service_url.return_value = "https://tenant-service"
         token_provider = Mock(TokenProvider)
--- libs/adsp-service-django-sdk/adsp_service_django_sdk/metadata.py
+++ libs/adsp-service-django-sdk/adsp_service_django_sdk/metadata.py
@@ -1,34 +1,24 @@
-from django.http import HttpResponse
-from urllib.parse import urlparse
+from django.http import JsonResponse
 
 from ._setup import adsp
 
 
 def get_metadata(request):
-    (
-        service_id,
-        display_name,
-        description,
-    ) = adsp.registration
-    request_url = urlparse(request.url)
-    links = {"self": request.url}
+    service_id, display_name, description, *_ = adsp.registration
+    links = {"self": request.build_absolute_uri()}
 
     if adsp.registration.health_endpoint_path:
-        links[
-            "health"
-        ] = f"{request_url.scheme}://{request_url.netloc}{adsp.registration.health_endpoint_path}"
+        links["health"] = request.build_absolute_uri(
+            adsp.registration.health_endpoint_path
+        )
 
     if adsp.registration.docs_endpoint_path:
-        links[
-            "docs"
-        ] = f"{request_url.scheme}://{request_url.netloc}{adsp.registration.docs_endpoint_path}"
+        links["docs"] = request.build_absolute_uri(adsp.registration.docs_endpoint_path)
 
     if adsp.registration.api_endpoint_path:
-        links[
-            "api"
-        ] = f"{request_url.scheme}://{request_url.netloc}{adsp.registration.api_endpoint_path}"
+        links["api"] = request.build_absolute_uri(adsp.registration.api_endpoint_path)
 
-    return HttpResponse(
+    return JsonResponse(
         {
             "name": display_name or service_id.service,
             "description": description,
