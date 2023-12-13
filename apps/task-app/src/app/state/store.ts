7c7636855e4fedffccdfc53fbf612d3da097de37
Ting Zuge <ting.zuge@gov.ab.ca>
2023-12-13 08:27:30 -0700

    feat(task-app): setting up and provisioning resources for a task app

---
+++ apps/task-app/src/app/state/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { CONFIG_FEATURE_KEY, configReducer } from './config.slice';
+import { USER_FEATURE_KEY, userReducer } from './user.slice';
+import { TASK_FEATURE_KEY, taskReducer } from './task.slice';
+
+export const store = configureStore({
+  reducer: {
+    [CONFIG_FEATURE_KEY]: configReducer,
+    [USER_FEATURE_KEY]: userReducer,
+    [TASK_FEATURE_KEY]: taskReducer,
+  },
+  devTools: process.env.NODE_ENV !== 'production',
+  // Optional Redux store enhancers
+  enhancers: [],
+  middleware: (getDefault) => getDefault(),
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type AppState = ReturnType<typeof store.getState>;
