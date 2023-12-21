c75574b9972cd3dba772465f1e253c7603de88c3
Ting Zuge <ting.zuge@gov.ab.ca>
2023-12-20 20:41:52 -0700

    feat(task-app): move feedback notification to separate state slice

--- apps/task-app/src/app/state/store.ts
+++ apps/task-app/src/app/state/store.ts
@@ -4,14 +4,16 @@ import { USER_FEATURE_KEY, userReducer } from './user.slice';
 import { TASK_FEATURE_KEY, taskReducer } from './task.slice';
 import { QUEUE_FEATURE_KEY, queueReducers } from './queue.slice';
 import { FILE_FEATURE_KEY, fileReducer } from './file.slice';
+import { FEEDBACK_FEATURE_KEY, feedbackReducer } from './feedback.slice';
 
 export const store = configureStore({
   reducer: {
     [CONFIG_FEATURE_KEY]: configReducer,
+    [FEEDBACK_FEATURE_KEY]: feedbackReducer,
+    [FILE_FEATURE_KEY]: fileReducer,
     [USER_FEATURE_KEY]: userReducer,
     [QUEUE_FEATURE_KEY]: queueReducers,
     [TASK_FEATURE_KEY]: taskReducer,
-    [FILE_FEATURE_KEY]: fileReducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
   // Optional Redux store enhancers
