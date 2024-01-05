c2196b551c85634b339d79451fc7f48139b989c5
Ting Zuge <ting.zuge@gov.ab.ca>
2024-01-05 08:40:41 -0700

    fix(comment-service): making comment title optional
    Also updating column types so content length is unlimited.

---
+++ apps/comment-service/migrations/20240105153554_change_content_type.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('comments', (table) => {
+    table.text('content').nullable().alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('comments', (table) => {
+    table.string('content').nullable().alter();
+  });
+};
