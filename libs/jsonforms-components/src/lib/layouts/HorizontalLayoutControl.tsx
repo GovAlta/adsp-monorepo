9c57efc8eb740fd057ff56bfb5c02024b66a7add
athena-chen-chen <athena.chen@gov.ab.ca>
2024-01-09 11:04:27 -0700

    feat:jsonforms-components includes GoA design system aligned Horizontal and Vertical layouts

---
+++ libs/jsonforms-components/src/lib/layouts/HorizontalLayoutControl.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from 'react';
+
+import { LayoutProps, RankedTester, rankWith, uiTypeIs, HorizontalLayout } from '@jsonforms/core';
+import { LayoutRenderer, LayoutRendererProps } from '../util/layout';
+import { withJsonFormsLayoutProps } from '@jsonforms/react';
+
+export const materialHorizontalLayoutTester: RankedTester = rankWith(2, uiTypeIs('HorizontalLayout'));
+
+export const GoAHorizontalLayout = ({ uischema, renderers, cells, schema, path, enabled }: LayoutProps) => {
+  const layout = uischema as HorizontalLayout;
+  const childProps: LayoutRendererProps = {
+    elements: layout.elements,
+    schema,
+    path,
+    enabled,
+    direction: 'row',
+  };
+
+  return <LayoutRenderer {...childProps} renderers={renderers} cells={cells} />;
+};
+
+export default withJsonFormsLayoutProps(GoAHorizontalLayout);
