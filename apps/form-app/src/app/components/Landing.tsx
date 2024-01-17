b2c498b9349958297dd4c71b4baa3661d0a04f56
Ting Zuge <ting.zuge@gov.ab.ca>
2024-01-17 11:03:22 -0700

    chore(form-app): stub project for form app

---
+++ apps/form-app/src/app/components/Landing.tsx
@@ -0,0 +1,31 @@
+import { GoAAppHeader, GoAMicrositeHeader } from '@abgov/react-components-new';
+import { Band, Container, Footer, Grid, GridItem } from '@core-services/app-common';
+import React, { FunctionComponent } from 'react';
+import GoALogo from '../../assets/goa-logo.svg';
+
+export const Landing: FunctionComponent = () => (
+  <React.Fragment>
+    <GoAMicrositeHeader type="alpha" />
+    <GoAAppHeader url="/" heading="Alberta Digital Service Platform - Form" />
+    <Band title="Form">Draft, save and submit information using forms.</Band>
+    <main>
+      <section>
+        <Container vs={3} hs={1}>
+          <Grid>
+            <GridItem md={1} />
+            <GridItem md={10}>
+              <Grid>
+                <GridItem md={12} className="center">
+                  <h2>Overview</h2>
+                  <p></p>
+                </GridItem>
+              </Grid>
+            </GridItem>
+            <GridItem md={1} />
+          </Grid>
+        </Container>
+      </section>
+    </main>
+    <Footer logoSrc={GoALogo} />
+  </React.Fragment>
+);
---
+++ apps/form-app/src/app/state/index.ts
@@ -0,0 +1,6 @@
+export * from './config.slice';
+export * from './feedback.slice';
+export * from './user.slice';
+export * from './types';
+
+export * from './store';
