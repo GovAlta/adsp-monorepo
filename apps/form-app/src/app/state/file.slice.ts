c531039db54ce3608f4b96a947022413d73283fc
Ting Zuge <ting.zuge@gov.ab.ca>
2024-01-24 16:26:39 -0700

    feat(form-app): adding file upload and delete in state slice
    Also adding some error handling in task app file state slice.

--- apps/form-app/src/app/state/file.slice.ts
+++ apps/form-app/src/app/state/file.slice.ts
@@ -1,6 +1,6 @@
 import { AdspId } from '@core-services/app-common';
 import { createAsyncThunk, createSelector, createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+import axios, { AxiosProgressEvent } from 'axios';
 import { AppState } from './store';
 import { getAccessToken } from './user.slice';
 
@@ -30,66 +30,177 @@ async function getFileMetadata(fileServiceUrl: string, urn: string): Promise<Fil
   return data;
 }
 
-export const loadFileMetadata = createAsyncThunk('file/get-file-metadata', async (urn: string, { getState }) => {
-  const { config } = getState() as AppState;
-  const fileServiceUrl = config.directory[FILE_SERVICE_ID];
+export const loadFileMetadata = createAsyncThunk(
+  'file/get-file-metadata',
+  async (urn: string, { getState, rejectWithValue }) => {
+    try {
+      const { config } = getState() as AppState;
+      const fileServiceUrl = config.directory[FILE_SERVICE_ID];
 
-  const metadata = await getFileMetadata(fileServiceUrl, urn);
+      const metadata = await getFileMetadata(fileServiceUrl, urn);
 
-  return metadata;
-});
-
-export const downloadFile = createAsyncThunk('file/download-file', async (urn: string, { getState }) => {
-  const { config, file: fileState } = getState() as AppState;
-  const fileServiceUrl = config.directory[FILE_SERVICE_ID];
-
-  let metadata = fileState.metadata[urn];
-  if (!metadata) {
-    metadata = await getFileMetadata(fileServiceUrl, urn);
-  }
-
-  const token = await getAccessToken();
-  const { data, headers } = await axios.get(
-    new URL(`/file/v1/${urn.substring(FILE_SERVICE_ID.length + 4)}/download`, fileServiceUrl).href,
-    {
-      responseType: 'blob',
-      headers: { Authorization: `Bearer ${token}` },
+      return metadata;
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        return rejectWithValue({
+          status: err.response?.status,
+          message: err.response?.data?.errorMessage || err.message,
+        });
+      } else {
+        throw err;
+      }
     }
-  );
-  const mimeType = headers['content-type']?.toString();
+  }
+);
 
-  const file = await new Promise<string>((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(new File([data], metadata.filename, { type: mimeType }));
-    reader.onload = () => resolve(reader.result as string);
-    reader.onerror = () => reject(reader.error);
-  });
+export const downloadFile = createAsyncThunk(
+  'file/download-file',
+  async (urn: string, { getState, rejectWithValue }) => {
+    try {
+      const { config, file: fileState } = getState() as AppState;
+      const fileServiceUrl = config.directory[FILE_SERVICE_ID];
 
-  return { file, metadata: { ...metadata, mimeType } };
+      let metadata = fileState.metadata[urn];
+      if (!metadata) {
+        metadata = await getFileMetadata(fileServiceUrl, urn);
+      }
+
+      const token = await getAccessToken();
+      const { data, headers } = await axios.get(
+        new URL(`/file/v1/${urn.substring(FILE_SERVICE_ID.length + 4)}/download`, fileServiceUrl).href,
+        {
+          responseType: 'blob',
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      const mimeType = headers['content-type']?.toString();
+
+      const file = await new Promise<string>((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(new File([data], metadata.filename, { type: mimeType }));
+        reader.onload = () => resolve(reader.result as string);
+        reader.onerror = () => reject(reader.error);
+      });
+
+      return { file, metadata: { ...metadata, mimeType } };
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        return rejectWithValue({
+          status: err.response?.status,
+          message: err.response?.data?.errorMessage || err.message,
+        });
+      } else {
+        throw err;
+      }
+    }
+  }
+);
+
+export const uploadFile = createAsyncThunk(
+  'file/upload-file',
+  async (
+    { typeId, file, recordId }: { typeId: string; file: File; recordId: string },
+    { dispatch, getState, rejectWithValue }
+  ) => {
+    try {
+      const { config } = getState() as AppState;
+      const fileServiceUrl = config.directory[FILE_SERVICE_ID];
+
+      const token = await getAccessToken();
+      const formData = new FormData();
+      formData.append('type', typeId);
+      formData.append('recordId', recordId);
+      formData.append('file', file);
+
+      const { data: metadata } = await axios.post<FileMetadata>(
+        new URL('/file/v1/files', fileServiceUrl).href,
+        formData,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+          onUploadProgress: ({ loaded, total }: AxiosProgressEvent) => {
+            const progress = Math.floor((loaded * 100) / total);
+            dispatch(fileActions.setUploadProgress({ name: file.name, progress }));
+          },
+        }
+      );
+
+      // Keep the file in data URL form in the state, so we don't need to download again.
+      const fileDataUrl = await new Promise<string>((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result as string);
+        reader.onerror = () => reject(reader.error);
+      });
+
+      return { metadata, file: fileDataUrl };
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        return rejectWithValue({
+          status: err.response?.status,
+          message: err.response?.data?.errorMessage || err.message,
+        });
+      } else {
+        throw err;
+      }
+    }
+  }
+);
+
+export const deleteFile = createAsyncThunk('file/delete-file', async (urn: string, { getState, rejectWithValue }) => {
+  try {
+    const { config } = getState() as AppState;
+    const fileServiceUrl = config.directory[FILE_SERVICE_ID];
+
+    const token = await getAccessToken();
+
+    const filePath = urn.split(':').pop();
+    const { data } = await axios.delete<{ deleted: boolean }>(new URL(`/file/v1${filePath}`, fileServiceUrl).href, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    return data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      return rejectWithValue({
+        status: err.response?.status,
+        message: err.response?.data?.errorMessage || err.message,
+      });
+    } else {
+      throw err;
+    }
+  }
 });
 
 interface FileState {
   files: Record<string, string>;
   metadata: Record<string, FileMetadata>;
+  upload: { name: string; progress: number };
   busy: {
     download: Record<string, boolean>;
     metadata: Record<string, boolean>;
+    uploading: boolean;
   };
 }
 
 const initialFileState: FileState = {
   files: {},
   metadata: {},
+  upload: null,
   busy: {
     download: {},
     metadata: {},
+    uploading: false,
   },
 };
 
 const fileSlice = createSlice({
   name: FILE_FEATURE_KEY,
   initialState: initialFileState,
-  reducers: {},
+  reducers: {
+    setUploadProgress: (state, { payload }: { payload: { name: string; progress: number } }) => {
+      state.upload = payload;
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(loadFileMetadata.pending, (state, { meta }) => {
@@ -112,9 +223,27 @@ const fileSlice = createSlice({
       })
       .addCase(downloadFile.rejected, (state, { meta }) => {
         state.busy.download[meta.arg] = false;
+      })
+      .addCase(uploadFile.pending, (state, { meta }) => {
+        state.busy.uploading = true;
+        state.upload = { name: meta.arg.file.name, progress: 0 };
+      })
+      .addCase(uploadFile.fulfilled, (state, { payload }) => {
+        state.busy.uploading = false;
+        state.files[payload.metadata.urn] = payload.file;
+        state.metadata[payload.metadata.urn] = payload.metadata;
+      })
+      .addCase(uploadFile.rejected, (state) => {
+        state.busy.uploading = false;
+      })
+      .addCase(deleteFile.fulfilled, (state, { meta }) => {
+        state.files[meta.arg] = null;
+        state.metadata[meta.arg] = null;
       });
   },
 });
+
+const fileActions = fileSlice.actions;
 
 export const fileReducer = fileSlice.reducer;
 
