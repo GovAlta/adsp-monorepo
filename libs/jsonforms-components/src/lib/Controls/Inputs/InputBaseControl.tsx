24d1eef761b1109bdcadb32ac860f82f2db8ea83
thdlam <47620431+thdlam@users.noreply.github.com>
2024-02-08 12:25:02 -0700

    Add required into input fields

--- libs/jsonforms-components/src/lib/Controls/Inputs/InputBaseControl.tsx
+++ libs/jsonforms-components/src/lib/Controls/Inputs/InputBaseControl.tsx
@@ -37,7 +37,16 @@ export const GoAInputBaseControl = (props: ControlProps & WithInput): JSX.Elemen
     labelToUpdate = label;
   }
 
-  return (
+  return required ? (
+    <GoAFormItem
+      requirement="required"
+      error={errors}
+      label={props?.noLabel === true ? '' : labelToUpdate}
+      helpText={typeof uischema?.options?.help === 'string' ? uischema?.options?.help : ''}
+    >
+      <InnerComponent {...props} />
+    </GoAFormItem>
+  ) : (
     <GoAFormItem
       error={errors}
       label={props?.noLabel === true ? '' : labelToUpdate}
