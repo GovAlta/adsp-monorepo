d9b78cfe065cc444dd3615cb8b81e97ebf7787ef
athena-chen-chen <58053108+athena-chen-chen@users.noreply.github.com>
2024-02-28 12:25:16 -0700

    fix: change margin bottom for help content (#2647)
    * Error creating events by calendar if End Date before Start Date or End time is before Start time
    
    * fix:Status service view includes extraneous top margin above heading
    
    * fix: add and edit notification type cancel state
    
    * fix:Save button in queue editor disables incorrectly on role changes
    
    * feat:form UI schema code completion hints based on the data schema
    
    * fix: scroll in form editor and help content.
    
    * fix:icon accessibilty issue and web-component upgrade issue
    
    * fix: change margin bottom for help content
    
    ---------
    
    Co-authored-by: Shuang chen <shuangchen@AthenaMacBook.local>
    Co-authored-by: Shuang chen <shuangchen@AthenaMacBook.lan>
    Co-authored-by: athena-chen-chen <athena.chen@gov.ab.ca>

--- libs/jsonforms-components/src/lib/Additional/styled-components.ts
+++ libs/jsonforms-components/src/lib/Additional/styled-components.ts
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 export const HelpContentDiv = styled.div`
-  margin-bottom: 1rem;
+  margin-bottom: 1.5rem;
   .label {
     text-align: left;
     font-size: 24px;
     font-weight: bold;
-    margin-bottom: 1.5rem;
+    margin-bottom: 1rem;
   }
   ul {
     margin: 0.5rem 0 0.5rem 0.5rem;
