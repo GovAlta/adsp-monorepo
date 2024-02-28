61eff81423382bdf068f18cfe0eaa858c07722ba
Ting Zuge <ting.zuge@gov.ab.ca>
2024-02-28 10:25:21 -0700

    feat(adsp-service-spring-sdk): allow sub-class and customization of security configuration

--- libs/adsp-service-spring-sdk/src/main/java/ca/ab/gov/alberta/adsp/sdk/access/AccessIssuer.java
+++ libs/adsp-service-spring-sdk/src/main/java/ca/ab/gov/alberta/adsp/sdk/access/AccessIssuer.java
@@ -2,7 +2,7 @@ package ca.ab.gov.alberta.adsp.sdk.access;
 
 import ca.ab.gov.alberta.adsp.sdk.tenant.Tenant;
 
-final class AccessIssuer {
+public final class AccessIssuer {
   private final Tenant tenant;
   private final boolean isCore;
 
