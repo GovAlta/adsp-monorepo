63a46588944118e2d416af9348bffccb3c92d671
Ting Zuge <ting.zuge@gov.ab.ca>
2024-03-25 16:31:16 -0600

    feat(pii-service): adding a service for PII detection

--- apps/pii-service/tests/test_hello.py
+++ apps/pii-service/tests/test_hello.py
@@ -1,8 +1,6 @@
 """Hello unit test module."""
 
-from pii_service.hello import hello
-
 
 def test_hello():
-    """Test the hello function."""
-    assert hello() == "Hello pii-service"
+    """Stub of a unit test"""
+    assert True
