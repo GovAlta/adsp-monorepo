0aa466f73a01627185a8a9b95c11b0dca75c9f9c
Ting Zuge <ting.zuge@gov.ab.ca>
2024-03-26 11:26:03 -0600

    feat(pii-service): adding anonymize endpoint
    Also adding swagger API documentation for the service.

--- apps/pii-service/pii_service/app.py
+++ apps/pii-service/pii_service/app.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from os import environ
+import os
 from typing import Any, Dict
 from adsp_service_flask_sdk import (
     AdspExtension,
@@ -16,8 +16,10 @@ from pii_service.recognizers.ca_passport_recognizer import CaPassportRecognizer
 from pii_service.recognizers.ca_sin_recognizer import CaSinRecognizer
 from presidio_analyzer.analyzer_engine import AnalyzerEngine
 from presidio_analyzer.analyzer_request import AnalyzerRequest
+from presidio_anonymizer import AnonymizerEngine
 from werkzeug.middleware.proxy_fix import ProxyFix
 from werkzeug.exceptions import BadRequest
+import yaml
 
 from pii_service import service_roles
 from pii_service.recognizers import CaPostalCodeRecognizer
@@ -26,7 +28,7 @@ from pii_service.recognizers import CaPostalCodeRecognizer
 load_dotenv()
 
 logging.basicConfig()
-logging.getLogger().setLevel(environ.get("LOG_LEVEL", "DEBUG"))
+logging.getLogger().setLevel(os.environ.get("LOG_LEVEL", "DEBUG"))
 
 
 def convert_config(tenant_config, _) -> Dict[str, Any]:
@@ -41,7 +43,14 @@ if __name__ != "__main__":
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=2, x_proto=2, x_host=2, x_prefix=2)
+proxy_count = int(os.environ.get("PROXY_COUNT", "2"))
+app.wsgi_app = ProxyFix(
+    app.wsgi_app,
+    x_for=proxy_count,
+    x_proto=proxy_count,
+    x_host=proxy_count,
+    x_prefix=proxy_count,
+)
 
 adsp = adsp_extension.init_app(
     app,
@@ -62,26 +71,37 @@ adsp = adsp_extension.init_app(
             )
         ],
         events=[],
+        api_endpoint_path="/swagger/docs/v1",
     ),
     {
-        "ADSP_ACCESS_SERVICE_URL": environ.get(
+        "ADSP_ACCESS_SERVICE_URL": os.environ.get(
             "KEYCLOAK_ROOT_URL", "https://access.adsp-dev.gov.ab.ca"
         ),
-        "ADSP_DIRECTORY_URL": environ.get(
+        "ADSP_DIRECTORY_URL": os.environ.get(
             "DIRECTORY_URL", "https://directory-service.adsp-dev.gov.ab.ca"
         ),
-        "ADSP_SERVICE_ID": environ.get("SERVICE_ID", "urn:ads:platform:pii-service"),
-        "ADSP_CLIENT_SECRET": environ.get("CLIENT_SECRET"),
+        "ADSP_SERVICE_ID": os.environ.get("SERVICE_ID", "urn:ads:platform:pii-service"),
+        "ADSP_CLIENT_SECRET": os.environ.get("CLIENT_SECRET"),
     },
 )
 
 app.register_blueprint(adsp.metadata_blueprint)
 
-engine = AnalyzerEngine()
-engine.registry.add_recognizer(CaBankRecognizer())
-engine.registry.add_recognizer(CaPostalCodeRecognizer())
-engine.registry.add_recognizer(CaPassportRecognizer())
-engine.registry.add_recognizer(CaSinRecognizer())
+analyzer_engine = AnalyzerEngine()
+analyzer_engine.registry.add_recognizer(CaBankRecognizer())
+analyzer_engine.registry.add_recognizer(CaPostalCodeRecognizer())
+analyzer_engine.registry.add_recognizer(CaPassportRecognizer())
+analyzer_engine.registry.add_recognizer(CaSinRecognizer())
+
+anonymizer_engine = AnonymizerEngine()
+
+
+@app.route("/pii/v1/entities", methods=["GET"])
+@require_user(service_roles.ANALYZER)
+def supported_entities():
+    language = request.args.get("language")
+    entities_list = analyzer_engine.get_supported_entities(language)
+    return jsonify(entities_list), 200
 
 
 @app.route("/pii/v1/analyze", methods=["POST"])
@@ -95,7 +115,7 @@ def analyze():
     if not analyzer_request.language:
         raise BadRequest("No language provided")
 
-    recognizer_result_list = engine.analyze(
+    recognizer_result_list = analyzer_engine.analyze(
         text=analyzer_request.text,
         language=analyzer_request.language,
         correlation_id=analyzer_request.correlation_id,
@@ -116,9 +136,54 @@ def analyze():
     )
 
 
-@app.route("/pii/v1/entities", methods=["GET"])
+@app.route("/pii/v1/anonymize", methods=["POST"])
 @require_user(service_roles.ANALYZER)
-def supported_entities():
-    language = request.args.get("language")
-    entities_list = engine.get_supported_entities(language)
-    return jsonify(entities_list), 200
+def anonymize():
+    analyzer_request = AnalyzerRequest(request.get_json())
+
+    if not analyzer_request.text:
+        raise BadRequest("No text provided")
+
+    if not analyzer_request.language:
+        raise BadRequest("No language provided")
+
+    recognizer_result_list = analyzer_engine.analyze(
+        text=analyzer_request.text,
+        language=analyzer_request.language,
+        correlation_id=analyzer_request.correlation_id,
+        score_threshold=analyzer_request.score_threshold,
+        entities=analyzer_request.entities,
+        return_decision_process=analyzer_request.return_decision_process,
+        # ad_hoc_recognizers=analyzer_request.ad_hoc_recognizers,
+        context=analyzer_request.context,
+    )
+
+    anonymizer_result = anonymizer_engine.anonymize(
+        analyzer_request.text, recognizer_result_list
+    )
+
+    return Response(
+        anonymizer_result.to_json(),
+        content_type="application/json",
+    )
+
+
+@app.route("/swagger/docs/v1", methods=["GET"])
+def apiDocs():
+    docs = _load_api_docs()
+    return Response(
+        json.dumps(docs, default=lambda o: o.to_dict()), content_type="application/json"
+    )
+
+
+API_DOCS_ATTR = "_service_api_docs"
+
+
+def _load_api_docs():
+    api_docs = globals().get(API_DOCS_ATTR, None)
+    if api_docs is None:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(os.path.join(dir_path, "resources", "swagger.yml"), "r") as docs_yml:
+            api_docs = yaml.safe_load(docs_yml)
+            globals()[API_DOCS_ATTR] = api_docs
+    return api_docs
