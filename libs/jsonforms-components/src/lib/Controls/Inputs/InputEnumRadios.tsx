9d7619c49ba59817f6fc04f8e1c9cc24288acbe1
Thang Lam <47620431+thdlam@users.noreply.github.com>
2024-04-25 16:08:22 -0600

    fix(form-app): draft form jumping around when there is a long page. (#2876)

--- libs/jsonforms-components/src/lib/Controls/Inputs/InputEnumRadios.tsx
+++ libs/jsonforms-components/src/lib/Controls/Inputs/InputEnumRadios.tsx
@@ -14,7 +14,7 @@ type RadioGroupProp = EnumCellProps & WithClassname & TranslateProps & WithInput
 export const RadioGroup = (props: RadioGroupProp): JSX.Element => {
   const { data, className, id, enabled, schema, uischema, path, handleChange, options, config, label, t } = props;
   const enumData = schema?.enum || [];
-  const appliedUiSchemaOptions = merge({}, config, props.uischema.options, uischema, options);
+  const appliedUiSchemaOptions = merge({}, config, props.uischema.options, options);
   const errorsFormInput = checkFieldValidity(props as ControlProps);
   return (
     <GoARadioGroup
@@ -27,8 +27,8 @@ export const RadioGroup = (props: RadioGroupProp): JSX.Element => {
       onChange={(name: string, value: string) => handleChange(path, value)}
       {...uischema?.options?.componentProps}
     >
-      {enumData.map((value) => {
-        return <GoARadioItem name={value} value={`${value}`} {...appliedUiSchemaOptions} label={value} />;
+      {enumData.map((enumValue) => {
+        return <GoARadioItem name={enumValue} value={`${enumValue}`} {...appliedUiSchemaOptions} label={enumValue} />;
       })}
     </GoARadioGroup>
   );
