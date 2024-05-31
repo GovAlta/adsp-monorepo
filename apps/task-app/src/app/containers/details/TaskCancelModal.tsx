28b2707fc5821b243292c60daf3eeb1b34a71112
thdlam <47620431+thdlam@users.noreply.github.com>
2024-05-31 11:14:38 -0600

    fix(task-app): add missing task cancel modal for form submission

---
+++ apps/task-app/src/app/containers/details/TaskCancelModal.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { GoAButton, GoAModal, GoAButtonGroup } from '@abgov/react-components-new';
+interface taskCancelModalProps {
+  title: string;
+  content?: string | JSX.Element;
+  isOpen: boolean;
+  onYes: () => void;
+  onNo: () => void;
+}
+
+export const TaskCancelModal = ({ isOpen, title, content, onYes, onNo }: taskCancelModalProps) => {
+  return (
+    <GoAModal
+      testId="cancel-task-confirmation"
+      open={isOpen}
+      heading={title}
+      width="640px"
+      actions={
+        <GoAButtonGroup alignment="end">
+          <GoAButton type="secondary" testId="task-cancelNo" onClick={onNo}>
+            No
+          </GoAButton>
+          <GoAButton type="primary" variant="destructive" testId="task-confirmYes" onClick={onYes}>
+            Yes
+          </GoAButton>
+        </GoAButtonGroup>
+      }
+    >
+      {content}
+    </GoAModal>
+  );
+};
