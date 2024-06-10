8d5f6913e59e182937d971e70a6dcf25198bf51b
Ting Zuge <ting.zuge@gov.ab.ca>
2024-06-10 15:56:01 -0600

    chore(form-app): enable log only redux dev tools for production

--- apps/form-app/src/app/state/store.ts
+++ apps/form-app/src/app/state/store.ts
@@ -1,10 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { DevToolsEnhancerOptions, configureStore } from '@reduxjs/toolkit';
 import { CONFIG_FEATURE_KEY, configReducer } from './config.slice';
 import { FEEDBACK_FEATURE_KEY, feedbackReducer } from './feedback.slice';
 import { FILE_FEATURE_KEY, fileReducer } from './file.slice';
 import { USER_FEATURE_KEY, userReducer } from './user.slice';
 import { FORM_FEATURE_KEY, formReducer } from './form.slice';
 import { COMMENT_FEATURE_KEY, commentReducer } from './comment.slice';
+
+// Use a log only configuration for Redux DevTools in production.
+const reduxDevToolsLogOnly: DevToolsEnhancerOptions = {
+  features: {
+    pause: false,
+    lock: false,
+    persist: false,
+    export: false,
+    import: false,
+    jump: false,
+    skip: false,
+    reorder: false,
+    dispatch: false,
+    test: false,
+  },
+};
 
 export const store = configureStore({
   reducer: {
@@ -15,7 +31,7 @@ export const store = configureStore({
     [FORM_FEATURE_KEY]: formReducer,
     [USER_FEATURE_KEY]: userReducer,
   },
-  devTools: process.env.NODE_ENV !== 'production',
+  devTools: process.env.NODE_ENV !== 'production' || reduxDevToolsLogOnly,
   // Optional Redux store enhancers
   enhancers: [],
   middleware: (getDefault) => getDefault(),
