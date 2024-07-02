a7676c8d30fa79b11a83a4f0f204f56a7218d014
Paul.Li <paul.li@gov.ab.ca>
2024-07-02 13:13:08 -0600

    feat: add the default ajv creation function
    feat: add the default ajv creation function

--- libs/jsonforms-components/src/lib/common/Ajv.ts
+++ libs/jsonforms-components/src/lib/common/Ajv.ts
@@ -1,6 +1,9 @@
 import Ajv from 'ajv';
 import addFormats from 'ajv-formats';
-export const ajv = new Ajv({ allErrors: true, verbose: true, strict: 'log' });
 
-//Example format: urn:ads:platform:file-service:v1:/files/f6de737e-c5fc-42fe-963b-28bfe14597c4
-ajv.addFormat('file-urn', /^urn:ads:platform:file-service:v[0-9]:\/files\/[a-zA-Z0-9.-]*$/);
+export const createDefaultAjv = () => {
+  const ajv = new Ajv({ allErrors: true, verbose: true, strict: 'log' });
+  ajv.addFormat('file-urn', /^urn:ads:platform:file-service:v[0-9]:\/files\/[a-zA-Z0-9.-]*$/);
+  addFormats(ajv);
+  return ajv;
+};
