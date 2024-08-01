6f92779a91277ab2b55acda58e44d367b7bb4acd
Ting Zuge <ting.zuge@gov.ab.ca>
2024-08-01 15:41:26 -0600

    fix(configuration-service): removing use of aggregation pipeline
    CosmosDb Mongo API to able to support the subquery needed for a left outer join to revisions. Switching back to two separate requests.

--- apps/configuration-service/src/mongo/types.ts
+++ apps/configuration-service/src/mongo/types.ts
@@ -18,5 +18,3 @@ export interface ActiveRevisionDoc {
 export type RevisionAggregateDoc<C = unknown> = Omit<ConfigurationRevisionDoc<C>, 'namespace' | 'name' | 'tenant'> & {
   _id: Pick<ConfigurationRevisionDoc<C>, 'namespace' | 'name' | 'tenant'>;
 };
-
-export type ActiveAggregateDoc<C = unknown> = ActiveRevisionDoc & { revision: ConfigurationRevisionDoc<C> []};
