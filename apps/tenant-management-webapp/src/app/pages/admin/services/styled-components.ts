efd78228e5a93d6d7b18e5dcc92475c010e172b3
RamaChandiniDevarapalli <rama.devarapalli@gov.ab.ca>
2024-08-08 09:19:36 -0600

    feat: delete value definitions

--- apps/tenant-management-webapp/src/app/pages/admin/services/styled-components.ts
+++ apps/tenant-management-webapp/src/app/pages/admin/services/styled-components.ts
@@ -5,3 +5,72 @@ export const ModalContent = styled.div`
   margin-top: -23px;
   padding-top: 23px;
 `;
+export const EntryDetail = styled.div`
+  background: #f3f3f3;
+  white-space: pre-wrap;
+  font-family: monospace;
+  font-size: 12px;
+  line-height: 12px;
+  padding: 16px;
+  text-align: left;
+`;
+
+export const TableDiv = styled.div`
+  & td:first-child {
+    width: 120px;
+    overflow-x: hidden;
+    text-overflow: ellipsis;
+    word-wrap: break-word;
+  }
+  & td:nth-child(2) {
+    word-wrap: break-word;
+    word-break: break-word;
+  }
+
+  & td:last-child {
+    width: 40px;
+    white-space: nowrap;
+    overflow-x: hidden;
+    text-overflow: ellipsis;
+    text-align: right;
+  }
+  & .group-name {
+    font-size: var(--fs-lg);
+    font-weight: var(--fw-bold);
+  }
+
+  & td:first-child {
+    width: 100px;
+    white-space: nowrap;
+    overflow-x: hidden;
+    text-overflow: ellipsis;
+  }
+
+  & td:last-child {
+    width: 40px;
+    white-space: nowrap;
+    overflow-x: hidden;
+    text-overflow: ellipsis;
+  }
+
+  & .payload-details {
+    div {
+      background: #f3f3f3;
+      white-space: pre-wrap;
+      font-family: monospace;
+      font-size: 12px;
+      line-height: 16px;
+      padding: 16px;
+    }
+    padding: 0;
+  }
+
+  table {
+    margin-bottom: 2rem;
+  }
+`;
+
+export const Buttons = styled.div`
+  margin-bottom: 1rem;
+  text-align: left;
+`;
