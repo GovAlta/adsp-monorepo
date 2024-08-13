8108455c5a00e0d564495f24fdefc6514b45598c
thdlam <47620431+thdlam@users.noreply.github.com>
2024-08-13 10:41:46 -0600

    fix: missing scrollbar on configuration definitions

--- apps/tenant-management-webapp/src/app/pages/admin/services/configuration/definitions/definitions.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/configuration/definitions/definitions.tsx
@@ -62,6 +62,8 @@ export const ConfigurationDefinitions: FunctionComponent<ParentCompProps> = ({ a
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getConfigurationDefinitions());
+    document.body.style.borderRight = '';
+    document.body.style.overflow = 'unset';
   }, [dispatch]);
 
   useEffect(() => {
