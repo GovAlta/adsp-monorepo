bfb8e67c30f1b3d8d6a2c982a543c3f7e1df3899
Ting Zuge <ting.zuge@gov.ab.ca>
2024-09-23 16:09:50 -0600

    fix(calendar-service): change knex migration
    There appears to be some syntax difference between using chained schema changes.

--- apps/calendar-service/migrations/20240923161246_add_event_record_id.js
+++ apps/calendar-service/migrations/20240923161246_add_event_record_id.js
@@ -7,7 +7,8 @@ const TABLE_NAME = 'calendar_events';
 exports.up = function (knex) {
   return knex.schema
     .table(TABLE_NAME, (table) => {
-      table.string('record_id').index(['tenant', 'calendar', 'record_id']);
+      table.string('record_id');
+      table.index(['tenant', 'calendar', 'record_id']);
     })
     .raw('CREATE INDEX idx_context on ?? USING GIN (?? jsonb_path_ops)', [TABLE_NAME, 'context']);
 };
@@ -18,6 +19,8 @@ exports.up = function (knex) {
  */
 exports.down = function (knex) {
   return knex.schema.table(TABLE_NAME, (table) => {
-    table.dropIndex('idx_context').dropIndex(['tenant', 'calendar', 'record_id']).dropColumn('record_id');
+    table.dropIndex('idx_context');
+    table.dropIndex(['tenant', 'calendar', 'record_id']);
+    table.dropColumn('record_id');
   });
 };
