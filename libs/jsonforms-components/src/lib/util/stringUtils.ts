8bd8ef9b55ac37fbf0463591041f3d4d403e8e90
RamaChandiniDevarapalli <rama.devarapalli@gov.ab.ca>
2024-09-26 11:58:21 -0600

    feat: sin renderer

--- libs/jsonforms-components/src/lib/util/stringUtils.ts
+++ libs/jsonforms-components/src/lib/util/stringUtils.ts
@@ -1,4 +1,4 @@
-import { ControlProps, JsonSchema } from '@jsonforms/core';
+import { ControlProps, JsonSchema, extractSchema } from '@jsonforms/core';
 
 /**
  * Sets the first word to be capitalized so that it is sentence cased.
@@ -55,6 +55,30 @@ const isEmptyNumber = (schema: JsonSchema, data: unknown): boolean => {
     ((schema.type === 'number' || schema.type === 'integer') && isNaN(+data))
   );
 };
+
+const validateSinWithLuhn = (input: number): boolean => {
+  const cardNumber = input.toString();
+  const digits = cardNumber.replace(/\D/g, '').split('').map(Number);
+  let sum = 0;
+  let isSecond = false;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    let digit = digits[i];
+    if (isSecond) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+    sum += digit;
+    isSecond = !isSecond;
+  }
+  return sum % 10 === 0;
+};
+
+interface extractSchema {
+  errorMessage?: string;
+}
+
 /**
  * Check if a required, defined input value is valid. Returns an appropriate
  * error message if not.
@@ -64,7 +88,13 @@ const isEmptyNumber = (schema: JsonSchema, data: unknown): boolean => {
 export const checkFieldValidity = (props: ControlProps): string => {
   const { data, errors: ajvErrors, required, label, uischema, schema } = props;
   const labelToUpdate = getLabelText(uischema.scope, label);
+  const extraSchema = schema as JsonSchema & extractSchema;
 
+  if (extraSchema && data && extraSchema?.title === 'Social insurance number') {
+    if (!validateSinWithLuhn(data)) {
+      return extraSchema.errorMessage;
+    }
+  }
   if (required) {
     if (data === undefined) return '';
 
