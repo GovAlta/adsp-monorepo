c582e86af2305acc97da4191bcda470385948c44
Sankeerth Mulkalla <Sankeerth.Mulkalla@gov.ab.ca>
2024-10-07 16:03:41 -0600

    fix: Update jest.config.ts and .babelrc to resolve Jest testing issues
    - Modified jest.config.ts to include the correct Babel preset and ensure compatibility with TypeScript and JSX files.
    - Adjusted transform options to properly handle ES modules and support for lit-html dependencies.
    - Updated .babelrc to use the necessary presets and plugins for Babel to transpile modern JavaScript and TypeScript.
    - Ensured proper handling of syntax transformation, especially for TypeScript, ES6+ features, and JSX.

--- libs/adsp-feedback/jest.config.ts
+++ libs/adsp-feedback/jest.config.ts
@@ -6,6 +6,11 @@ export default {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
+    // Added this line to ensure lit-html is transformed
+  transformIgnorePatterns: [
+    '/node_modules/(?!lit-html|lit-html/directives)'
+  ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/adsp-feedback',
 };
+
