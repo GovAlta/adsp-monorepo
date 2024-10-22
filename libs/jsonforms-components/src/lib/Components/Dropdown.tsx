1d55f7b21659f9a696dcc52285a022a0f3b47fe4
thdlam <47620431+thdlam@users.noreply.github.com>
2024-10-22 09:11:06 -0600

    refactor and code cleanup of the dropdown component.

--- libs/jsonforms-components/src/lib/Components/Dropdown.tsx
+++ libs/jsonforms-components/src/lib/Components/Dropdown.tsx
@@ -1,120 +1,9 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { GoAInput } from '@abgov/react-components-new';
-import styled from 'styled-components';
 import { isEqual } from 'lodash';
+import { ARROW_DOWN_KEY, ARROW_UP_KEY, DropdownProps, ENTER_KEY, ESCAPE_KEY, Item, TAB_KEY } from './DropDownTypes';
+import { GoADropdownListContainer, GoADropdownListContainerWrapper, GoADropdownListOption } from './styled-components';
 
-export interface Item {
-  label: string;
-  value: string;
-}
-
-interface DropdownProps {
-  items: Array<Item>;
-  label: string;
-  selected: string;
-  optionListMaxHeight?: string;
-  onChange: (value: string) => void;
-  isAutocompletion?: boolean;
-  id?: string;
-}
-
-interface GoADropdownTextboxProps {
-  isOpen: boolean;
-}
-
-const GoADropdownTextbox = styled.div<GoADropdownTextboxProps>`
-  text-overflow: ellipsis;
-  overflow: hidden;
-  min-height: 43px;
-  box-sizing: border-box;
-  width: 100%;
-  padding: 8px;
-  padding-right: 8px;
-  border-radius: 4px;
-  border: 1px solid #666;
-  font-size: 18px;
-  font-weight: 400;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  box-shadow: ${(p) => (p.isOpen ? `0 0 0 3px #feba35` : '')};
-`;
-
-interface GoADropdownListContainerWrapperProps {
-  isOpen: boolean;
-}
-
-const GoADropdownListContainerWrapper = styled.div<GoADropdownListContainerWrapperProps>`
-  position: relative;
-  display: ${(p) => !p?.isOpen && 'none'};
-`;
-
-interface GoADropdownListContainerProps {
-  optionListMaxHeight?: string;
-}
-
-interface GoADropdownListOptionProps {
-  isSelected: boolean;
-}
-interface GoAInputDropDownProps {
-  isAutoCompletion: boolean;
-}
-
-const GoADropdownListContainer = styled.div<GoADropdownListContainerProps>`
-  border: solid 1px #dcdcdc;
-  border-radius: 4px;
-  background: #fff;
-  padding: 0;
-  margin-top: 3px;
-  width: 100%;
-  overflow-y: auto;
-  z-index: 1000;
-  caret-color: transparent;
-  position: absolute;
-  line-height: 2rem;
-  max-height: ${(p) => p.optionListMaxHeight || '272px'};
-
-  &:focus-visible {
-    outline-color: var(--goa-color-interactive-default);
-  }
-  &:hover {
-    background-color: var(--goa-color-interactive-hover) !important;
-    color: white !important;
-  }
-`;
-
-const GoADropdownListOption = styled.div<GoADropdownListOptionProps>`
-  padding: 4px 12px;
-  text-overflow: ellipsis;
-  color: ${(p) => (p.isSelected ? 'white' : 'var(--goa-color-greyscale-black)')} !important;
-  border: 0px solid #f1f1f1;
-
-  z-index: 1001;
-  cursor: pointer;
-  background: ${(p) => (p.isSelected ? 'var(--goa-color-interactive-default)' : '#fff')};
-  &:hover {
-    background-color: ${(p) =>
-      p.isSelected ? 'var(--goa-color-interactive-hover)' : 'var(--goa-color-greyscale-100) !important'};
-    color: ${(p) =>
-      p.isSelected ? 'var(--goa-color-interactive-default)' : 'var(--goa-color-interactive-hover) !important'};
-  }
-
-  &:focus-visible {
-    caret-color: transparent;
-    color: black !important;
-    background-color: var(--goa-color-interactive-hover) !important;
-  }
-  &:focus-within {
-    caret-color: transparent;
-    color: ${(p) => (p.isSelected ? 'white' : 'var(--goa-color-greyscale-black)')} !important;
-
-    background-color: var(--goa-color-interactive-hover) !important;
-  }
-  .dropDownClass:focus-visible {
-    caret-color: transparent;
-    color: ${(p) => (p.isSelected ? 'white' : 'var(--goa-color-interactive-hover) !important')};
-  }
-`;
 export const Dropdown = (props: DropdownProps): JSX.Element => {
   const { label, selected, onChange, optionListMaxHeight, isAutocompletion, id } = props;
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -122,16 +11,9 @@ export const Dropdown = (props: DropdownProps): JSX.Element => {
   const [items, setItems] = useState(props.items);
   const [inputText, setInputText] = useState<string>(selected);
   const prevCountRef = useRef(props.items);
-
   const trailingIcon = isOpen ? 'chevron-up' : 'chevron-down';
   const textInputName = `dropdown-${label}` || '';
   const textInput = document.getElementsByName(textInputName)[0] ?? null;
-
-  const ENTER_KEY = 'Enter';
-  const ESCAPE_KEY = 'Escape';
-  const ARROW_DOWN_KEY = 'ArrowDown';
-  const ARROW_UP_KEY = 'ArrowUp';
-  const TAB_KEY = 'Tab';
 
   useEffect(() => {
     if (textInput) {
@@ -148,11 +30,27 @@ export const Dropdown = (props: DropdownProps): JSX.Element => {
   }, [textInput]);
 
   function inputTextOnClick(e: MouseEvent) {
-    console.log('mouse clicked', e);
     setIsOpen(!isOpen);
   }
 
+  function updateDropDownData(item: Item) {
+    onChange(item.value);
+    setSelectedOption(item.value);
+    setInputText(item.label);
+    setIsOpen(false);
+  }
+
   function keyDown(e: KeyboardEvent) {
+    if (!isAutocompletion) {
+      handleKeyDownWithNoAutoCompletion(e);
+    } else {
+      //TO DO: Handle auto completion.
+    }
+  }
+
+  function handleKeyDownOnAutoCompletion(e: KeyboardEvent) {}
+
+  function handleKeyDownWithNoAutoCompletion(e: KeyboardEvent) {
     if (e.key === ENTER_KEY) {
       setIsOpen(!isOpen);
       const val = `jsonforms-dropdown-${label}-${items.at(0)?.value}`;
@@ -193,12 +91,9 @@ export const Dropdown = (props: DropdownProps): JSX.Element => {
 
   function handDropDownItemOnKeyDown(e: React.KeyboardEvent<HTMLDivElement>, item: Item) {
     if (e.key === ENTER_KEY) {
-      onChange(item.value);
-      setSelectedOption(item.value);
-      setInputText(item.label);
-      setIsOpen(false);
+      updateDropDownData(item);
     }
-    if (e.key === 'Escape') {
+    if (e.key === ESCAPE_KEY) {
       setIsOpen(false);
     }
 
@@ -207,7 +102,7 @@ export const Dropdown = (props: DropdownProps): JSX.Element => {
     });
 
     //Prevent jumping to the next control or element if
-    //we are on the last item in the drop down
+    //we are on the last item in the drop down list
     if (e.key === ARROW_DOWN_KEY) {
       if (item.label === items.at(-1)?.label) {
         e.preventDefault();
@@ -242,8 +137,7 @@ export const Dropdown = (props: DropdownProps): JSX.Element => {
       const val = `jsonforms-dropdown-${label}-${items.at(index - 1)?.value}`;
       const el = document.getElementById(val);
       if (el) {
-        const nextSibling = el.nextSibling as HTMLElement;
-        //e.preventDefault();
+        setIsOpen(false);
       }
     }
   }
@@ -319,10 +213,7 @@ export const Dropdown = (props: DropdownProps): JSX.Element => {
                     }
                   }}
                   onClick={() => {
-                    onChange(item.value);
-                    setSelectedOption(item.value);
-                    setInputText(item.label);
-                    setIsOpen(false);
+                    updateDropDownData(item);
                   }}
                 >
                   {item.label}
