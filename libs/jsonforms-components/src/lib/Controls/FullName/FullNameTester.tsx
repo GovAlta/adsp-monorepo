bc2603b1253dd4d857091264b5c908d0295c9d4a
Paul.Li <paul.li@gov.ab.ca>
2024-10-24 15:43:02 -0600

    feat: update the fullname and related testers

--- libs/jsonforms-components/src/lib/Controls/FullName/FullNameTester.tsx
+++ libs/jsonforms-components/src/lib/Controls/FullName/FullNameTester.tsx
@@ -1,3 +1,41 @@
-import { rankWith, RankedTester, and, scopeEndsWith } from '@jsonforms/core';
+import {
+  rankWith,
+  RankedTester,
+  UISchemaElement,
+  JsonSchema,
+  TesterContext,
+  isScoped,
+  isControl,
+} from '@jsonforms/core';
 
-export const FullNameTester: RankedTester = rankWith(5, and(scopeEndsWith('personFullName')));
+import { isObject } from 'lodash';
+
+const isFullNameSchema = (obj: unknown): boolean => {
+  if (isObject(obj)) {
+    const keys = Object.keys(obj);
+    return ['firstName', 'middleName', 'lastName'].every((attr) => keys.includes(attr)) && keys.length === 3;
+  }
+
+  return false;
+};
+
+export const isFullName = (uischema: UISchemaElement, schema: JsonSchema, context: TesterContext) => {
+  if (!isControl(uischema) || !isScoped(uischema)) {
+    return false;
+  }
+
+  if (schema?.properties && isObject(schema?.properties)) {
+    const propertyFromScope = uischema['scope'].split('/').pop() as string;
+    if (isObject(schema.properties[propertyFromScope]) && 'properties' in schema.properties[propertyFromScope]) {
+      const objToTest = schema.properties[propertyFromScope]['properties'];
+
+      if (objToTest && isFullNameSchema(objToTest)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+export const FullNameTester: RankedTester = rankWith(4, isFullName);
