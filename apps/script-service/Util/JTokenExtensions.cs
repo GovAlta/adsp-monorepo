37d46d319ea51c2ecda669a8ae12378838217519
Roy Styan <rstyan@telus.net>
2024-11-26 09:23:50 -0700

    fix: Modify technique to convert json to dictionary.

--- apps/script-service/Util/JTokenExtensions.cs
+++ apps/script-service/Util/JTokenExtensions.cs
@@ -3,32 +3,33 @@ using Newtonsoft.Json.Linq;
 namespace Adsp.Platform.ScriptService.Services.Util;
 internal static class JTokenExtensions
 {
-  public static Dictionary<string, object>? ToDictionary(this JToken token)
+
+  public static IDictionary<string, T>? ToDictionary<T>(this JToken token)
   {
     if (token != null && token is JObject item)
     {
-      return (Dictionary<string, object>?)DictionaryHelper(item);
+      return (Dictionary<string, T>?)DictionaryHelper<T>(item);
     }
     return null;
   }
 
-  public static object? DictionaryHelper(JToken token)
+  public static object? DictionaryHelper<T>(JToken token)
   {
     if (token is JObject jObject)
     {
-      var result = new Dictionary<string, object?>();
-      foreach (var property in jObject.Properties())
+      var result = new Dictionary<string, T?>();
+      foreach (JProperty property in jObject.Properties())
       {
-        result[property.Name] = DictionaryHelper(property.Value);
+        result[property.Name] = (T?)DictionaryHelper<T>(property.Value);
       }
       return result;
     }
     else if (token is JArray jArray)
     {
       var items = new List<object>();
-      foreach (var item in jArray)
+      foreach (JToken item in jArray)
       {
-        var converted = DictionaryHelper(item);
+        var converted = DictionaryHelper<T>(item);
         if (converted is not null)
         {
           items.Add(converted);
