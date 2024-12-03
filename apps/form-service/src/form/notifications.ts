f3946202a04680da1b0d3dd3805665c785b4e5a7
Ting Zuge <ting.zuge@gov.ab.ca>
2024-12-03 10:53:38 -0700

    chore(form-service): adjust notification message template for forms

--- apps/form-service/src/form/notifications.ts
+++ apps/form-service/src/form/notifications.ts
@@ -17,17 +17,20 @@ export const FormStatusNotificationType: NotificationType = {
       templates: {
         email: {
           subject: '{{ event.payload.form.definition.name }} draft created',
+          title: '{{ event.payload.form.definition.name }}',
+          subtitle: 'Form draft created',
           body: `
 <section>
-  <p>Your draft {{ event.payload.form.definition.name }} form has been created. We will use this email address to keep you updated.</p>
-  <p>Click <a href="{{ event.payload.form.formDraftUrl }}">here</a> to get back to your draft.</p>
+  <p>We're saving the draft of the <b>{{ event.payload.form.definition.name }}</b> form you started so you can submit it later. Use the following link to go back to the draft when you're ready to continue working on it: <a href="{{ event.payload.form.formDraftUrl }}">{{ event.payload.form.formDraftUrl }}</a></p>
+  <p>Future updates on your form draft and submission will also be sent to this inbox.</p>
 </section>`,
         },
         sms: {
           subject: '{{ event.payload.form.definition.name }} draft created',
           body:
-            'Your draft {{ event.payload.form.definition.name }} form has been created. We will use this email address to keep you ' +
-            'updated. Go to {{ event.payload.form.formDraftUrl }} to get back to your draft.',
+            `We're saving the draft of the {{ event.payload.form.definition.name }} form you started so you can submit it later. ` +
+            `Use the following link to go back to the draft when you're ready to continue working on it: {{ event.payload.form.formDraftUrl }}. ` +
+            'Future updates on your form draft and submission will also be sent to this number.',
         },
       },
     },
@@ -37,9 +40,11 @@ export const FormStatusNotificationType: NotificationType = {
       templates: {
         email: {
           subject: '{{ event.payload.form.definition.name }} locked',
+          title: '{{ event.payload.form.definition.name }}',
+          subtitle: 'Form draft locked',
           body: `
 <section>
-  <p>Your draft {{ event.payload.form.definition.name }} form has been locked due to inactivity. It will be deleted on {{ formatDate event.payload.deleteOn }}.</p>
+  <p>Your draft <b>{{ event.payload.form.definition.name }}</b> form has been locked due to inactivity. It will be deleted on {{ formatDate event.payload.deleteOn }}.</p>
   <p>No action is required if you do not intend to complete the submission. If you do wish to continue, please contact {{ event.payload.definition.supportEmail }} to unlock the draft.</p>
 </section>`,
         },
@@ -58,9 +63,11 @@ export const FormStatusNotificationType: NotificationType = {
       templates: {
         email: {
           subject: '{{ event.payload.form.definition.name }} unlocked',
+          title: '{{ event.payload.form.definition.name }}',
+          subtitle: 'Form draft unlocked',
           body: `
 <section>
-  <p>Your draft {{ event.payload.form.definition.name }} form has been unlocked.</p>
+  <p>Your draft <b>{{ event.payload.form.definition.name }}</b> form has been unlocked.</p>
   <p>Click <a href="{{ event.payload.form.formDraftUrl }}">here</a> to get back to your draft.</p>
 </section>`,
         },
@@ -77,10 +84,12 @@ export const FormStatusNotificationType: NotificationType = {
       name: FORM_SET_TO_DRAFT,
       templates: {
         email: {
-          subject: '{{ event.payload.form.definition.name }} set to draft',
+          subject: '{{ event.payload.form.definition.name }} returned to draft',
+          title: '{{ event.payload.form.definition.name }}',
+          subtitle: 'Form returned to draft',
           body: `
 <section>
-  <p>Your {{ event.payload.form.definition.name }} form has been set to draft.</p>
+  <p>Your <b>{{ event.payload.form.definition.name }}</b> form has been returned to draft.</p>
   <p>Click <a href="{{ event.payload.form.formDraftUrl }}">here</a> to get back to your draft.</p>
 </section>`,
         },
@@ -98,18 +107,19 @@ export const FormStatusNotificationType: NotificationType = {
       templates: {
         email: {
           subject: '{{ event.payload.form.definition.name }} received',
+          title: '{{ event.payload.form.definition.name }}',
+          subtitle: 'Form submission received',
           body: `
 <section>
-  <p>Your form submission has been received.</p>
-  <p>Click <a href="{{ event.payload.form.formDraftUrl }}">here</a> to get back to {{ event.payload.form.definition.name }}</p>
+  <p>We received your <b>{{ event.payload.form.definition.name }}</b> form submission. We appreciate your patience while it is being reviewed.</p>
+  <p>Use the following link to see a copy of your submitted form: <a href="{{ event.payload.form.formDraftUrl }}">{{ event.payload.form.formDraftUrl }}</a></p>
 </section>`,
         },
         sms: {
           subject: '{{ event.payload.form.definition.name }} received',
-          body: `
-          Your form submission has been received.
-          Go to {{ event.payload.form.formDraftUrl }} to get back to {{ event.payload.form.definition.name }}.',
-         `,
+          body:
+            'We received your {{ event.payload.form.definition.name }} form submission. We appreciate your patience while it is being reviewed. ' +
+            'Use the following link to see a copy of your submitted form: {{ event.payload.form.formDraftUrl }}',
         },
       },
     },
