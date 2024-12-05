45a83dcb1ff424c33ef76e0f4eed000f268b4f17
athena-chen-chen <58053108+athena-chen-chen@users.noreply.github.com>
2024-12-05 14:31:33 -0700

    feat: Update Address control label for street address (#3829)
    * Error creating events by calendar if End Date before Start Date or End time is before Start time
    
    * fix:Status service view includes extraneous top margin above heading
    
    * fix: add and edit notification type cancel state
    
    * fix:Save button in queue editor disables incorrectly on role changes
    
    * feat:form UI schema code completion hints based on the data schema
    
    * fix: scroll in form editor and help content.
    
    * fix:icon accessibilty issue and web-component upgrade issue
    
    * fix: Form -- All uppercase letter in Input label in schema, but in preview show input label in Pascal Case
    
    * fix: mobile view bottom up lay out
    
    * feat: Update Address control label for street address
    
    ---------
    
    Co-authored-by: Shuang chen <shuangchen@AthenaMacBook.local>
    Co-authored-by: Shuang chen <shuangchen@AthenaMacBook.lan>
    Co-authored-by: athena-chen-chen <athena.chen@gov.ab.ca>

--- libs/jsonforms-components/src/lib/Controls/AddressLookup/styled-components.tsx
+++ libs/jsonforms-components/src/lib/Controls/AddressLookup/styled-components.tsx
@@ -20,12 +20,10 @@ export const SearchBox = styled.div`
     background: var(--color-white);
     box-shadow: 0 8px 8px rgb(0 0 0 / 20%), 0 4px 4px rgb(0 0 0 / 10%);
     z-index: 99;
-    padding-left: 0px;
-
     overflow: hidden auto;
   }
   .suggestions li {
-    padding: var(--goa-space-2xs);
+    padding: var(--goa-space-xs) var(--goa-space-2xs) var(--goa-space-xs) 2px;
     color: var(--color-gray-900);
   }
   .suggestion-active,
