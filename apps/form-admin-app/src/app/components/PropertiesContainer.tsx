05614f8b5d233a663027d6fd37060cd794369c47
Ting Zuge <ting.zuge@gov.ab.ca>
2024-12-18 10:43:04 -0700

    feat(form-admin-app): add properties to details

---
+++ apps/form-admin-app/src/app/components/PropertiesContainer.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const PropertiesContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  padding-top: var(--goa-space-s);
+  padding-bottom: var(--goa-space-s);
+  padding-left: var(--goa-space-s);
+  padding-right: var(--goa-space-s);
+`;
