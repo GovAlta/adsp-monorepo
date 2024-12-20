60bef7720403bea58f31aabc173c2e8c3af699bc
Jonathan Weyermann <jonathan.weyermann@gov.ab.ca>
2024-12-20 12:59:16 -0700

    fix(file size limit enforcement) - these are not normally enforced, I want to ensure 1mb anon upload is not related to them

--- apps/file-service/src/main.ts
+++ apps/file-service/src/main.ts
@@ -39,7 +39,7 @@ async function initializeApp(): Promise<express.Application> {
 
   app.use(compression());
   app.use(helmet());
-  app.use(express.json({ limit: '1mb' }));
+  app.use(express.json({ limit: '3mb' }));
   app.use(cors());
 
   if (environment.TRUSTED_PROXY) {
--- apps/form-gateway/src/main.ts
+++ apps/form-gateway/src/main.ts
@@ -19,7 +19,7 @@ const initializeApp = async (): Promise<express.Application> => {
 
   app.use(compression());
   app.use(helmet());
-  app.use(express.json({ limit: '1mb' }));
+  app.use(express.json({ limit: '4mb' }));
   app.use(cors());
 
   if (environment.TRUSTED_PROXY) {
