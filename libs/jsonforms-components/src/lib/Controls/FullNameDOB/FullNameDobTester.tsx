bb9fa4332ba3de973ae93dbcd44865bb9fbcde14
Ting Zuge <ting.zuge@gov.ab.ca>
2025-01-03 11:44:48 -0700

    fix(jsonforms-components): use library schemaMatches tester factory
    Custom resolution of scope is not accounting for nested properties.

--- libs/jsonforms-components/src/lib/Controls/FullNameDOB/FullNameDobTester.tsx
+++ libs/jsonforms-components/src/lib/Controls/FullNameDOB/FullNameDobTester.tsx
@@ -1,43 +1,6 @@
-import {
-  rankWith,
-  RankedTester,
-  UISchemaElement,
-  JsonSchema,
-  TesterContext,
-  isScoped,
-  isControl,
-} from '@jsonforms/core';
+import { rankWith, RankedTester } from '@jsonforms/core';
+import { createSchemaMatchTester } from '../../common/tester';
 
-import { isObject } from 'lodash';
-
-const isFullNameDoBSchema = (obj: unknown): boolean => {
-  if (isObject(obj)) {
-    const keys = Object.keys(obj);
-    return (
-      ['firstName', 'middleName', 'lastName', 'dateOfBirth'].every((attr) => keys.includes(attr)) && keys.length === 4
-    );
-  }
-
-  return false;
-};
-
-export const isFullNameDoB = (uischema: UISchemaElement, schema: JsonSchema, context: TesterContext) => {
-  if (!isControl(uischema) || !isScoped(uischema)) {
-    return false;
-  }
-
-  if (schema?.properties && isObject(schema?.properties)) {
-    const propertyFromScope = uischema['scope'].split('/').pop() as string;
-    if (isObject(schema.properties[propertyFromScope]) && 'properties' in schema.properties[propertyFromScope]) {
-      const objToTest = schema.properties[propertyFromScope]['properties'];
-
-      if (objToTest && isFullNameDoBSchema(objToTest)) {
-        return true;
-      }
-    }
-  }
-
-  return false;
-};
+export const isFullNameDoB = createSchemaMatchTester(['firstName', 'middleName', 'lastName', 'dateOfBirth'], true);
 
 export const FullNameDobTester: RankedTester = rankWith(4, isFullNameDoB);
