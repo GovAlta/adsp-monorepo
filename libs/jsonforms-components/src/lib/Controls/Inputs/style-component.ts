eca1cce76a17092f2211c409404c998c91eb71d2
Thang Lam <47620431+thdlam@users.noreply.github.com>
2025-01-20 10:35:09 -0700

    fix(jsonforms-components): Checkbox not display correctly on the review summary page. (#3986)

--- libs/jsonforms-components/src/lib/Controls/Inputs/style-component.ts
+++ libs/jsonforms-components/src/lib/Controls/Inputs/style-component.ts
@@ -3,3 +3,19 @@ import styled from 'styled-components';
 export const FormFieldWrapper = styled.div`
   margin-bottom: var(--goa-space-l);
 `;
+
+export const WarningIconDiv = styled.div`
+  display: inline-flex;
+  align-items: flex-start;
+  gap: 0.25rem;
+  font-size: var(--goa-font-size-2);
+  color: var(--goa-color-interactive-error);
+`;
+
+export const RequiredTextLabel = styled.label`
+  color: var(--goa-color-greyscale-700);
+  font-weight: var(--goa-font-weight-regular);
+  font-size: var(--goa-font-size-2);
+  line-height: var(--goa-line-height-1);
+  font-style: normal;
+`;
