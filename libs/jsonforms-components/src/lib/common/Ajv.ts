78989a647297ba0ca5148a9f776669b3cc2f164e
Jonathan Weyermann <jonathan.weyermann@gov.ab.ca>
2025-02-11 12:19:21 -0700

    fix(time validation) - time is not supported by ajv

--- libs/jsonforms-components/src/lib/common/Ajv.ts
+++ libs/jsonforms-components/src/lib/common/Ajv.ts
@@ -7,9 +7,10 @@ export const createDefaultAjv = (...schemas: AnySchema[]) => {
   ajv.addSchema(schemas);
 
   addErrors(ajv);
+  addFormats(ajv);
 
   ajv.addFormat('file-urn', /^urn:ads:platform:file-service:v[0-9]:\/files\/[a-zA-Z0-9.-]*$/);
-  addFormats(ajv);
+  ajv.addFormat('time', /^([01]\d|2[0-3]):[0-5]\d(:[0-5]\d)?$/);
 
   return ajv;
 };
