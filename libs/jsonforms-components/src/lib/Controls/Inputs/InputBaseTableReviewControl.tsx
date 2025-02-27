7ae23b015f8e07028f51357a059725d08d3df473
Jonathan Weyermann <jonathan.weyermann@gov.ab.ca>
2025-02-27 15:27:53 -0500

    wip: code moved to components

--- libs/jsonforms-components/src/lib/Controls/Inputs/InputBaseTableReviewControl.tsx
+++ libs/jsonforms-components/src/lib/Controls/Inputs/InputBaseTableReviewControl.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { ReactNode, useContext } from 'react';
 import { ControlProps } from '@jsonforms/core';
 import { withJsonFormsControlProps } from '@jsonforms/react';
 import { GoAButton } from '@abgov/react-components';
@@ -7,6 +7,78 @@ import { convertToSentenceCase, getLastSegmentFromPointer } from '../../util';
 import { getLabelText } from '../../util/stringUtils';
 import { JsonFormsStepperContext } from '../FormStepper/context';
 import { GoATable } from '@abgov/react-components';
+import { GoAGrid } from '@abgov/react-components';
+import { ControlElement } from '@jsonforms/core';
+import { JsonSchema4, JsonSchema7 } from '@jsonforms/core';
+
+export interface ExtendedControlElement extends ControlElement {
+  elements?: ControlElement[];
+}
+
+export interface TableProps {
+  properties:
+    | {
+        [property: string]: JsonSchema4;
+      }
+    | {
+        [property: string]: JsonSchema7;
+      };
+  itemsSchema: {
+    properties: Record<string, unknown>;
+  };
+  reviewText: any;
+  newKey: string;
+}
+
+export interface TablePropsIsArray extends TableProps {
+  currentElements: string[];
+}
+
+//eslint-disable-next-line
+const XDataTable = ({ properties, itemsSchema, reviewText, newKey }: TableProps): JSX.Element => {
+  return (
+    <GoATable width="100%">
+      <thead>
+        <tr>
+          {properties[newKey] &&
+            Object.keys(itemsSchema?.properties)?.map((headNames, ix) => {
+              return <th key={ix}>{JSON.stringify(headNames)}</th>;
+            })}
+        </tr>
+      </thead>
+      <tbody>
+        {(reviewText[newKey] as Record<string, unknown>[]).map((obj, index) => (
+          <tr key={index}>
+            {properties[newKey] &&
+              Object.keys(itemsSchema?.properties).map((headNames, ix) => {
+                return <td key={ix}>{JSON.stringify(obj[headNames as keyof typeof obj] as ReactNode)}</td>;
+              })}
+          </tr>
+        ))}
+      </tbody>
+    </GoATable>
+  );
+};
+
+const IsArray = ({ properties, itemsSchema, reviewText, newKey, currentElements }: TablePropsIsArray): JSX.Element => {
+  return Array.isArray(reviewText[newKey]) ? (
+    <XDataTable properties={properties} itemsSchema={itemsSchema} reviewText={reviewText} newKey={newKey} />
+  ) : (
+    <div key={newKey}>
+      <GoAGrid minChildWidth="26ch" gap="m">
+        {Object.keys(reviewText[newKey])
+          .filter((k) => currentElements.includes(k))
+          .map((element) => {
+            return (
+              <div>
+                <b>{JSON.stringify(convertToSentenceCase(element))}</b>: {JSON.stringify(reviewText[newKey][element])}
+              </div>
+            );
+          })}
+      </GoAGrid>
+    </div>
+  );
+};
 
 export const GoAInputBaseTableReview = (props: ControlProps): JSX.Element => {
   const { data, uischema, label, schema } = props;
@@ -30,10 +102,10 @@ export const GoAInputBaseTableReview = (props: ControlProps): JSX.Element => {
     }
   }
 
-  const elements = (uischema as any)?.elements
-    ?.map((element: any) => {
+  const elements = (uischema as ExtendedControlElement)?.elements
+    ?.map((element) => {
       console.log(JSON.stringify(element) + '<element');
-      const result = element?.scope.split('/').filter((part, index, arr) => part !== 'properties' && index !== 0);
+      const result = element?.scope?.split('/').filter((part, index, arr) => part !== 'properties' && index !== 0);
       return result;
     })
     .flat();
@@ -49,49 +121,29 @@ export const GoAInputBaseTableReview = (props: ControlProps): JSX.Element => {
       )}
       <PageReviewValueCol>
         {typeof reviewText === 'string' ? (
-          <div>a{reviewText}a</div>
+          <div>{reviewText}</div>
         ) : (
           <div>
             {Object.keys(reviewText)
               ?.filter((k) => elements?.includes(k))
               .map((key) => {
-                const itemsSchema = properties[key]?.items as { properties: any };
+                const itemsSchema = properties[key]?.items as { properties: Record<string, unknown> };
+                const currentElements = (uischema as unknown as { elements: { scope: string }[] }).elements.map((e) => {
+                  return e.scope?.substring(e.scope?.lastIndexOf('/') + 1);
+                });
 
-                return Array.isArray(reviewText[key]) ? (
-                  <GoATable width="100%">
-                    <thead>
-                      <tr>
-                        {properties[key] &&
-                          Object.keys(itemsSchema?.properties).map((headNames, ix) => {
-                            return <th key={ix}>{headNames}</th>;
-                          })}
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {reviewText[key]?.map((obj: any, index: string) => (
-                        <tr key={index}>
-                          {properties[key] &&
-                            Object.keys(itemsSchema?.properties).map((headNames, ix) => {
-                              return <td key={ix}>{obj[headNames]}</td>;
-                            })}
-                        </tr>
-                      ))}
-                    </tbody>
-                  </GoATable>
-                ) : (
-                  <div key={key}>
-                    <div>a{JSON.stringify(uischema)}uischema</div>
-                    <div>a{JSON.stringify(key)}key</div>
-                    -----------
-                    <div>a{JSON.stringify(reviewText)}reviewText</div>
-                    --------
-                    <div>a{JSON.stringify(reviewText[key])}reviewText[key]</div>
-                  </div>
+                return (
+                  <IsArray
+                    properties={properties}
+                    itemsSchema={itemsSchema}
+                    reviewText={reviewText}
+                    newKey={key}
+                    currentElements={currentElements}
+                  />
                 );
               })}
           </div>
         )}
-        {/* <pre>{JSON.stringify(elements, null, 2)}</pre> */}
       </PageReviewValueCol>
       <PageReviewActionCol>
         <GoAButton
