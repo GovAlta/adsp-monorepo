e26c884c5ccc85d79d7fba2360292fa9232038df
Ting Zuge <ting.zuge@gov.ab.ca>
2025-03-07 08:12:44 -0700

    feat(form-service): unsubscribe applicant on form archive

--- apps/form-service/src/form/model/form.spec.ts
+++ apps/form-service/src/form/model/form.spec.ts
@@ -841,7 +841,7 @@ describe('FormEntity', () => {
       expect(deleted).toBe(true);
       expect(fileMock.delete).toHaveBeenCalled();
       expect(notificationMock.unsubscribe).toHaveBeenCalledWith(tenantId, entity.applicant.id, entity.id);
-      expect(repositoryMock.delete).toHaveBeenCalledWith(entity);
+      expect(repositoryMock.delete).toHaveBeenCalledWith(entity, notificationMock);
     });
 
     it('can throw for non admin user', async () => {
