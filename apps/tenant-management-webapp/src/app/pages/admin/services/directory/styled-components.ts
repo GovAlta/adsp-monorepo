24393e4ef532f191a5ad40ca5fc6ce0a8db3af49
athena-chen-chen <athena.chen@gov.ab.ca>
2025-03-07 09:35:13 -0700

    feat: Add resource type page and show list of resource type

---
+++ apps/tenant-management-webapp/src/app/pages/admin/services/directory/styled-components.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const NameDiv = styled.div`
+  margin-top: 1rem;
+  font-size: var(--fs-xl);
+  font-weight: var(--fw-bold);
+  padding-left: 0.4rem;
+  padding-bottom: 0.5rem;
+`;
+
+export const EntryDetail = styled.div`
+  background: #f3f3f3;
+  white-space: pre-wrap;
+  font-family: monospace;
+  font-size: 12px;
+  line-height: 12px;
+  padding: 16px;
+  text-align: left;
+`;
