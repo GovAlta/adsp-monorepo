ec2859421c97ee9231dea041f17358963f80c61f
Ting Zuge <ting.zuge@gov.ab.ca>
2025-04-04 13:39:59 -0600

    chore: upgrade to nx 19

--- libs/jsonforms-components/src/lib/Controls/FormStepper/util/helpers.ts
+++ libs/jsonforms-components/src/lib/Controls/FormStepper/util/helpers.ts
@@ -12,17 +12,16 @@ export const getProperty: any = (obj: any, propName: string) => {
 
 // TODO: Typing in this function is inconsistent. Needs to be cleaned up since it's not clear that
 // output typing of string[] is correct which could affect downstream code.
-export const pickPropertyValues = (obj: Record<string, unknown>, property: string, endWithType?: string) => {
+export const pickPropertyValues = (obj: unknown, property: string, endWithType?: string) => {
   let values: string[] = [];
-  Object.keys(obj).forEach(function (key) {
-    const value = obj[key] as Record<string, unknown>;
+  Object.entries(obj as object).forEach(function ([key, value]) {
     if (key === property) {
-      values.push(obj[key] as string);
-    } else if (_.isObject(obj[key])) {
+      values.push(value as string);
+    } else if (_.isObject(value)) {
       // if the object type is equal to end type, we are not going to continue the recursive approach
-      if (endWithType && value?.type === endWithType) {
+      if (endWithType && (value as Record<string, unknown>)?.type === endWithType) {
         if (property in value) {
-          values.push(value[property] as string);
+          values.push((value as  Record<string, string>)[property]);
         }
       } else {
         values = [...values, ...pickPropertyValues(value, property, endWithType)];
