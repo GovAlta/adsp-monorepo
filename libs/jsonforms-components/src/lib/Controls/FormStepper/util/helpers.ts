d7a379fadc9c095d01d1199d10f12ec49cf677b5
Ting Zuge <ting.zuge@gov.ab.ca>
2025-04-04 12:52:40 -0600

    chore: upgrade to nx 19

--- libs/jsonforms-components/src/lib/Controls/FormStepper/util/helpers.ts
+++ libs/jsonforms-components/src/lib/Controls/FormStepper/util/helpers.ts
@@ -10,26 +10,28 @@ export const getProperty: any = (obj: any, propName: string) => {
   }
 };
 
-// eslint-disable-next-line
-export const pickPropertyValues = (obj: any, property: string, endWithType?: string) => {
+// TODO: Typing in this function is inconsistent. Needs to be cleaned up since it's not clear that
+// output typing of string[] is correct which could affect downstream code.
+export const pickPropertyValues = (obj: Record<string, unknown>, property: string, endWithType?: string) => {
   let values: string[] = [];
   Object.keys(obj).forEach(function (key) {
+    const value = obj[key] as Record<string, unknown>;
     if (key === property) {
-      values.push(obj[key]);
+      values.push(obj[key] as string);
     } else if (_.isObject(obj[key])) {
       // if the object type is equal to end type, we are not going to continue the recursive approach
-      if (endWithType && obj[key]?.type === endWithType) {
-        if (property in obj[key]) {
-          values.push(obj[key][property]);
+      if (endWithType && value?.type === endWithType) {
+        if (property in value) {
+          values.push(value[property] as string);
         }
       } else {
-        values = [...values, ...pickPropertyValues(obj[key], property, endWithType)];
+        values = [...values, ...pickPropertyValues(value, property, endWithType)];
       }
-    } else if (_.isArray(obj[key])) {
-      const nextValues = obj[key].map(function (arrayObj: object) {
+    } else if (_.isArray(value)) {
+      const nextValues = value.map(function (arrayObj: Record<string, unknown>) {
         return pickPropertyValues(arrayObj, property, endWithType);
       });
-      values = [...values, ...nextValues];
+      values = [...values, ...nextValues.reduce((vs, v) => [...vs, ...v], [])];
     }
   });
   return values;
