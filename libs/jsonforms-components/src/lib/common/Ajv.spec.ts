4ac32ba278f2e4ca0c1699248a0ed9cb1a1a00ef
Haipeng (Paul) Li <haipeng1339@gmail.com>
2025-04-21 11:43:59 -0600

    feat: update the ajv file-urn

--- libs/jsonforms-components/src/lib/common/Ajv.spec.ts
+++ libs/jsonforms-components/src/lib/common/Ajv.spec.ts
@@ -2,9 +2,49 @@ import '@testing-library/jest-dom';
 import { createDefaultAjv } from './Ajv';
 
 describe('Ajv tests', () => {
-  it('should create a default ajv  instance', () => {
+  it('should create a default ajv instance', () => {
     const ajv = createDefaultAjv();
-
     expect(ajv).not.toBeNull();
+  });
+
+  it('can test file-urn using the ajv', () => {
+    const ajv = createDefaultAjv();
+    let valid = ajv.validate(
+      {
+        type: 'object',
+        properties: {
+          'file-urn': { type: 'string', format: 'file-urn' },
+        },
+      },
+      {
+        'file-urn': 'urn:ads:platfor:file-service:v1:/files/c7f1c6aa-3564-4541-a6f4-a0915dcb8906',
+      }
+    );
+    expect(valid).toBe(false);
+    valid = ajv.validate(
+      {
+        type: 'object',
+        properties: {
+          'file-urn': { type: 'string', format: 'file-urn' },
+        },
+      },
+      {
+        'file-urn': 'urn:ads:platform:file-service:v1:/files/c7f1c6aa-3564-4541-a6f4-a0915dcb8906',
+      }
+    );
+    expect(valid).toBe(true);
+    valid = ajv.validate(
+      {
+        type: 'object',
+        properties: {
+          'file-urn': { type: 'string', format: 'file-urn' },
+        },
+      },
+      {
+        'file-urn':
+          'urn:ads:platform:file-service:v1:/files/c7f1c6aa-3564-4541-a6f4-a0915dcb8906;urn:ads:platform:file-service:v1:/files/c7f1c6aa-3564-4541-a6f4-a0915dcb8906',
+      }
+    );
+    expect(valid).toBe(true);
   });
 });
