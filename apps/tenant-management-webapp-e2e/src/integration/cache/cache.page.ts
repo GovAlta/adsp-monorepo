318df3bdbf3b55068340d5975c275e1b4d2b354f
howard-zhang <howard_zhang@hotmail.com>
2025-05-01 16:16:09 -0600

    complete add edit delete target test

--- apps/tenant-management-webapp-e2e/src/integration/cache/cache.page.ts
+++ apps/tenant-management-webapp-e2e/src/integration/cache/cache.page.ts
@@ -8,23 +8,23 @@ class CachePage {
   }
 
   addCacheTargetOverviewTabBtn() {
-    return cy.xpath('//*[@data-testid="cache-service-overview-tab-overview"]//goa-button[text()="Add cache target"]');
+    return cy.xpath('//*[@data-testid="cache-service-overview-tab"]//goa-button[text()="Add cache target"]');
   }
 
   addCacheTargetBtn() {
-    return cy.xpath('//*[@data-testid="cache-service-overview-tab-target"]//goa-button[text()="Add cache target"]');
+    return cy.xpath('//*[@data-testid="cache-service-targets-tab"]//goa-button[text()="Add cache target"]');
   }
 
-  addTargetModalTitle() {
-    return cy.xpath('//goa-modal[@testid="target-cache" and @open="true"]//*[@slot="heading"]');
+  targetModalTitle() {
+    return cy.xpath('//goa-modal[@testid="cache-target-modal" and @open="true"]//*[@slot="heading"]');
   }
 
   targetModalCancelButton() {
-    return cy.get('[testid="add-edit-cache-cancel"]');
+    return cy.get('[testid="add-edit-target-cancel"]');
   }
 
   targetModalSaveButton() {
-    return cy.get('[testid="cache-save"]');
+    return cy.get('[testid="target-save"]');
   }
 
   targetTableHeader() {
@@ -32,11 +32,17 @@ class CachePage {
   }
 
   targetModalTtlField() {
-    return cy.get('[testid="cache-url-id"]');
+    return cy.get('[testid="target-ttl-seconds"]');
   }
 
   targetModalTtlFieldTrailingContent() {
-    return cy.xpath('//goa-form-item[@label="TTL"]/goa-input[@testid="cache-url-id"]/div[@slot="trailingContent"]');
+    return cy.xpath(
+      '//goa-form-item[@label="TTL"]/goa-input[@testid="target-ttl-seconds"]/div[@slot="trailingContent"]'
+    );
+  }
+
+  targetModalTargetFormItem() {
+    return cy.xpath('//goa-form-item[@label="Target"]');
   }
 
   targetModalTargetDropdown() {
@@ -45,6 +51,24 @@ class CachePage {
 
   targetModalTargetDropdownItems() {
     return cy.xpath('//goa-dropdown[@testid="target"]/goa-dropdown-item');
+  }
+
+  targetModalUrlField() {
+    return cy.xpath('//goa-form-item[@label="Url"]/goa-input[@testid="target-url"]');
+  }
+
+  targetsTableBody() {
+    return cy.xpath('//table[@data-testid="cache-targets-table"]//tbody');
+  }
+
+  targetEditButton(rowNumber) {
+    return cy.xpath(`(//table[@data-testid="cache-targets-table"]//goa-icon-button[@title="Edit"])[${rowNumber}]`);
+  }
+
+  targetDeleteButton(rowNumber) {
+    return cy.xpath(
+      `(//table[@data-testid="cache-targets-table"]//*[contains(@testid, "delete-target-item")])[${rowNumber}]`
+    );
   }
 }
 export default CachePage;
