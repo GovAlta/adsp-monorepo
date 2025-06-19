8e7a74c5291afdc8976ebfad3be5333669f7918c
Sreenath Sadupally <Sreenath.Sadupally@gov.ab.ca>
2025-06-19 14:20:38 -0600

    revert: feedback back to original

--- apps/tenant-management-webapp/src/app/pages/admin/services/feedback/feedback/feedbacksTable.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/feedback/feedback/feedbacksTable.tsx
@@ -1,54 +1,28 @@
 import React, { FunctionComponent } from 'react';
 import DataTable from '@components/DataTable';
+import { FeedbackTableItem } from './feedbackTableItem';
 import { Feedback } from '@store/feedback/models';
 import { TableDiv } from '../styled-components';
-import moment from 'moment';
 
 export interface FeedbackTableProps {
   feedbacks: Feedback[];
 }
 
 export const FeedbackListTable: FunctionComponent<FeedbackTableProps> = ({ feedbacks }) => {
-  function formatDate(dateString: string) {
-    const date = new Date(dateString);
-    return {
-      formattedDate: moment(date).format('MMMM Do, YYYY'),
-      formattedDateTime: moment(date).format('h:mm A'),
-    };
-  }
-
   return (
     <TableDiv>
       <DataTable data-testid="feedback-table">
         <thead data-testid="feedback-table-header">
           <tr>
-            <th data-testid="feedback-table-header-submitted-on">Submitted On</th>
+            <th data-testid="feedback-table-header-submitted-on">Submitted on</th>
             <th data-testid="feedback-table-header-view">View</th>
             <th data-testid="feedback-table-header-rating">Rating</th>
-            <th data-testid="feedback-table-header-comments">Comments</th>
-            <th data-testid="feedback-table-header-technical-issue">Technical Issue</th>
+            <th data-testid="feedback-table-header-action">Action</th>
           </tr>
         </thead>
         <tbody>
-          {feedbacks.map((feedback, idx) => {
-            const submissionDate = formatDate(feedback.timestamp);
-            const ratingValue = feedback.value.rating ?? '-';
-
-            return (
-              <tr key={idx}>
-                <td>
-                  {submissionDate.formattedDate} {submissionDate.formattedDateTime}
-                </td>
-                <td>{feedback.context.view ?? '-'}</td>
-                <td>{ratingValue}</td>
-                <td>{feedback.value.comment && feedback.value.comment.trim() !== '' ? feedback.value.comment : '-'}</td>
-                <td>
-                  {feedback.value.technicalIssue && feedback.value.technicalIssue.trim() !== ''
-                    ? feedback.value.technicalIssue
-                    : '-'}
-                </td>
-              </tr>
-            );
+          {feedbacks.map((feedback, id) => {
+            return <FeedbackTableItem key={id * 11} id={id} feedback={feedback} />;
           })}
         </tbody>
       </DataTable>
--- apps/tenant-management-webapp/src/app/pages/admin/services/feedback/routers.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/feedback/routers.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { FeedbackResults } from './feedback/feedbackResults';
 import { FeedbackTabs } from './feedbackTabs';
 
 export const Feedback = () => {
   return (
     <Routes>
       <Route path="/" element={<FeedbackTabs />} />
-      <Route path="results" element={<FeedbackResults />} />
     </Routes>
   );
 };
