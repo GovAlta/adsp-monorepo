c4b58bdf627771ba8cc283f951fd0e53fc85fd5a
howard-zhang <howard_zhang@hotmail.com>
2025-07-29 09:19:44 -0600

    increase wait for login page

--- apps/tenant-management-webapp-e2e/src/integration/common/common-library.ts
+++ apps/tenant-management-webapp-e2e/src/integration/common/common-library.ts
@@ -5,7 +5,7 @@ const commonObj = new common();
 export function tenantAdminDirectURLLogin(url, id, user, password) {
   const urlToTenantLogin = url + '/' + id + '/login?kc_idp_hint=';
   cy.visit(urlToTenantLogin);
-  cy.wait(4000); // Wait all the redirects to settle down
+  cy.wait(6000); // Wait all the redirects to settle down
   // cy.url().then(function (urlString) {
   //   if (urlString.includes('openid-connect')) {
   //     commonObj.usernameEmailField().type(user);
