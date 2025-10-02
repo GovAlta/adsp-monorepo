8af5b369e018a1df72d5b0d70c5bad1167b1b6c7
Ting Zuge <ting.zuge@gov.ab.ca>
2025-10-02 10:01:36 -0600

    feat(notification-service): support attachment on subscription notification

--- libs/adsp-service-sdk/src/notification/types.ts
+++ libs/adsp-service-sdk/src/notification/types.ts
@@ -16,7 +16,7 @@ export interface NotificationTypeEvent {
   namespace: string;
   name: string;
   templates: Partial<Record<Channel, Template>>;
-  customized?: boolean;
+  attachments?: string | string[];
 }
 
 export interface NotificationType {
