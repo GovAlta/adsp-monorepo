2fb6d210e1252041a7a486feb353364c4fc5e6a9
Haipeng (Paul) Li <haipeng1339@gmail.com>
2025-10-02 16:16:08 -0600

    Revert "fix: Update back to task list to be hyperlink with hover effect"

--- apps/feedback-service/src/environments/environment.ts
+++ apps/feedback-service/src/environments/environment.ts
@@ -7,14 +7,10 @@ dotenv.config();
 export const environment = envalid.cleanEnv(
   process.env,
   {
-    // KEYCLOAK_ROOT_URL: envalid.str({ default: 'http://localhost:8080' }),
-    // DIRECTORY_URL: envalid.str({ default: 'http://localhost:3331' }),
-    // CLIENT_ID: envalid.str({ default: 'urn:ads:platform:feedback-service' }),
-    // CLIENT_SECRET: envalid.str(),
-    KEYCLOAK_ROOT_URL: envalid.str({ default: 'https://access.adsp-dev.gov.ab.ca/auth' }),
-    DIRECTORY_URL: envalid.str({ default: 'https://directory-service.adsp-dev.gov.ab.ca' }),
+    KEYCLOAK_ROOT_URL: envalid.str({ default: 'http://localhost:8080' }),
+    DIRECTORY_URL: envalid.str({ default: 'http://localhost:3331' }),
     CLIENT_ID: envalid.str({ default: 'urn:ads:platform:feedback-service' }),
-    CLIENT_SECRET: envalid.str({ default: 'rFluZV3rLzpltwkfWh4CfNxkfgyojTyn' }),
+    CLIENT_SECRET: envalid.str(),
     AMQP_HOST: envalid.str({ default: 'localhost' }),
     AMQP_USER: envalid.str({ default: 'guest' }),
     AMQP_PASSWORD: envalid.str({ default: 'guest' }),
