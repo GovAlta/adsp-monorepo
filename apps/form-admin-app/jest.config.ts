251472facfcae33e51da97b0f5b41611a270d682
Ting Zuge <ting.zuge@gov.ab.ca>
2025-10-23 13:13:41 -0600

    fix: adjusting jest config

--- apps/form-admin-app/jest.config.ts
+++ apps/form-admin-app/jest.config.ts
@@ -13,6 +13,6 @@ export default {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/form-admin-app',
   transformIgnorePatterns: [
-    'node_modules/(?!react-markdown)',
+    'node_modules/(?!react-markdown|vfile|unist-util-stringify-position|unified|bail|is-plain-obj|trough|remark-parse|mdast-util-from-markdown|mdast-util-to-string|micromark|decode-named-character-reference|character-entities|remark-rehype|mdast-util-to-hast|trim-lines|unist-builder|unist-util-visit|unist-util-is|unist-util-position|unist-util-generated|mdast-util-definitions|property-information|hast-util-whitespace|space-separated-tokens|comma-separated-tokens|rehype-raw|hast-util-raw|hast-util-from-parse5|hastscript|hast-util-parse-selector|web-namespaces|hast-util-to-parse5|hast-to-hyperscript|zwitch|hast-util-raw|html-void-elements|remark-gfm|mdast-util-gfm|ccount|mdast-util-find-and-replace|escape-string-regexp|mdast-util-to-markdown|markdown-table)',
   ],
 };
