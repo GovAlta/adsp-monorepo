3648769bcc9436cb7a80a3ebad06992c256d6252
Sreenath Sadupally <159588704+SreenathSadupallyGoA@users.noreply.github.com>
2025-10-30 15:41:37 -0600

    fix: add margin bottom CS-4516 (#4756)
    * fix: add margin bottom CS-4516
    
    * fix: use goa-space

--- libs/jsonforms-components/src/lib/Controls/PhoneNumber/PhoneNumberWithTypeControl.tsx
+++ libs/jsonforms-components/src/lib/Controls/PhoneNumber/PhoneNumberWithTypeControl.tsx
@@ -10,6 +10,7 @@ export const PhoneGrid = styled.div`
   grid-template-columns: 1fr 2fr;
   gap: 0.2rem;
   align-items: start;
+  margin-bottom: var(--goa-space-l) !important;
 
   goa-form-item {
     margin-right: 0 !important;
