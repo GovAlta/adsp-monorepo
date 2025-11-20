8e0e180ba389260348aa61aea5a1a66bd76124a3
Ting Zuge <ting.zuge@gov.ab.ca>
2025-11-20 13:58:26 -0700

    chore(content-service): removing content service
    No use cases for this service for now; removing so it doesn't need to be maintained.

--- apps/content-service/src/adsp-strapi/server/constants.ts
+++
@@ -1,6 +0,0 @@
-import { initializePlatform } from '@abgov/adsp-service-sdk';
-
-export const ADSP_SERVICE_NAME = 'adsp';
-
-type PromiseResultType<T> = T extends Promise<infer U> ? U : never
-export type PlatformCapabilities = PromiseResultType<ReturnType<typeof initializePlatform>>;
--- apps/content-service/src/adsp-strapi/server/policies/index.ts
+++
@@ -1,9 +0,0 @@
-import isAdminUser from './is-admin-user';
-import isTenantContentManager from './is-tenant-content-manager';
-import isTenantUserWithRole from './is-tenant-user-with-role';
-
-export default {
-  isAdminUser,
-  isTenantUserWithRole,
-  isTenantContentManager,
-};
