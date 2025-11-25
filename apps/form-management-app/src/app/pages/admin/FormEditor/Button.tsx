f6be8c041d1d80dc3ba80174f16b54ec9dd5df07
Jonathan Weyermann <jonathan.weyermann@gov.ab.ca>
2025-11-25 12:09:18 -0700

    feat(form-management-app): Editor (ui/data) with modifiy/save

---
+++ apps/form-management-app/src/app/pages/admin/FormEditor/Button.tsx
@@ -0,0 +1,144 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { GoAButtonGroup, GoAButton } from '@abgov/react-components';
+import { useNavigate } from 'react-router-dom';
+import { SaveFormModal } from '../../../components/saveModal';
+import { FinalButtonPadding } from './styled-components';
+import type * as monacoNS from 'monaco-editor';
+import { FormDefinition } from '../../../state/types';
+
+type IEditor = monacoNS.editor.IStandaloneCodeEditor;
+
+export interface EditorProps {
+  definition: FormDefinition;
+
+  editorErrors: {
+    uiSchema: string | null;
+    dataSchemaJSON: string | null;
+    dataSchemaJSONSchema: string | null;
+  };
+  updateFormDefinition: (definition: FormDefinition) => void;
+  activeIndex: number;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  getCurrentEditorRef: () => any;
+  foldAll: (editor: IEditor) => void;
+  unfoldAll: (editor: IEditor) => void;
+  isFormUpdated: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  validators: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  indicator: any;
+}
+
+export const Buttons: React.FC<EditorProps> = ({
+  activeIndex,
+  editorErrors,
+  definition,
+
+  updateFormDefinition,
+  getCurrentEditorRef,
+  foldAll,
+  unfoldAll,
+  isFormUpdated,
+  validators,
+  indicator,
+}) => {
+  const [saveModal, setSaveModal] = useState({ visible: false });
+  const navigate = useNavigate();
+
+  const close = () => {
+    navigate('/');
+  };
+
+  return (
+    <div>
+      <FinalButtonPadding>
+        <GoAButtonGroup alignment="start">
+          <GoAButton
+            type="tertiary"
+            testId="collapse-all"
+            onClick={() => {
+              const editor = getCurrentEditorRef();
+              if (editor) foldAll(editor);
+            }}
+            disabled={activeIndex > 1}
+          >
+            Collapse all
+          </GoAButton>
+          <GoAButton
+            testId="expand-all"
+            type="tertiary"
+            disabled={activeIndex > 1}
+            onClick={() => {
+              const editor = getCurrentEditorRef();
+              if (editor) unfoldAll(editor);
+            }}
+          >
+            Expand all
+          </GoAButton>
+        </GoAButtonGroup>
+
+        <GoAButtonGroup alignment="end">
+          <GoAButton
+            type="primary"
+            testId="definition-form-save"
+            disabled={
+              !isFormUpdated ||
+              !definition.name ||
+              validators.haveErrors() ||
+              editorErrors.dataSchemaJSON !== null ||
+              editorErrors.dataSchemaJSONSchema !== null ||
+              editorErrors.uiSchema !== null
+            }
+            onClick={() => {
+              // if (indicator.show !== true) {
+              updateFormDefinition(definition);
+              //}
+            }}
+          >
+            Save
+          </GoAButton>
+          <GoAButton
+            testId="form-editor-cancel"
+            type="secondary"
+            onClick={() => {
+              if (isFormUpdated) {
+                setSaveModal({ visible: true });
+              } else {
+                validators.clear();
+
+                close();
+              }
+            }}
+          >
+            Back
+          </GoAButton>
+        </GoAButtonGroup>
+      </FinalButtonPadding>
+      <SaveFormModal
+        open={saveModal.visible}
+        onDontSave={() => {
+          close();
+        }}
+        onSave={() => {
+          updateFormDefinition(definition);
+          setSaveModal({ visible: false });
+          close();
+        }}
+        saveDisable={!isFormUpdated}
+        onCancel={() => {
+          setSaveModal({ visible: false });
+        }}
+      />
+    </div>
+  );
+};
+
+const Main = styled.div`
+  flex: 1 1 auto;
+  padding: var(--goa-space-l, 24px) 0;
+`;
+
+const AdminLayout = styled.div`
+  display: flex;
+`;
--- apps/form-management-app/src/app/pages/admin/FormEditor/index.tsx
+++ apps/form-management-app/src/app/pages/admin/FormEditor/index.tsx
@@ -1,14 +1,84 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
+import { FormTemplateEditorContainer } from './styled-components';
+import { useSelector, useDispatch } from 'react-redux';
+import { AppDispatch, AppState } from '../../../state';
+import {
+  updateDefinition,
+  openEditorForDefinition,
 
-const FormEditor = (): JSX.Element => {
-  const { formId } = useParams<{ formId: string }>();
+} from '../../../state/form/form.slice';
+import { isFormUpdatedSelector } from '../../../state/form/selectors';
+
+import { Editor } from './Editor';
+import { modifiedDefinitionSelector } from '../../../state/form/selectors';
+import { setDraftDataSchema, setDraftUiSchema } from '../../../state/form/form.slice';
+
+const EditorWrapper = (): JSX.Element => {
+  const { id } = useParams<{ id: string }>();
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const selectedId = useSelector((state: AppState) => state.form.editor.selectedId);
+
+  useEffect(() => {
+    try {
+      if (id && id !== selectedId) {
+        dispatch(openEditorForDefinition({ id }));
+      }
+    } catch (e) {
+      console.error('ggggggggg:' + JSON.stringify(e));
+    }
+  }, [id, selectedId, dispatch]);
+
+  const { isFormUpdated } = useSelector((state: AppState) => ({
+    isFormUpdated: isFormUpdatedSelector(state),
+  }));
+
+  const updateFormDefinition = (definition) => {
+    dispatch(updateDefinition(definition));
+  };
+
+  const definition = useSelector(modifiedDefinitionSelector);
+
+  const setDraftData = (definition: string) => {
+    dispatch(setDraftDataSchema(definition));
+  };
+  const setDraftUi = (definition: string) => {
+    dispatch(setDraftUiSchema(definition));
+  };
+
+  const resolvedDataSchema = useSelector((state: AppState) => state.form.editor.resolvedDataSchema) as Record<
+    string,
+    unknown
+  >;
 
   return (
-    <div>
-      <h1>Form Editor - {formId}</h1>
-    </div>
+    <AdminLayout>
+      <FormTemplateEditorContainer>
+        {definition?.id && (
+          <Editor
+            updateFormDefinition={updateFormDefinition}
+            definition={definition}
+            setDraftDataSchema={setDraftData}
+            setDraftUiSchema={setDraftUi}
+            isFormUpdated={isFormUpdated}
+            resolvedDataSchema={resolvedDataSchema}
+          />
+        )}
+      </FormTemplateEditorContainer>
+    </AdminLayout>
   );
 };
 
-export default FormEditor;
+const Main = styled.div`
+  flex: 1 1 auto;
+  padding: var(--goa-space-l, 24px) 0;
+`;
+
+const AdminLayout = styled.div`
+  display: flex;
+`;
+
+export default EditorWrapper
