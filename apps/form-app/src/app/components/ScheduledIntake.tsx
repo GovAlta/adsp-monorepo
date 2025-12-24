c64a5e75cc1ba8b079b4a11fb37052164ae3b298
Jonathan Weyermann <jonathan.weyermann@gov.ab.ca>
2025-12-24 11:05:06 -0700

    fix(form-app): don't show 'form not available' during loading

--- apps/form-app/src/app/components/ScheduledIntake.tsx
+++ apps/form-app/src/app/components/ScheduledIntake.tsx
@@ -22,16 +22,26 @@ export const ScheduledIntake: FunctionComponent<ScheduledIntakeProps> = ({ defin
 
   const { form } = useSelector(defaultUserFormSelector);
   // Allow through to the form if the form definition doesn't use scheduled intakes, or if there is an active intake.
-  return !definition.scheduledIntakes ||
-    (definition.intake && !definition.intake.isUpcoming) ||
-    user?.roles?.includes('urn:ads:platform:form-service:form-tester') ||
-    form?.status.toUpperCase() === 'SUBMITTED' ? (
-    children
-  ) : definition.intake?.isUpcoming ? (
-    <GoACallout mt="2xl" ml="2xl" mr="2xl" mb="2xl" type="information" heading="Upcoming intake">
-      This form will be available for applications on {definition.intake.start.toFormat('LLLL d, yyyy')}.
-    </GoACallout>
-  ) : (
-    <FormNotAvailable />
-  );
+
+  const isTester = user?.roles?.includes('urn:ads:platform:form-service:form-tester');
+  const hasNoSchedule = !definition.scheduledIntakes;
+  const intakeIsOpen = definition.intake && !definition.intake.isUpcoming;
+  const formIsLoading = !form?.status;
+  const formIsSubmitted = form?.status?.toUpperCase() === 'SUBMITTED';
+
+  const allowForm = hasNoSchedule || intakeIsOpen || isTester || formIsLoading || formIsSubmitted;
+
+  if (allowForm) {
+    return children;
+  }
+
+  if (definition.intake?.isUpcoming) {
+    return (
+      <GoACallout mt="2xl" ml="2xl" mr="2xl" mb="2xl" type="information" heading="Upcoming intake">
+        This form will be available for applications on {definition.intake.start.toFormat('LLLL d, yyyy')}.
+      </GoACallout>
+    );
+  }
+
+  return <FormNotAvailable />;
 };
