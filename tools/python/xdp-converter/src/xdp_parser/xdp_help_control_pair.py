95335623846eafed349d276bf98444a62f97ae30
Roy Styan <rstyan@telus.net>
2026-01-23 10:42:52 -0700

    fix: reverse order when rendering pair elements (#4965)
    Co-authored-by: Roy Styan <roy.styan@telus.net>

--- tools/python/xdp-converter/src/xdp_parser/xdp_help_control_pair.py
+++ tools/python/xdp-converter/src/xdp_parser/xdp_help_control_pair.py
@@ -1,15 +1,21 @@
 from xdp_parser.xdp_layout import XdpLayout  # wherever it lives
 from xdp_parser.xdp_element import XdpElement
 
+debug = False
+
 
 class XdpHelpControlPair(XdpLayout):
     def __init__(self, help_elem: XdpElement, control_elem: XdpElement, context):
         super().__init__(
-            help_elem.xdp_element, "VerticalLayout", [control_elem, help_elem]
+            help_elem.xdp_element, "VerticalLayout", [help_elem, control_elem]
         )
         self.context = context
         self.control_elem = control_elem
         self.help_elem = help_elem
+        if debug:
+            name = control_elem.get_name()
+            text = help_elem.help_text()
+            print(f"[XdpHelpControlPair] Created pair: {name} -> {text}")
 
     @property
     def control(self) -> XdpElement:
