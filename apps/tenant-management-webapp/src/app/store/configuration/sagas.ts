7b5db754bdb945f728821dad97b22d204051c1f8
Paul Li <paul.li@gov.ab.ca>
2026-03-24 15:04:31 -0600

    fix: update the data register

--- apps/tenant-management-webapp/src/app/store/configuration/sagas.ts
+++ apps/tenant-management-webapp/src/app/store/configuration/sagas.ts
@@ -244,8 +244,6 @@ export function* fetchRegisterData(): SagaIterator {
 
     const tenantConfigs = Object.entries(tenantConfigDefinition);
 
-    console.log('Tenant configurations fetched for register data:', tenantConfigs);
-
     const registerConfigs =
       tenantConfigs
         // eslint-disable-next-line
