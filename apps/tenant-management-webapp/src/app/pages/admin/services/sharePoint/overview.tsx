faecd87f289e62b66f0a3f0906ba5b9afed87763
Sreenath Sadupally <159588704+SreenathSadupallyGoA@users.noreply.github.com>
2026-05-07 13:53:06 -0600

    feat: add Add connection button to overview as well. (#5382)

--- apps/tenant-management-webapp/src/app/pages/admin/services/sharePoint/overview.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/sharePoint/overview.tsx
@@ -1,20 +1,47 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { GoabButton } from '@abgov/react-components';
 import { OverviewLayout } from '@components/Overview';
+import { updateSharepointConnection } from '@store/sharePoint/actions';
+import { ConnectionModal } from './connections/connectionModal';
 
 export const SharePointOverview: FunctionComponent = () => {
+  const dispatch = useDispatch();
+  const [openAddConnection, setOpenAddConnection] = useState(false);
+
   return (
-    <OverviewLayout
-      testId="sharepoint-service-overall"
-      description={
-        <div>
-          <section>
-            <p>
-              The SharePoint service allows developers to connect to a SharePoint List, so that they can read, create,
-              and update its data.
-            </p>
-          </section>
-        </div>
-      }
-    />
+    <>
+      <OverviewLayout
+        testId="sharepoint-service-overall"
+        description={
+          <div>
+            <section>
+              <p>
+                The SharePoint service allows developers to connect to a SharePoint List, so that they can read, create,
+                and update its data.
+              </p>
+            </section>
+          </div>
+        }
+        addButton={
+          <GoabButton
+            testId="add-connection-btn"
+            onClick={() => {
+              setOpenAddConnection(true);
+            }}
+          >
+            Add connection
+          </GoabButton>
+        }
+      />
+      {openAddConnection && (
+        <ConnectionModal
+          open={openAddConnection}
+          connectionId={undefined}
+          onCancel={() => setOpenAddConnection(false)}
+          onSave={(connection) => dispatch(updateSharepointConnection(connection))}
+        />
+      )}
+    </>
   );
 };
