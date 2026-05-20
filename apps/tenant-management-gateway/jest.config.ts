183129241a9c2d8267a799231f23cc87d036189d
Subbu <subbumettu007@gmail.com>
2026-05-20 12:44:45 -0600

    feat: add tenant management gateway application with initial configuration and setup (#5415)

---
+++ apps/tenant-management-gateway/jest.config.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export default {
+  displayName: 'tenant-management-gateway',
+  preset: './jest.preset.js',
+  globals: {},
+  coverageDirectory: '../../coverage/apps/tenant-management-gateway',
+  testEnvironment: 'node',
+};
