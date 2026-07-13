42203284977c3e1f95625beb4d42d593c5749d16
Thang Lam <Thang.Lam+goa@gov.ab.ca>
2026-07-13 12:52:49 -0600

    fix: additional fixes to hard refreshes

--- apps/sandbox-app/src/app/components/SignIn.tsx
+++ apps/sandbox-app/src/app/components/SignIn.tsx
@@ -1,11 +1,12 @@
 import { Band, Container, Grid, GridItem } from '@core-services/app-common';
 import { FunctionComponent, useEffect } from 'react';
 
-import { GoabButton, GoabButtonGroup, GoabCallout } from '@abgov/react-components';
+import { GoabButton, GoabButtonGroup, GoabCallout, GoabCircularProgress } from '@abgov/react-components';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { AppDispatch, authenticatedUserSelector, environmentSelector, loginUser, tenantSelector } from '../state';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { CenteredProgress } from './styled-components';
 
 const Placeholder = styled.div`
   padding: 48px;
@@ -19,7 +20,9 @@ interface SignInProps {
 export const SignIn: FunctionComponent<SignInProps> = ({ url }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
+
   const location = useLocation();
+  const from = (location.state as { from?: string })?.from;
   const tenant = useSelector(tenantSelector);
   const authenticatedUser = useSelector(authenticatedUserSelector);
   const environment = useSelector(environmentSelector);
@@ -27,12 +30,24 @@ export const SignIn: FunctionComponent<SignInProps> = ({ url }) => {
 
   useEffect(() => {
     if (environment.tenantName && !location.pathname.includes(environment.tenantName)) {
-      navigate('/');
+      navigate('/', { state: { from } });
     }
-  }, [environment.tenantName, location.pathname, navigate]);
+  }, [environment.tenantName, location.pathname, navigate, from]);
+
+  useEffect(() => {
+    if (from && authenticatedUser === null) {
+      dispatch(loginUser({ tenant, from }));
+    }
+  }, [from, authenticatedUser, dispatch, tenant]);
 
   const shouldShowSignInButton = () => {
-    return authenticatedUser === null && (url.endsWith(`/${environment.tenantName}`) || isServicesUrl(url));
+    if (
+      (environment && from && !from.includes(`${environment.tenantName}`)) ||
+      !location.pathname.endsWith(`${environment.tenantName}`)
+    )
+      return false;
+
+    return authenticatedUser === null && !from;
   };
 
   const onSignInStart = () => {
@@ -51,12 +66,20 @@ export const SignIn: FunctionComponent<SignInProps> = ({ url }) => {
           <GridItem md={1} />
           <GridItem md={10}>
             <div>
+              {!shouldShowSignInButton() && (
+                <CenteredProgress>
+                  <GoabCircularProgress variant="inline" size="large" message="Loading services..." visible={true} />
+                </CenteredProgress>
+              )}
               {authenticatedUser && authenticatedUser.roles.length === 0 && (
                 <Placeholder>
                   <GoabCallout heading="Not authorized" type="information">
                     You do not have a permitted role to access this sandbox.
                   </GoabCallout>
                 </Placeholder>
+              )}
+              {from && authenticatedUser === null && (
+                <GoabCircularProgress variant="inline" size="large" message="Signing in..." visible={true} />
               )}
               {shouldShowSignInButton() && (
                 <GoabButtonGroup alignment="end">
