5e425a442316beb7f364ab462f7cb460d3801d8f
thdlam <47620431+thdlam@users.noreply.github.com>
2026-08-06 20:14:50 -0600

    fix: add spacing

--- apps/tenant-management-webapp/src/app/pages/admin/services/task/tasks/taskModal.tsx
+++ apps/tenant-management-webapp/src/app/pages/admin/services/task/tasks/taskModal.tsx
@@ -51,6 +51,7 @@ interface TaskModalProps {
   onSave: (task: QueueTaskDefinition) => void;
   open: boolean;
 }
+
 export const TaskModal: FunctionComponent<TaskModalProps> = ({
   initialValue,
   type,
