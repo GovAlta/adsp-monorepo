f2108105b3938333548d8ef350e973b6b11336e9
howard-zhang <howard_zhang@hotmail.com>
2026-08-24 17:02:53 -0600

    Add 2 seconds wait

--- apps/tenant-management-webapp-e2e/src/integration/common/common-library.ts
+++ apps/tenant-management-webapp-e2e/src/integration/common/common-library.ts
@@ -32,6 +32,7 @@ export function tenantAdminDirectURLLogin(url, id, user, password) {
       }
     })
     .then(() => {
+      cy.wait(2000);
       cy.url({ timeout: 30000 }).should('include', '/admin');
       commonObj.adminMenuItem('menu-dashboard').should('be.visible', { timeout: 30000 });
     });
